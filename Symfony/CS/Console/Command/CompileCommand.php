8bbf6164ce563ea23915570e6a076cf9cdda75de
Fabien Potencier <fabien.potencier@gmail.com>
2012-05-17 16:14:27 +0200

    fixed CS ;)

--- Symfony/CS/Console/Command/CompileCommand.php
+++ Symfony/CS/Console/Command/CompileCommand.php
@@ -12,8 +12,6 @@
 namespace Symfony\CS\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\CS\Util\Compiler;
