PHP-CS-Fixer/PHP-CS-Fixer

commit 86a0f41450880f5224b0939645ed557306e45233

Fix $config(Dir|File)

Symfony/CS/Console/Command/FixCommand.php
@@ -181,7 +181,14 @@ EOF
 
         $configFile = $input->getOption('config-file');
         if (null === $configFile) {
-            $configDir = $path ?: getcwd();
+            if (is_file($path) && $dirName = pathinfo($path, PATHINFO_DIRNAME)) {
+                $configDir = $dirName;
+            } elseif ($stdin) {
+                $configDir = getcwd();
+                // path is directory
+            } else {
+                $configDir = $path;
+            }
             $configFile = $configDir . DIRECTORY_SEPARATOR . '.php_cs';
         }
 

commit c92a6efc21e59c9dd825d3a19fab52603e780f80

Fix: Regression introduced with 86a0f41

Symfony/CS/Console/Command/FixCommand.php
@@ -183,7 +183,7 @@ EOF
         if (null === $configFile) {
             if (is_file($path) && $dirName = pathinfo($path, PATHINFO_DIRNAME)) {
                 $configDir = $dirName;
-            } elseif ($stdin) {
+            } elseif ($stdin || null === $path) {
                 $configDir = getcwd();
                 // path is directory
             } else {