100483d35cbc93c94b1b91081e8a4c66c3da6164
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-06-23 08:32:05 +0200

    author tag for previous test

--- Symfony/CS/Tests/Fixer/LowercaseKeywordsFixerTest.php
+++ Symfony/CS/Tests/Fixer/LowercaseKeywordsFixerTest.php
@@ -13,6 +13,9 @@ namespace Symfony\CS\Tests\Fixer;
 
 use Symfony\CS\Fixer\LowercaseKeywordsFixer;
 
+/**
+ * @author Dariusz Ruminski <dariusz.ruminski@gmail.com>
+ */
 class LowercaseKeywordsFixerTest extends \PHPUnit_Framework_TestCase
 {
     /**
