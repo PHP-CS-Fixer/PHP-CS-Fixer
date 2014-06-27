9236985fb9c882d5fc960c7068af9c086b39af97
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-06-27 10:33:35 +0200

    Tokens::getKeywords - method getting available keywords

--- Symfony/CS/Tokens.php
+++ Symfony/CS/Tokens.php
@@ -54,4 +54,38 @@ class Tokens extends \SplFixedArray
     {
         return static::fromArray(token_get_all($code));
     }
+
+    /**
+     * Generate keywords array contains all keywords that exists in used PHP version.
+     *
+     * @return array
+     */
+    public static function getKeywords()
+    {
+        static $keywords = null;
+
+        if (null === $keywords) {
+            $keywords = array();
+            $keywordsStrings = array('T_ABSTRACT', 'T_ARRAY', 'T_AS', 'T_BREAK', 'T_CALLABLE', 'T_CASE',
+                'T_CATCH', 'T_CLASS', 'T_CLONE', 'T_CONST', 'T_CONTINUE', 'T_DECLARE', 'T_DEFAULT', 'T_DO',
+                'T_ECHO', 'T_ELSE', 'T_ELSEIF', 'T_EMPTY', 'T_ENDDECLARE', 'T_ENDFOR', 'T_ENDFOREACH',
+                'T_ENDIF', 'T_ENDSWITCH', 'T_ENDWHILE', 'T_EVAL', 'T_EXIT', 'T_EXTENDS', 'T_FINAL',
+                'T_FINALLY', 'T_FOR', 'T_FOREACH', 'T_FUNCTION', 'T_GLOBAL', 'T_GOTO', 'T_HALT_COMPILER',
+                'T_IF', 'T_IMPLEMENTS', 'T_INCLUDE', 'T_INCLUDE_ONCE', 'T_INSTANCEOF', 'T_INSTEADOF',
+                'T_INTERFACE', 'T_ISSET', 'T_LIST', 'T_LOGICAL_AND', 'T_LOGICAL_OR', 'T_LOGICAL_XOR',
+                'T_NAMESPACE', 'T_NEW', 'T_PRINT', 'T_PRIVATE', 'T_PROTECTED', 'T_PUBLIC', 'T_REQUIRE',
+                'T_REQUIRE_ONCE', 'T_RETURN', 'T_STATIC', 'T_SWITCH', 'T_THROW', 'T_TRAIT', 'T_TRY',
+                'T_UNSET', 'T_USE', 'T_VAR', 'T_WHILE', 'T_YIELD'
+            );
+
+            foreach ($keywordsStrings as $keywordName) {
+                if (defined($keywordName)) {
+                    $keyword = constant($keywordName);
+                    $keywords[$keyword] = $keyword;
+                }
+            }
+        }
+
+        return $keywords;
+    }
 }
