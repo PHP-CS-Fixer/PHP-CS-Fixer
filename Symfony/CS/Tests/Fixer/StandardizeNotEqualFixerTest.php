a91d8fbf9353dd7fc5d9ba02a28e475e799c98df
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-07-03 21:12:49 +0200

    StandardizeNotEqualFixer - new fixer

---
+++ Symfony/CS/Tests/Fixer/StandardizeNotEqualFixerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Symfony CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Tests\Fixer;
+
+use Symfony\CS\Fixer\StandardizeNotEqualFixer as Fixer;
+
+/**
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ */
+class StandardizeNotEqualFixerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideCases
+     */
+    public function testFix($expected, $input)
+    {
+        $fixer = new Fixer();
+        $file = $this->getTestFile();
+
+        $this->assertEquals($expected, $fixer->fix($file, $input));
+    }
+
+    public function provideCases()
+    {
+        return array(
+            array('<?php $a = ($b != $c);', '<?php $a = ($b != $c);'),
+            array('<?php $a = ($b != $c);', '<?php $a = ($b <> $c);'),
+        );
+    }
+
+    private function getTestFile($filename = __FILE__)
+    {
+        static $files = array();
+
+        if (!isset($files[$filename])) {
+            $files[$filename] = new \SplFileInfo($filename);
+        }
+
+        return $files[$filename];
+    }
+}
