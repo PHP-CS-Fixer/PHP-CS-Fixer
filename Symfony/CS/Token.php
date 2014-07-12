8ee55ca4da42ac3375a438c315f3748c847d1757
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-07-12 23:20:46 +0200

    Token - reorder properties/methods

--- Symfony/CS/Token.php
+++ Symfony/CS/Token.php
@@ -20,40 +20,6 @@ namespace Symfony\CS;
 class Token
 {
     /**
-     * Generate keywords array contains all keywords that exists in used PHP version.
-     *
-     * @return array
-     */
-    public static function getKeywords()
-    {
-        static $keywords = null;
-
-        if (null === $keywords) {
-            $keywords = array();
-            $keywordsStrings = array('T_ABSTRACT', 'T_ARRAY', 'T_AS', 'T_BREAK', 'T_CALLABLE', 'T_CASE',
-                'T_CATCH', 'T_CLASS', 'T_CLONE', 'T_CONST', 'T_CONTINUE', 'T_DECLARE', 'T_DEFAULT', 'T_DO',
-                'T_ECHO', 'T_ELSE', 'T_ELSEIF', 'T_EMPTY', 'T_ENDDECLARE', 'T_ENDFOR', 'T_ENDFOREACH',
-                'T_ENDIF', 'T_ENDSWITCH', 'T_ENDWHILE', 'T_EVAL', 'T_EXIT', 'T_EXTENDS', 'T_FINAL',
-                'T_FINALLY', 'T_FOR', 'T_FOREACH', 'T_FUNCTION', 'T_GLOBAL', 'T_GOTO', 'T_HALT_COMPILER',
-                'T_IF', 'T_IMPLEMENTS', 'T_INCLUDE', 'T_INCLUDE_ONCE', 'T_INSTANCEOF', 'T_INSTEADOF',
-                'T_INTERFACE', 'T_ISSET', 'T_LIST', 'T_LOGICAL_AND', 'T_LOGICAL_OR', 'T_LOGICAL_XOR',
-                'T_NAMESPACE', 'T_NEW', 'T_PRINT', 'T_PRIVATE', 'T_PROTECTED', 'T_PUBLIC', 'T_REQUIRE',
-                'T_REQUIRE_ONCE', 'T_RETURN', 'T_STATIC', 'T_SWITCH', 'T_THROW', 'T_TRAIT', 'T_TRY',
-                'T_UNSET', 'T_USE', 'T_VAR', 'T_WHILE', 'T_YIELD'
-            );
-
-            foreach ($keywordsStrings as $keywordName) {
-                if (defined($keywordName)) {
-                    $keyword = constant($keywordName);
-                    $keywords[$keyword] = $keyword;
-                }
-            }
-        }
-
-        return $keywords;
-    }
-
-    /**
      * Content of token prototype.
      * @type misc
      */
@@ -124,6 +90,40 @@ class Token
             $this->content,
             $this->line,
         );
+    }
+
+    /**
+     * Generate keywords array contains all keywords that exists in used PHP version.
+     *
+     * @return array
+     */
+    public static function getKeywords()
+    {
+        static $keywords = null;
+
+        if (null === $keywords) {
+            $keywords = array();
+            $keywordsStrings = array('T_ABSTRACT', 'T_ARRAY', 'T_AS', 'T_BREAK', 'T_CALLABLE', 'T_CASE',
+                'T_CATCH', 'T_CLASS', 'T_CLONE', 'T_CONST', 'T_CONTINUE', 'T_DECLARE', 'T_DEFAULT', 'T_DO',
+                'T_ECHO', 'T_ELSE', 'T_ELSEIF', 'T_EMPTY', 'T_ENDDECLARE', 'T_ENDFOR', 'T_ENDFOREACH',
+                'T_ENDIF', 'T_ENDSWITCH', 'T_ENDWHILE', 'T_EVAL', 'T_EXIT', 'T_EXTENDS', 'T_FINAL',
+                'T_FINALLY', 'T_FOR', 'T_FOREACH', 'T_FUNCTION', 'T_GLOBAL', 'T_GOTO', 'T_HALT_COMPILER',
+                'T_IF', 'T_IMPLEMENTS', 'T_INCLUDE', 'T_INCLUDE_ONCE', 'T_INSTANCEOF', 'T_INSTEADOF',
+                'T_INTERFACE', 'T_ISSET', 'T_LIST', 'T_LOGICAL_AND', 'T_LOGICAL_OR', 'T_LOGICAL_XOR',
+                'T_NAMESPACE', 'T_NEW', 'T_PRINT', 'T_PRIVATE', 'T_PROTECTED', 'T_PUBLIC', 'T_REQUIRE',
+                'T_REQUIRE_ONCE', 'T_RETURN', 'T_STATIC', 'T_SWITCH', 'T_THROW', 'T_TRAIT', 'T_TRY',
+                'T_UNSET', 'T_USE', 'T_VAR', 'T_WHILE', 'T_YIELD'
+            );
+
+            foreach ($keywordsStrings as $keywordName) {
+                if (defined($keywordName)) {
+                    $keyword = constant($keywordName);
+                    $keywords[$keyword] = $keyword;
+                }
+            }
+        }
+
+        return $keywords;
     }
 
     /**
