eb3b04136ec68a585b21490de44e19e2142b148b
Jeremy Livingston <jeremyjlivingston@gmail.com>
2014-07-25 10:34:23 -0400

    Update EndOfFileLineFeedFixer to use strict comparison

--- Symfony/CS/Fixer/EndOfFileLineFeedFixer.php
+++ Symfony/CS/Fixer/EndOfFileLineFeedFixer.php
@@ -44,7 +44,7 @@ class EndOfFileLineFeedFixer implements FixerInterface
 
     public function supports(\SplFileInfo $file)
     {
-        return 'php' == pathinfo($file->getFilename(), PATHINFO_EXTENSION);
+        return 'php' === pathinfo($file->getFilename(), PATHINFO_EXTENSION);
     }
 
     public function getName()
