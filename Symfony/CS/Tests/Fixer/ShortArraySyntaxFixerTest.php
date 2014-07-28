8ce903dd3f607b5e3ceafd09c37f155cdc6a9101
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-07-28 19:29:47 +0200

    ShortArraySyntaxFixerTest - new test case

--- Symfony/CS/Tests/Fixer/ShortArraySyntaxFixerTest.php
+++ Symfony/CS/Tests/Fixer/ShortArraySyntaxFixerTest.php
@@ -44,6 +44,7 @@ class ShortArraySyntaxFixerTest extends \PHPUnit_Framework_TestCase
             array('<?php print_r([($y ? true : false)]);', '<?php print_r(array(($y ? true : false)));'),
             array('<?php $x = [[[]]];', '<?php $x = array(array(array()));'),
             array('<?php $x = [[[]]]; $y = [[[]]];', '<?php $x = array(array(array())); $y = array(array(array()));'),
+            array('<?php function(array $foo = [] {}', '<?php function(array $foo = array() {}'),
         );
     }
 
