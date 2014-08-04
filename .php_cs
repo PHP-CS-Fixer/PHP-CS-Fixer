07a9cc2b71838b23e0327a04cc24a093af98fdda
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-08-04 17:10:33 +0200

    ElseifFixer - rewrite fixer to use Tokens

--- .php_cs
+++ .php_cs
@@ -10,7 +10,6 @@ return Symfony\CS\Config\Config::create()->finder(Symfony\CS\Finder\DefaultFinde
     ->exclude('Symfony/CS/Tests/Fixer')
     ->exclude('Symfony/CS/Tests/Fixtures')
     ->notName('phar-stub.php')
-    ->notName('ElseifFixer.php')
     ->notName('FunctionDeclarationSpacingFixer.php')
     ->in(__DIR__)
 );
--- Symfony/CS/Fixer/ElseifFixer.php
+++ Symfony/CS/Fixer/ElseifFixer.php
@@ -12,16 +12,49 @@
 namespace Symfony\CS\Fixer;
 
 use Symfony\CS\FixerInterface;
+use Symfony\CS\Tokens;
 
 /**
- * @author Fabien Potencier <fabien@symfony.com>
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  */
 class ElseifFixer implements FixerInterface
 {
+    /**
+     * Replace all `else if` (T_ELSE T_IF) with `elseif` (T_ELSEIF)
+     */
     public function fix(\SplFileInfo $file, $content)
     {
-        // [Structure] elseif, not else if
-        return preg_replace('/} else +if \(/', '} elseif (', $content);
+        $tokens = Tokens::fromCode($content);
+
+        foreach ($tokens as $index => $token) {
+            // if token is not T_ELSE - continue searching, this is not right token to fix
+            if (!$token->isGivenKind(T_ELSE)) {
+                continue;
+            }
+
+            $nextIndex = null;
+            $nextToken = $tokens->getNextNonWhitespace($index, array(), $nextIndex);
+
+            // if next meaning token is not T_IF - continue searching, this is not the case for fixing
+            if (!$nextToken->isGivenKind(T_IF)) {
+                continue;
+            }
+
+            // now we have T_ELSE following by T_IF so we could fix this
+            // 1. clear all whitespaces between T_ELSE and T_IF
+            for ($i = $index + 1; $i < $nextIndex; ++$i) {
+                $tokens[$i]->clear();
+            }
+
+            // 2. change token from T_ELSE into T_ELSEIF
+            $token->content = 'elseif';
+            $token->id = T_ELSEIF;
+
+            // 3. clear succeeding T_IF
+            $nextToken->clear();
+        }
+
+        return $tokens->generateCode();
     }
 
     public function getLevel()
