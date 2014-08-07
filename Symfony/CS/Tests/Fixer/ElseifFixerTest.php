dcf2e0993536a9c5b7ca91fe718708e92f63b649
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-08-07 16:16:07 +0200

    ElseifFixerTest - plase closing ) into correct place

--- Symfony/CS/Tests/Fixer/ElseifFixerTest.php
+++ Symfony/CS/Tests/Fixer/ElseifFixerTest.php
@@ -30,18 +30,18 @@ class ElseifFixerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertSame(
             '<?php if ($some) { $test = true; } elseif ($some !== "test") { $test = false; }',
-            $fixer->fix($this->getTestFile(), '<?php if ($some) { $test = true; } else if ($some !== "test") { $test = false; }'
-        ));
+            $fixer->fix($this->getTestFile(), '<?php if ($some) { $test = true; } else if ($some !== "test") { $test = false; }')
+        );
 
         $this->assertSame(
             '<?php if ($some) { $test = true; } elseif ($some !== "test") { $test = false; }',
-            $fixer->fix($this->getTestFile(), '<?php if ($some) { $test = true; } else  if ($some !== "test") { $test = false; }'
-        ));
+            $fixer->fix($this->getTestFile(), '<?php if ($some) { $test = true; } else  if ($some !== "test") { $test = false; }')
+        );
 
         $this->assertSame(
             '<?php $js = \'if (foo.a) { foo.a = "OK"; } else if (foo.b) { foo.b = "OK"; }\';',
-            $fixer->fix($this->getTestFile(), '<?php $js = \'if (foo.a) { foo.a = "OK"; } else if (foo.b) { foo.b = "OK"; }\';'
-        ));
+            $fixer->fix($this->getTestFile(), '<?php $js = \'if (foo.a) { foo.a = "OK"; } else if (foo.b) { foo.b = "OK"; }\';')
+        );
 
         $this->assertSame(
             '<?php
@@ -50,14 +50,17 @@ if ($a) {
 } elseif ($b) {
     $x = 2;
 }',
-            $fixer->fix($this->getTestFile(), '<?php
+            $fixer->fix(
+                $this->getTestFile(),
+                '<?php
 if ($a) {
     $x = 1;
 } else
 if ($b) {
     $x = 2;
 }'
-        ));
+            )
+        );
     }
 
     /**
