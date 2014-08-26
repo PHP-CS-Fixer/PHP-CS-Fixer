fc6aa59330c3b692813b5b10812078a4e12cdfa9
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-08-26 22:38:39 +0200

    missing file header

--- Symfony/CS/Tests/Fixer/CurlyBracketsNewlineFixerTest.php
+++ Symfony/CS/Tests/Fixer/CurlyBracketsNewlineFixerTest.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Symfony\CS\Tests\Fixer;
 
 use Symfony\CS\Fixer\CurlyBracketsNewlineFixer as Fixer;
