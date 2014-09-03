71b41a8c79d93be1a6c8687f0c0c610fe32336ca
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-09-03 18:21:34 +0200

    rename ReturnStatementsFixer

--- Symfony/CS/Tests/Fixer/All/ReturnFixerTest.php
+++ Symfony/CS/Tests/Fixer/All/ReturnFixerTest.php
@@ -16,7 +16,7 @@ use Symfony\CS\Tests\Fixer\AbstractFixerTestBase;
 /**
  * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  */
-class ReturnStatementsFixerTest extends AbstractFixerTestBase
+class ReturnFixerTest extends AbstractFixerTestBase
 {
     /**
      * @dataProvider provideCases
