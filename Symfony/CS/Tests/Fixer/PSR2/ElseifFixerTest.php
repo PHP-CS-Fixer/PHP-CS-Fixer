4a6f439a298ab1faa2a45ae6a053581d8bdd7589
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-09-03 20:05:34 +0200

    update ElseifFixerTest

--- Symfony/CS/Tests/Fixer/PSR2/ElseifFixerTest.php
+++ Symfony/CS/Tests/Fixer/PSR2/ElseifFixerTest.php
@@ -13,63 +13,46 @@ namespace Symfony\CS\Tests\Fixer\PSR2;
 
 use Symfony\CS\Fixer;
 use Symfony\CS\FixerInterface;
-use Symfony\CS\Fixer\PSR2\ElseifFixer;
+use Symfony\CS\Tests\Fixer\AbstractFixerTestBase;
 
 /**
  * @author Leszek Prabucki <leszek.prabucki@gmail.com>
  */
-class ElseifFixerTest extends \PHPUnit_Framework_TestCase
+class ElseifFixerTest extends AbstractFixerTestBase
 {
     /**
      * @covers Symfony\CS\Fixer\ElseifFixer::fix
      */
     public function testThatInvalidElseIfIsFixed()
     {
-        $fixer = new ElseifFixer();
-
-        $this->assertSame(
+        $this->makeTest(
             '<?php if ($some) { $test = true; } elseif ($some !== "test") { $test = false; }',
-            $fixer->fix($this->getTestFile(), '<?php if ($some) { $test = true; } else if ($some !== "test") { $test = false; }')
+            '<?php if ($some) { $test = true; } else if ($some !== "test") { $test = false; }'
         );
 
-        $this->assertSame(
+        $this->makeTest(
             '<?php if ($some) { $test = true; } elseif ($some !== "test") { $test = false; }',
-            $fixer->fix($this->getTestFile(), '<?php if ($some) { $test = true; } else  if ($some !== "test") { $test = false; }')
+            '<?php if ($some) { $test = true; } else  if ($some !== "test") { $test = false; }'
         );
 
-        $this->assertSame(
-            '<?php $js = \'if (foo.a) { foo.a = "OK"; } else if (foo.b) { foo.b = "OK"; }\';',
-            $fixer->fix($this->getTestFile(), '<?php $js = \'if (foo.a) { foo.a = "OK"; } else if (foo.b) { foo.b = "OK"; }\';')
+        $this->makeTest(
+            '<?php $js = \'if (foo.a) { foo.a = "OK"; } else if (foo.b) { foo.b = "OK"; }\';'
         );
 
-        $this->assertSame(
+        $this->makeTest(
             '<?php
 if ($a) {
     $x = 1;
 } elseif ($b) {
     $x = 2;
 }',
-            $fixer->fix(
-                $this->getTestFile(),
-                '<?php
+            '<?php
 if ($a) {
     $x = 1;
 } else
 if ($b) {
     $x = 2;
 }'
-            )
         );
-    }
-
-    private function getTestFile($filename = __FILE__)
-    {
-        static $files = array();
-
-        if (!isset($files[$filename])) {
-            $files[$filename] = new \SplFileInfo($filename);
-        }
-
-        return $files[$filename];
     }
 }
