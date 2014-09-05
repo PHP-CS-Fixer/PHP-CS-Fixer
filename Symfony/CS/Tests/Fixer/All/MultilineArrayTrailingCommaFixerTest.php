943b1666f4fba7b6d6917e4b5fcbc3b4cf7bb7b1
Andreas Möller <am@localheinz.com>
2014-09-05 10:45:28 +0200

    Add data to data provider
    Related to https://github.com/fabpot/PHP-CS-Fixer/issues/525.

--- Symfony/CS/Tests/Fixer/All/MultilineArrayTrailingCommaFixerTest.php
+++ Symfony/CS/Tests/Fixer/All/MultilineArrayTrailingCommaFixerTest.php
@@ -261,6 +261,53 @@ class MultilineArrayTrailingCommaFixerTest extends AbstractFixerTestBase
                     )
                 );",
             ),
+
+            array(
+                "<?php
+
+                throw new BadMethodCallException(
+                    sprintf(
+                        'Method \"%s\" not implemented',
+                        __METHOD__
+                    )
+                );"
+            ),
+            array(
+                "<?php
+
+                throw new BadMethodCallException(sprintf(
+                    'Method \"%s\" not implemented',
+                    __METHOD__
+                ));"
+            ),
+            array(
+                "<?php
+
+                namespace FOS\\RestBundle\\Controller;
+
+                class ExceptionController extends ContainerAware
+                {
+                    public function showAction(Request \$request, \$exception, DebugLoggerInterface \$logger = null, \$format = 'html')
+                    {
+                        if (!\$exception instanceof DebugFlattenException && !\$exception instanceof HttpFlattenException) {
+                            throw new \\InvalidArgumentException(sprintf(
+                                'ExceptionController::showAction can only accept some exceptions (%s, %s), \"%s\" given',
+                                'Symfony\\Component\\HttpKernel\\Exception\\FlattenException',
+                                'Symfony\\Component\\Debug\\Exception\\FlattenException',
+                                get_class(\$exception)
+                            ));
+                        }
+                    }
+                }"
+            ),
+            array(
+                "<?php
+
+                \$foo = array(
+                    array(
+                    )
+                );"
+            ),
         );
     }
 }
