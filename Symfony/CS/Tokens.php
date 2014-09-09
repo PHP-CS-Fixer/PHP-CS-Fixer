71216d75963a8ebb00d0b85b6a2cf039cfb1bf1d
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-09-09 21:13:24 +0200

    Tokens - little DRY, use Token::equalsAny

--- Symfony/CS/Tokens.php
+++ Symfony/CS/Tokens.php
@@ -646,12 +646,10 @@ class Tokens extends \SplFixedArray
 
             $token = $this[$index];
 
-            foreach ($tokens as $tokenKind) {
-                if ($token->equals($tokenKind)) {
-                    $foundIndex = $index;
+            if ($token->equalsAny($tokens)) {
+                $foundIndex = $index;
 
-                    return $token;
-                }
+                return $token;
             }
         }
     }
@@ -677,10 +675,8 @@ class Tokens extends \SplFixedArray
 
             $token = $this[$index];
 
-            foreach ($tokens as $tokenKind) {
-                if ($token->equals($tokenKind)) {
-                    continue 2;
-                }
+            if ($token->equalsAny($tokens)) {
+                continue;
             }
 
             $foundIndex = $index;
