747169e15f28f383921d9a526b26f30bcbcae9ce
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-09-14 14:20:28 +0200

    TransformatorsTest - new test class for Transformators

---
+++ Symfony/CS/Tests/Tokenizer/TransformatorsTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Tests\Tokenizer;
+
+use Symfony\CS\Tokenizer\Transformators;
+
+/**
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ */
+class TransformatorsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideCustomTokenPrefixCases
+     */
+    public function testCustomTokenPrefix($value, $name)
+    {
+        $this->assertStringStartsWith('CT_', $name, 'Custom token should start with `CT_` prefix.');
+    }
+
+    public function provideCustomTokenPrefixCases()
+    {
+        $class = new \ReflectionClass('Symfony\\CS\\Tokenizer\\Transformators');
+        $property = $class->getProperty('items');
+        $property->setAccessible(true);
+
+        $transformators = Transformators::create();
+        $items = $property->getValue($transformators);
+
+        $cases = array();
+
+        foreach ($items as $item) {
+            foreach ($item->getConstantDefinitions() as $value => $name) {
+                $cases[] = array('value' => $value, 'name' => $name);
+            }
+        }
+
+        return $cases;
+    }
+}
