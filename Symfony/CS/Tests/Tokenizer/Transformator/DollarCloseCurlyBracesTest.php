121899f4b323b605bb82c3fea2188da26005cfc5
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-09-18 22:57:20 +0200

    add Transformator/DollarCloseCurlyBraces

---
+++ Symfony/CS/Tests/Tokenizer/Transformator/DollarCloseCurlyBracesTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Tests\Tokenizer\Transformator;
+
+use Symfony\CS\Tests\Tokenizer\AbstractTransformatorTestBase;
+use Symfony\CS\Tokenizer\Tokens;
+
+/**
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ */
+class DollarCloseCurlyBracesTest extends AbstractTransformatorTestBase
+{
+    /**
+     * @dataProvider provideProcessCases
+     */
+    public function testProcess($source, array $expectedTokens)
+    {
+        $tokens = Tokens::fromCode($source);
+
+        foreach ($expectedTokens as $index => $name) {
+            $this->assertSame(constant($name), $tokens[$index]->id);
+            $this->assertSame($name, $tokens[$index]->getName());
+        }
+    }
+
+    public function provideProcessCases()
+    {
+        return array(
+            array(
+                '<?php echo "This is ${great}"',
+                array(
+                    5 => 'T_DOLLAR_OPEN_CURLY_BRACES',
+                    7 => 'CT_DOLLAR_CLOSE_CURLY_BRACES',
+                ),
+            ),
+        );
+    }
+}
