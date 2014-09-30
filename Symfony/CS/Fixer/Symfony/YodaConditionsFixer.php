60fe3cedd073bf311e77ab11ab888f333fba85df
Bram Gotink <bram@gotink.me>
2014-09-30 23:56:02 +0200

    only fix == and ===, use variables instead of "simple" values

--- Symfony/CS/Fixer/Symfony/YodaConditionsFixer.php
+++ Symfony/CS/Fixer/Symfony/YodaConditionsFixer.php
@@ -35,57 +35,29 @@ class YodaConditionsFixer extends AbstractFixer
     private function fixTokens(Tokens $tokens)
     {
         for ($index = count($tokens) - 1; 0 <= $index; --$index) {
-            if ($this->isHighComparison($tokens[$index])) {
-                $this->fixHighComparison($tokens, $index);
-            }
-        }
-
-        for ($index = count($tokens) - 1; 0 <= $index; --$index) {
-            if ($this->isLowComparison($tokens[$index])) {
-                $this->fixLowComparison($tokens, $index);
+            if ($this->isEqualComparison($tokens[$index])) {
+                $this->fixComparison($tokens, $index);
             }
         }
     }
 
-    private function isHighComparison(Token $token)
+    private function isEqualComparison(Token $token)
     {
-        if ($token->equalsAny(array('>', '<'))) {
-            return true;
-        }
-
         return $token->isGivenKind(array(
-            T_IS_SMALLER_OR_EQUAL, T_IS_GREATER_OR_EQUAL,
+            T_IS_EQUAL, T_IS_IDENTICAL,
         ));
     }
 
-    private function isLowComparison(Token $token)
+    private function fixComparison(Tokens $tokens, &$index)
     {
-        return $token->isGivenKind(array(
-            T_IS_EQUAL, T_IS_NOT_EQUAL,
-            T_IS_IDENTICAL, T_IS_NOT_IDENTICAL,
-        ));
-    }
-
-    private function fixHighComparison(Tokens $tokens, &$index)
-    {
-        $this->fixComparison($tokens, $index, true);
-    }
-
-    private function fixLowComparison(Tokens $tokens, &$index)
-    {
-        $this->fixComparison($tokens, $index, false);
-    }
-
-    private function fixComparison(Tokens $tokens, &$index, $high)
-    {
-        $startLeft = $this->findComparisonStart($tokens, $index, $high);
+        $startLeft = $this->findComparisonStart($tokens, $index);
         $endLeft = $tokens->getPrevNonWhitespace($index);
 
         $startRight = $tokens->getNextNonWhitespace($index);
-        $endRight = $this->findComparisonEnd($tokens, $index, $high);
+        $endRight = $this->findComparisonEnd($tokens, $index);
 
-        if ($this->isSimple($tokens, $startLeft, $endLeft)
-                || !$this->isSimple($tokens, $startRight, $endRight)) {
+        if (!$this->isVariable($tokens, $startLeft, $endLeft)
+                || $this->isVariable($tokens, $startRight, $endRight)) {
             // already using Yoda conditions, or impossible to write Yoda-style
             return;
         }
@@ -109,50 +81,108 @@ class YodaConditionsFixer extends AbstractFixer
             $tokens[$i]->clear();
         }
 
-        $this->turnAround($tokens[$index]);
         $tokens->insertAt($startRight, $left);
         $tokens->insertAt($startLeft, $right);
 
         $index = $startLeft;
     }
 
-    private function turnAround(Token $token)
+    private function isVariable(Tokens $tokens, $start, $end)
     {
-        static $mapping = array(
-            '<' => '>',
-            '>' => '<',
-            '<=' => '>=',
-            '>=' => '<=',
-        );
-
-        if (isset($mapping[$token->getContent()])) {
-            $token->setContent($mapping[$token->getContent()]);
-        }
-    }
-
-    private function isSimple(Tokens $tokens, $start, $end)
-    {
-        if ($end !== $start) {
-            return false;
+        if ($end === $start) {
+            return $tokens[$start]->isGivenKind(T_VARIABLE);
         }
 
-        return $tokens[$start]->isGivenKind(array(
-            // numbers
-            T_LNUMBER, T_DNUMBER,
-            // string
-            T_CONSTANT_ENCAPSED_STRING,
-            // keywords
-            T_STRING,
-        ));
+        $index = $start;
+        $expectString = false;
+        while ($index <= $end) {
+            $current = $tokens[$index];
+            if ($index < $end) {
+                $next = $tokens[$index + 1];
+
+                // self:: or ClassName::
+                if ($current->isGivenKind(T_STRING) && $next->isGivenKind(T_DOUBLE_COLON)) {
+                    $index += 2;
+                    continue;
+                }
+
+                // \ClassName
+                if ($current->isGivenKind(T_NS_SEPARATOR) && $next->isGivenKind(T_STRING)) {
+                    ++$index;
+                    continue;
+                }
+
+                // ClassName\
+                if ($current->isGivenKind(T_STRING) && $next->isGivenKind(T_NS_SEPARATOR)) {
+                    $index += 2;
+                    continue;
+                }
+
+                // $a-> or a-> (as in $b->a->c)
+                if ($current->isGivenKind($expectString ? T_STRING : T_VARIABLE) && $next->isGivenKind(T_OBJECT_OPERATOR)) {
+                    $index += 2;
+                    $expectString = true;
+                    continue;
+                }
+
+                // {...} (as in $a->{$b})
+                if ($expectString && $current->equals('{')) {
+                    $index = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_CURLY_BRACE, $index);
+
+                    if ($index === $end) {
+                        return true;
+                    } elseif ($index > $end) {
+                        return false;
+                    }
+
+                    ++$index;
+
+                    if (!$tokens[$index]->isGivenKind(T_OBJECT_OPERATOR)) {
+                        return false;
+                    }
+                    ++$index;
+
+                    continue;
+                }
+
+                // $a[...] or a[...] (as in $c->a[$b])
+                if ($current->isGivenKind($expectString ? T_STRING : T_VARIABLE) && $next->equals('[')) {
+                    $index = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_SQUARE_BRACE, $index + 1);
+
+                    if ($index === $end) {
+                        return true;
+                    } elseif ($index > $end) {
+                        return false;
+                    }
+
+                    ++$index;
+
+                    if (!$tokens[$index]->isGivenKind(T_OBJECT_OPERATOR)) {
+                        return false;
+                    }
+                    ++$index;
+
+                    $expectString = true;
+                    continue;
+                }
+
+                return false;
+            } else {
+                // this is the last token!
+                return $current->isGivenKind($expectString ? T_STRING : T_VARIABLE);
+            }
+        }
+
+        return false;
     }
 
-    private function findComparisonStart(Tokens $tokens, $index, $high)
+    private function findComparisonStart(Tokens $tokens, $index)
     {
         $level = 0;
         while (0 <= $index) {
             $token = $tokens[$index];
 
-            if (0 === $level && $this->isTokenOfLowerPrecedence($token, $high)) {
+            if (0 === $level && $this->isTokenOfLowerPrecedence($token)) {
                 break;
             }
 
@@ -172,14 +202,14 @@ class YodaConditionsFixer extends AbstractFixer
         return $tokens->getNextNonWhitespace($index);
     }
 
-    private function findComparisonEnd(Tokens $tokens, $index, $high)
+    private function findComparisonEnd(Tokens $tokens, $index)
     {
         $level = 0;
         $count = count($tokens);
         while ($index < $count) {
             $token = $tokens[$index];
 
-            if (0 === $level && $this->isTokenOfLowerPrecedence($token, $high)) {
+            if (0 === $level && $this->isTokenOfLowerPrecedence($token)) {
                 break;
             }
 
@@ -199,7 +229,7 @@ class YodaConditionsFixer extends AbstractFixer
         return $tokens->getPrevNonWhitespace($index);
     }
 
-    private function isTokenOfLowerPrecedence(Token $token, $high)
+    private function isTokenOfLowerPrecedence(Token $token)
     {
         static $tokens = array(
             // '&&', '||',
@@ -222,13 +252,6 @@ class YodaConditionsFixer extends AbstractFixer
             '=',
         );
 
-        static $highTokens = array(
-            // '==', '!=',
-            T_IS_EQUAL, T_IS_NOT_EQUAL,
-            // '===', '!==', '<>'
-            T_IS_IDENTICAL, T_IS_NOT_IDENTICAL,
-        );
-
         if ($token->isGivenKind($tokens) || $token->equalsAny($nonTokens)) {
             return true;
         }
@@ -236,12 +259,6 @@ class YodaConditionsFixer extends AbstractFixer
         // PHP 5.6 introduced **=
         if (defined('T_POW_EQUAL')) {
             if ($token->isGivenKind(constant('T_POW_EQUAL'))) {
-                return true;
-            }
-        }
-
-        if ($high) {
-            if ($token->isGivenKind($highTokens)) {
                 return true;
             }
         }
--- Symfony/CS/Tests/Fixer/Symfony/YodaConditionsFixerTest.php
+++ Symfony/CS/Tests/Fixer/Symfony/YodaConditionsFixerTest.php
@@ -19,162 +19,90 @@ use Symfony\CS\Tests\Fixer\AbstractFixerTestBase;
 class YodaConditionsFixerTest extends AbstractFixerTestBase
 {
     /**
-     * @dataProvider provideHighPrececenceExamples
+     * @dataProvider provideExamples
      */
-    public function testHighPrececence($expected, $input = null)
+    public function testFixer($expected, $input = null)
     {
         $this->makeTest($expected, $input);
     }
 
-    public function provideHighPrececenceExamples()
+    public function provideExamples()
     {
         return array(
             // simple Yoda conditions
-            array('<?php return 2 < $a;'),
-            array('<?php return 2 >= $a;'),
-            array('<?php return 2 > $this->getA();'),
+            array('<?php return 2 == $a;'),
+            array('<?php return null == $a[2];'),
+            array('<?php return $this->getStuff() === $myVarirable;'),
+            array('<?php return "" === $this->myArray[$index];'),
+            array('<?php return "" === $this->myArray[$index]->a;'),
+            array('<?php return "" === $this->myObject->{$index};'),
+            array('<?php return "" === $this->myObject->{$index}->a;'),
+            array('<?php return ($a & self::MY_BITMASK) === $a;'),
+            array('<?php return self::MY_CONST === self::$myVariable;'),
+            array('<?php return 2 * $myVar % 3 === $a;'),
 
             // simple non-Yoda conditions
             array(
-                '<?php return 2 < $a;',
-                '<?php return $a > 2;',
+                '<?php return 2 == $a;',
+                '<?php return $a == 2;',
             ),
             array(
-                '<?php return 2 >= $a;',
-                '<?php return $a <= 2;',
+                '<?php return null == $a[2];',
+                '<?php return $a[2] == null;',
             ),
             array(
-                '<?php return 2 > $this->getA();',
-                '<?php return $this->getA() < 2;',
+                '<?php return $this->getStuff() === $myVarirable;',
+                '<?php return $myVarirable === $this->getStuff();',
+            ),
+            array(
+                '<?php return "" === $this->myArray[$index];',
+                '<?php return $this->myArray[$index] === "";'
+            ),
+            array(
+                '<?php return "" === $this->myArray[$index]->a;',
+                '<?php return $this->myArray[$index]->a === "";'
+            ),
+            array(
+                '<?php return "" === $this->myObject->{$index};',
+                '<?php return $this->myObject->{$index} === "";'
+            ),
+            array(
+                '<?php return "" === $this->myObject->{$index}->a;',
+                '<?php return $this->myObject->{$index}->a === "";'
+            ),
+            array(
+                '<?php return ($a & self::MY_BITMASK) === $a;',
+                '<?php return $a === ($a & self::MY_BITMASK);',
+            ),
+            array(
+                '<?php return self::MY_CONST === self::$myVariable;',
+                '<?php return self::$myVariable === self::MY_CONST;',
+            ),
+            array(
+                '<?php return 2 * $myVar % 3 === $a;',
+                '<?php return $a === 2 * $myVar % 3;',
             ),
 
-            // non-simple Yoda conditions
-            array('<?php return 2 < $this->getA() % (4 - 1);'),
-            array('<?php return 2 >= $a ? true : false;'),
-            array('<?php return 0 < (0 > $a ? 0 : $a->getB());'),
-
-            // non-simple non-Yoda conditions
-            array(
-                '<?php return 2 < $this->getA() % (4 - 1);',
-                '<?php return $this->getA() % (4 - 1) > 2;',
-            ),
-            array(
-                '<?php return 2 >= $a ? true : false;',
-                '<?php return $a <= 2 ? true : false;',
-            ),
-            array(
-                '<?php return 0 < (0 > $a ? 0 : $a->getB());',
-                '<?php return ($a < 0 ? 0 : $a->getB()) > 0;',
-            ),
-
-            // large complicated code sample
+            // complex code sample
             array(
 '<?php
-if (2 < $a) {
-    return 1 > $b % 2 ? $b - 1 : $b;
-} elseif (0 < (1 < $a % 3 ? $b->getC($c ? $c : $a))) {
-    return $a or 0 < $b;
+
+if ($a == $b) {
+    return null === $b ? (null === $a ? 0 : 0 === $a->b) : 0 === $b->a;
+} else {
+    if ((null === $b) === $c) {
+        return false === $d;
+    }
 }',
 '<?php
-if ($a > 2) {
-    return $b % 2 < 1 ? $b - 1 : $b;
-} elseif (($a % 3 > 1 ? $b->getC($c ? $c : $a)) > 0) {
-    return $a or $b > 0;
+
+if ($a == $b) {
+    return $b === null ? ($a === null ? 0 : $a->b === 0) : $b->a === 0;
+} else {
+    if ($c === ($b === null)) {
+        return $d === false;
+    }
 }',
-            ),
-        );
-    }
-
-    /**
-     * @dataProvider provideLowPrecedenceExamples
-     */
-    public function testLowPrecedence($expected, $input = null)
-    {
-        $this->makeTest($expected, $input);
-    }
-
-    public function provideLowPrecedenceExamples()
-    {
-        return array(
-            // simple Yoda conditions
-            array('<?php return 2 === $a;'),
-            array('<?php return null !== $a;'),
-            array('<?php return "" !== $this->getA();'),
-
-            // simple non-Yoda conditions
-            array(
-                '<?php return 2 === $a;',
-                '<?php return $a === 2;',
-            ),
-            array(
-                '<?php return null !== $a;',
-                '<?php return $a !== null;',
-            ),
-            array(
-                '<?php return "" !== $this->getA();',
-                '<?php return $this->getA() !== "";',
-            ),
-
-            // non-simple Yoda conditions
-            array('<?php return 2 !== $this->getA() % (4 - 1);'),
-            array('<?php return 2 !== $a ? true : false;'),
-            array('<?php return null !== (null === $a ? null : $a->getB());'),
-
-            // non-simple non-Yoda conditions
-            array(
-                '<?php return 2 !== $this->getA() % (4 - 1);',
-                '<?php return $this->getA() % (4 - 1) !== 2;',
-            ),
-            array(
-                '<?php return 2 !== $a ? true : false;',
-                '<?php return $a !== 2 ? true : false;',
-            ),
-            array(
-                '<?php return null !== (null === $a ? null : $a->getB());',
-                '<?php return ($a === null ? null : $a->getB()) !== null;',
-            ),
-
-            // large complicated code sample
-            array(
-'<?php
-if (2 === $a) {
-    return 1 === $b % 2 ? $b - 1 : $b;
-} elseif (null !== (1 === $a % 2 ? $b->getC($c ? $c : $a))) {
-    return $a or null !== $b;
-}',
-'<?php
-if ($a === 2) {
-    return $b % 2 === 1 ? $b - 1 : $b;
-} elseif (($a % 2 === 1 ? $b->getC($c ? $c : $a)) !== null) {
-    return $a or $b !== null;
-}',
-            ),
-        );
-    }
-
-    /**
-     * @dataProvider provideMixedExamples
-     */
-    public function testMix($expected, $input = null)
-    {
-        $this->makeTest($expected, $input);
-    }
-
-    public function provideMixedExamples()
-    {
-        return array(
-            // Yoda conditions
-            array('<?php return false !== 0 > $a;'),
-            array('<?php return 0 > (null === $a ? 0 : $a->getB());'),
-
-            // non-Yoda conditions
-            array(
-                '<?php return false !== 0 > $a;',
-                '<?php return $a < 0 !== false;',
-            ),
-            array(
-                '<?php return 0 > (null === $a ? 0 : $a->getB());',
-                '<?php return ($a === null ? 0 : $a->getB()) < 0;',
             ),
         );
     }
