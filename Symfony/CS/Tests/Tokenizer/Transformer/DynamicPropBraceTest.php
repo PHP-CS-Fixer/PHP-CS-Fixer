0635d7bf5f6fdd7a8fde565db1c854af685588da
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-10-18 17:50:58 +0200

    Add transformer: DynamicPropBrace

---
+++ Symfony/CS/Tests/Tokenizer/Transformer/DynamicPropBraceTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Tests\Tokenizer\Transformer;
+
+use Symfony\CS\Tests\Tokenizer\AbstractTransformerTestBase;
+use Symfony\CS\Tokenizer\Tokens;
+
+/**
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ */
+class DynamicPropBraceTest extends AbstractTransformerTestBase
+{
+    /**
+     * @dataProvider provideProcessCases
+     */
+    public function testProcess($source, array $expectedTokens)
+    {
+        $tokens = Tokens::fromCode($source);
+
+        foreach ($expectedTokens as $index => $name) {
+            $this->assertSame(constant($name), $tokens[$index]->getId());
+            $this->assertSame($name, $tokens[$index]->getName());
+        }
+    }
+
+    public function provideProcessCases()
+    {
+        return array(
+            array(
+                '<?php $foo->{$bar}',
+                array(
+                    3 => 'CT_DYNAMIC_PROP_BRACE_OPEN',
+                    5 => 'CT_DYNAMIC_PROP_BRACE_CLOSE',
+                ),
+            ),
+        );
+    }
+}
