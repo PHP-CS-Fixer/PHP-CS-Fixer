276e63f383df222f0e4d7353e8bf315263371eed
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-11-07 19:02:57 +0100

    YodaConditionsFixerTest - only conditions with const on right side should be fixed

--- Symfony/CS/Tests/Fixer/Symfony/YodaConditionsFixerTest.php
+++ Symfony/CS/Tests/Fixer/Symfony/YodaConditionsFixerTest.php
@@ -15,6 +15,7 @@ use Symfony\CS\Tests\Fixer\AbstractFixerTestBase;
 
 /**
  * @author Bram Gotink <bram@gotink.me>
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  */
 class YodaConditionsFixerTest extends AbstractFixerTestBase
 {
@@ -29,24 +30,21 @@ class YodaConditionsFixerTest extends AbstractFixerTestBase
     public function provideExamples()
     {
         return array(
-            // simple Yoda conditions
-            array('<?php return 2 == $a;'),
+            // valid conditions that should not be changed
             array('<?php return array(2) == $a;'),
+            array('<?php return $a == array(2);'),
             array('<?php return array($a) == $a;'),
             array('<?php return $a == array($a);'),
-            array('<?php return null == $a[2];'),
             array('<?php return $this->getStuff() === $myVarirable;'),
-            array('<?php return "" === $this->myArray[$index];'),
-            array('<?php return "" === $this->myArray[$index]->a;'),
-            array('<?php return "" === $this->myObject->{$index};'),
-            array('<?php return "" === $this->myObject->{$index}->a;'),
-            array('<?php return ($a & self::MY_BITMASK) === $a;'),
-            array('<?php return self::MY_CONST === self::$myVariable;'),
+            array('<?php return $myVarirable === $this->getStuff();'),
             array('<?php return 2 * $myVar % 3 === $a;'),
-            array('<?php return null === $a[0 === $b ? $c : $d];'),
-            array('<?php return null === $this->{null === $a ? \'a\' : \'b\'};'),
+            array('<?php return $a === 2 * $myVar % 3;'),
+            array('<?php return ($a & self::MY_BITMASK) === $a;'),
+            array('<?php return $a === ($a & self::MY_BITMASK);'),
+            array('<?php return count($this->array[$var]) === $a;'),
+            array('<?php return $a === count($this->array[$var]);'),
 
-            // simple non-Yoda conditions
+            // simple non-Yoda conditions that need to be fixed
             array(
                 '<?php return 2 == $a;',
                 '<?php return $a == 2;',
@@ -58,10 +56,6 @@ class YodaConditionsFixerTest extends AbstractFixerTestBase
             array(
                 '<?php return null == $a[2];',
                 '<?php return $a[2] == null;',
-            ),
-            array(
-                '<?php return $this->getStuff() === $myVarirable;',
-                '<?php return $myVarirable === $this->getStuff();',
             ),
             array(
                 '<?php return "" === $this->myArray[$index];',
@@ -80,29 +74,16 @@ class YodaConditionsFixerTest extends AbstractFixerTestBase
                 '<?php return $this->myObject->{$index}->a === "";',
             ),
             array(
-                '<?php return ($a & self::MY_BITMASK) === $a;',
-                '<?php return $a === ($a & self::MY_BITMASK);',
-            ),
-            array(
                 '<?php return self::MY_CONST === self::$myVariable;',
                 '<?php return self::$myVariable === self::MY_CONST;',
-            ),
-            array(
-                '<?php return 2 * $myVar % 3 === $a;',
-                '<?php return $a === 2 * $myVar % 3;',
             ),
             array(
                 '<?php return null === $a[0 === $b ? $c : $d];',
                 '<?php return $a[$b === 0 ? $c : $d] === null;',
             ),
             array(
-                '<?php return null === $this->{null === $a ? \'a\' : \'b\'};',
-                '<?php return $this->{$a === null ? \'a\' : \'b\'} === null;',
-            ),
-
-            array(
-                '<?php return count($this->array[$var]) === $a[0 === $b ? $c : $d];',
-                '<?php return $a[$b === 0 ? $c : $d] === count($this->array[$var]);',
+                '<?php return null === $this->{null === $a ? "a" : "b"};',
+                '<?php return $this->{$a === null ? "a" : "b"} === null;',
             ),
 
             // complex code sample
@@ -111,7 +92,7 @@ class YodaConditionsFixerTest extends AbstractFixerTestBase
 if ($a == $b) {
     return null === $b ? (null === $a ? 0 : 0 === $a->b) : 0 === $b->a;
 } else {
-    if ((null === $b) === $c) {
+    if ($c === (null === $b)) {
         return false === $d;
     }
 }',
@@ -139,10 +120,7 @@ if ($a == $b) {
     public function provideExamplesPhp56()
     {
         return array(
-            // simple Yoda condition
-            array('<?php $a **= 4 === $b ? 2 : 3;'),
-
-            // simple non-Yoda condition
+            // simple non-Yoda conditions that need to be fixed
             array(
                 '<?php $a **= 4 === $b ? 2 : 3;',
                 '<?php $a **= $b === 4 ? 2 : 3;',
