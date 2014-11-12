da5d60b55b136436eb70eeb14e34089e1ff0e8ee
Fabien Potencier <fabien.potencier@gmail.com>
2014-11-12 20:32:09 +0100

    bumped version to 1.1

--- Symfony/CS/Fixer.php
+++ Symfony/CS/Fixer.php
@@ -24,7 +24,7 @@ use Symfony\Component\Stopwatch\Stopwatch;
  */
 class Fixer
 {
-    const VERSION = '1.0';
+    const VERSION = '1.1-DEV';
 
     protected $fixers = array();
     protected $configs = array();
