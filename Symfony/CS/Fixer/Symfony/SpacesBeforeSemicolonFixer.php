899816da5d8b3cc282a982e66fab98441663b8bc
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-11-16 17:39:20 +0100

    SpacesBeforeSemicolonFixer - fix next test case

--- Symfony/CS/Fixer/Symfony/SpacesBeforeSemicolonFixer.php
+++ Symfony/CS/Fixer/Symfony/SpacesBeforeSemicolonFixer.php
@@ -32,7 +32,7 @@ class SpacesBeforeSemicolonFixer extends AbstractFixer
 
             $previous = $tokens[$index - 1];
 
-            if ($previous->isWhitespace(array('whitespaces' => " \t"))) {
+            if ($previous->isWhitespace(array('whitespaces' => " \t")) && !$tokens[$index - 2]->isComment()) {
                 $previous->clear();
             }
         }
