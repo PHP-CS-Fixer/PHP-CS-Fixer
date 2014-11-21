b2e829a0ce16e31ee9354bf0610b78e24f42d3e6
Carlos C <uldericofilho@gmail.com>
2014-11-21 18:39:39 +0100

    Fix broken cases for AlignDoubleArrowFixer

--- Symfony/CS/Tests/Fixer/Contrib/AlignDoubleArrowFixerTest.php
+++ Symfony/CS/Tests/Fixer/Contrib/AlignDoubleArrowFixerTest.php
@@ -31,12 +31,88 @@ class AlignDoubleArrowFixerTest extends AbstractFixerTestBase
         return array(
             array(
                 '<?php
+                $data = [
+                    "foo"  => "Bar",
+                    "main" => array(
+                        [
+                            "baz"     => "Test",
+                            "bazaa"   => $a->{"Test"},
+                            "bazaa"   => $a["Test"],
+                            "bazaaaa" => b("Test"),
+                        ]
+                    ),
+                    "bar"  => array(),
+                ];
+                ',
+                '<?php
+                $data = [
+                    "foo"  => "Bar",
+                    "main" => array(
+                        [
+                            "baz" => "Test",
+                            "bazaa" => $a->{"Test"},
+                            "bazaa" => $a["Test"],
+                            "bazaaaa" => b("Test"),
+                        ]
+                    ),
+                    "bar"  => array(),
+                ];
+                ',
+            ),
+            array(
+                '<?php
+                $data = [
+                    "foo"  => "Bar",
+                    "main" => [array("baz" => "Test")],
+                    "bar"  => array(),
+                ];
+                $data = array(
+                    "foo"  => "Bar",
+                    "main" => array("baz" => "Test"),
+                    "bar"  => array(),
+                );
                 $var = [];
                 foreach ($foo as $i => $bar) {
                     $var[] = /* Comment */ [$i => $bar];
                 }
                 ',
             ),
+            array(
+                '<?php
+                $data = [
+                    "foo"  => "Bar",
+                    "main" => [array("baz" => "Test")],
+                    "bar"  => array(),
+                ];
+                ',
+            ),
+            array(
+                '<?php
+                $data = array(
+                    "foo"  => "Bar",
+                    "main" => array("baz" => "Test"),
+                    "bar"  => array(),
+                );
+                ',
+            ),
+            array(
+                '<?php
+                $data = array(
+                    "foo"  => "Bar",
+                    "main" => array(array("baz" => "Test")),
+                    "bar"  => array(),
+                );
+                ',
+            ),
+            array(
+                '<?php
+                $var = [];
+                foreach ($foo as $i => $bar) {
+                    $var[] = /* Comment */ [$i => $bar];
+                }
+                ',
+            ),
+
             array(
                 '<?php
                 $var = [];
@@ -136,36 +212,36 @@ class AlignDoubleArrowFixerTest extends AbstractFixerTestBase
             array(
                 '<?php
                 $arr = array(
-                $a    => 1,
-                $bbbb => \'
-                $cccccccc = 3;
-                \',
+                    $a    => 1,
+                    $bbbb => \'
+                    $cccccccc = 3;
+                    \',
                 );
                 ',
                 '<?php
                 $arr = array(
-                $a => 1,
-                $bbbb => \'
-                $cccccccc = 3;
-                \',
+                    $a => 1,
+                    $bbbb => \'
+                    $cccccccc = 3;
+                    \',
                 );
                 ',
             ),
             array(
                 '<?php
                 $arr = [
-                $a    => 1,
-                $bbbb => \'
-                $cccccccc = 3;
-                \',
+                    $a    => 1,
+                    $bbbb => \'
+                    $cccccccc = 3;
+                    \',
                 ];
                 ',
                 '<?php
                 $arr = [
-                $a => 1,
-                $bbbb => \'
-                $cccccccc = 3;
-                \',
+                    $a => 1,
+                    $bbbb => \'
+                    $cccccccc = 3;
+                    \',
                 ];
                 ',
             ),
@@ -183,23 +259,101 @@ class AlignDoubleArrowFixerTest extends AbstractFixerTestBase
             ),
             array(
                 '<?php
-    $a = array(
-        10    => 11,
-        20    => 22,
-        30    => 33,
-        40
-            =>
-                44,
-    );',
+                    $a = array(
+                        10    => 11,
+                        20    => 22,
+                        30    => 33,
+                        40
+                            =>
+                                44,
+                    );',
                 '<?php
-    $a = array(
-        10    =>    11,
-        20  =>    22,
-        30=>33,
-        40
-            =>
-                44,
-    );',
+                    $a = array(
+                        10    =>    11,
+                        20  =>    22,
+                        30=>33,
+                        40
+                            =>
+                                44,
+                    );',
+            ),
+            array(
+                '<?php
+                    return array(
+                        " "    => "",    "\t"    => "",
+                        "\n"   => "", "\r"   => "",
+                        "\0"   => "", "\x0B"    => "",
+                    );',
+                '<?php
+                    return array(
+                        " "   => "",    "\t"    => "",
+                        "\n"   => "", "\r"   => "",
+                        "\0"  => "", "\x0B"    => "",
+                    );',
+            ),
+            array(
+                '<?php
+                    return $this->grabAttribsBeforeToken(
+                        $tokens,
+                        $index,
+                        $tokenAttribsMap,
+                        array(
+                            "abstract"   => null,
+                            "final"      => null,
+                            "visibility" => new Token(array(T_PUBLIC, "public")),
+                            "static"     => null,
+                        )
+                    );',
+                '<?php
+                    return $this->grabAttribsBeforeToken(
+                        $tokens,
+                        $index,
+                        $tokenAttribsMap,
+                        array(
+                            "abstract" => null,
+                            "final" => null,
+                            "visibility" => new Token(array(T_PUBLIC, "public")),
+                            "static" => null,
+                        )
+                    );',
+            ),
+            array(
+                '<?php
+                    return array(
+                        self::STATUS_UNKNOWN    => array("symbol" => "?", "description" => "unknown"),
+                        self::STATUS_INVALID    => array("symbol" => "III", "description" => "invalid file syntax, file ignored"),
+                    );',
+                '<?php
+                    return array(
+                        self::STATUS_UNKNOWN => array("symbol" => "?", "description" => "unknown"),
+                        self::STATUS_INVALID    => array("symbol" => "III", "description" => "invalid file syntax, file ignored"),
+                    );',
+            ),
+            array(
+                '<?php
+                $array = array(
+                    "bazab" => b(array(
+                        1     => 2,
+                        5     => [
+                            6     => 7,
+                            8     => 9,
+                        ],
+                        3       => 4,
+                        10      => 11,
+                    )),
+                );',
+                '<?php
+                $array = array(
+                    "bazab" => b(array(
+                        1 => 2,
+                        5     => [
+                            6 => 7,
+                            8     => 9,
+                        ],
+                        3    => 4,
+                        10      => 11,
+                    )),
+                );',
             ),
         );
     }
