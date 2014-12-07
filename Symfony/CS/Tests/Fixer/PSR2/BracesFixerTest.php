b674929206122c135f5a42cc7d0015826bcd8cbd
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-12-07 02:35:03 +0100

    BracesFixer - add new case for testing ${$foo} syntax

--- Symfony/CS/Tests/Fixer/PSR2/BracesFixerTest.php
+++ Symfony/CS/Tests/Fixer/PSR2/BracesFixerTest.php
@@ -117,6 +117,16 @@ class BracesFixerTest extends AbstractFixerTestBase
         }
     }',
             ),
+            array(
+                '<?php
+    if (1) {
+        self::${$key} = $val;
+        self::${$type}[$rule] = $pattern;
+        self::${$type}[$rule] = array_merge($pattern, self::${$type}[$rule]);
+        self::${$type}[$rule] = $pattern + self::${$type}["rules"];
+    }
+                ',
+            ),
         );
     }
 
