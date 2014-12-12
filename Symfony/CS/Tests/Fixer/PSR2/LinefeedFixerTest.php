b40e92c4cfc0ef4048ea69c6a01e8536337596a4
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-12-12 22:17:04 +0100

    add LinefeedFixerTest

---
+++ Symfony/CS/Tests/Fixer/PSR2/LinefeedFixerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Symfony CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Tests\Fixer\PSR2;
+
+use Symfony\CS\Tests\Fixer\AbstractFixerTestBase;
+
+/**
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ */
+class LinefeedFixerTest extends AbstractFixerTestBase
+{
+    /**
+     * @dataProvider provideCases
+     */
+    public function testFix($expected, $input = null)
+    {
+        $this->makeTest($expected, $input);
+    }
+
+    public function provideCases()
+    {
+        return array(
+            array(
+                "<?php \n \$a = 1;",
+                "<?php \r\n \$a = 1;",
+            ),
+        );
+    }
+}
