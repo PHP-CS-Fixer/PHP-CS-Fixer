653cefbf33241185b58f7323157f1829552e370d
Fabien Potencier <fabien.potencier@gmail.com>
2014-12-12 07:09:01 +0100

    prepared the 1.3 release

--- Symfony/CS/Fixer.php
+++ Symfony/CS/Fixer.php
@@ -24,7 +24,7 @@ use Symfony\CS\Tokenizer\Tokens;
  */
 class Fixer
 {
-    const VERSION = '1.3-DEV';
+    const VERSION = '1.3';
 
     protected $fixers = array();
     protected $configs = array();
