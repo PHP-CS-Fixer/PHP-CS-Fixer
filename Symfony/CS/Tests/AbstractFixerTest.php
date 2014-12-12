26ce4ccc19174154f91f13340d7c7f6b4f965fe9
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-12-12 22:05:08 +0100

    AbstractFixerTest

---
+++ Symfony/CS/Tests/AbstractFixerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Tests;
+
+/**
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ */
+class AbstractFixerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSupports()
+    {
+        $mockup = $this->getMockForAbstractClass('\\Symfony\\CS\\AbstractFixer');
+        $this->assertTrue($mockup->supports(new \SplFileInfo(__FILE__)));
+    }
+}
