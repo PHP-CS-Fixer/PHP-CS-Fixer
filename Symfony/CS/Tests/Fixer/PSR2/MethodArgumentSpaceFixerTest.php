c1954c15a40869bb47b4d3b7f2d835516855606f
trilopin <jmpeso@gmail.com>
2014-12-17 19:11:32 +0100

    MethodArgumentSpaceFixer - enhance fix, only one space after comma

--- Symfony/CS/Tests/Fixer/PSR2/MethodArgumentSpaceFixerTest.php
+++ Symfony/CS/Tests/Fixer/PSR2/MethodArgumentSpaceFixerTest.php
@@ -34,20 +34,53 @@ class MethodArgumentSpaceFixerTest extends AbstractFixerTestBase
                 '<?php function xyz($a=10, $b=20, $c=30) {',
                 '<?php function xyz($a=10,$b=20,$c=30) {',
             ),
+            // test method arguments with multiple spaces
+            array(
+                '<?php function xyz($a=10, $b=20, $c=30) {',
+                '<?php function xyz($a=10,         $b=20 , $c=30) {',
+            ),
             // test method call
             array(
                 '<?php xyz($a=10, $b=20, $c=30);',
                 '<?php xyz($a=10 ,$b=20,$c=30);',
+            ),
+            // test method call with multiple spaces
+            array(
+                '<?php xyz($a=10, $b=20, $c=30);',
+                '<?php xyz($a=10 , $b=20 ,          $c=30);',
+            ),
+            // test method call with tab
+            array(
+                '<?php xyz($a=10, $b=20, $c=30);',
+                "<?php xyz(\$a=10 , \$b=20 ,\t \$c=30);",
+            ),
+            // test method call with \n not affected
+            array(
+                "<?php xyz(\$a=10, \$b=20,\n                    \$c=30);",
+            ),
+            // test method call with \r\n not affected
+            array(
+                "<?php xyz(\$a=10, \$b=20,\r\n                    \$c=30);",
             ),
             // test method call
             array(
                 '<?php xyz($a=10, $b=20, $this->foo(), $c=30);',
                 '<?php xyz($a=10,$b=20 ,$this->foo() ,$c=30);',
             ),
+            // test method call with multiple spaces
+            array(
+                '<?php xyz($a=10, $b=20, $this->foo(), $c=30);',
+                '<?php xyz($a=10,$b=20 ,         $this->foo() ,$c=30);',
+            ),
             // test receiving data in list context with omitted values
             array(
                 '<?php list($a, $b, , , $c) = foo();',
                 '<?php list($a, $b,, ,$c) = foo();',
+            ),
+            // test receiving data in list context with omitted values and multiple spaces
+            array(
+                '<?php list($a, $b, , , $c) = foo();',
+                '<?php list($a, $b,,    ,$c) = foo();',
             ),
             // skip array
             array(
@@ -57,6 +90,39 @@ class MethodArgumentSpaceFixerTest extends AbstractFixerTestBase
             array(
                 '<?php list($path, $mode, ) = foo();',
                 '<?php list($path, $mode,) = foo();',
+            ),
+            //inline comments with spaces
+            array(
+                '<?php xyz($a=10, /*comment1*/ $b=2000, /*comment2*/ $c=30);',
+                '<?php xyz($a=10,    /*comment1*/ $b=2000,/*comment2*/ $c=30);',
+
+            ),
+            // must keep align comments
+            array(
+                '<?php function xyz(
+                    $a=10,      //comment1
+                    $b=20,      //comment2
+                    $c=30) {
+                }',
+            ),
+            array(
+                '<?php function xyz(
+                    $a=10,  //comment1
+                    $b=2000,//comment2
+                    $c=30) {
+                }',
+            ),
+            //multiline comments also must be ignored
+            array(
+                '<?php function xyz(
+                    $a=10,  /* comment1a
+                               comment1b
+                            */
+                    $b=2000,/* comment2a
+                        comment 2b
+                        comment 2c */
+                    $c=30) {
+                }',
             ),
             // multi line testing method arguments
             array(
@@ -83,6 +149,15 @@ class MethodArgumentSpaceFixerTest extends AbstractFixerTestBase
                     $b=20,
                     $c=30
                     )',
+            ),
+            // skip arrays but replace arg methods
+            array(
+                '<?php fnc(1, array(2, func2(6, 7) ,4), 5);',
+                '<?php fnc(1,array(2, func2(6,    7) ,4),    5);',
+            ),
+            // ignore commas inside call argument
+            array(
+                '<?php fnc(1, array(2, 3 ,4), 5);',
             ),
             // skip multi line array
             array(
