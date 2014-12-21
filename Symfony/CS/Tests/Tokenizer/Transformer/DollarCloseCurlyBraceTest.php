96a552064d62e18aa9a4e456be5770e5b547f119
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-12-21 01:58:50 +0100

    Transformers - standardize Transformer name

--- Symfony/CS/Tests/Tokenizer/Transformer/DollarCloseCurlyBraceTest.php
+++ Symfony/CS/Tests/Tokenizer/Transformer/DollarCloseCurlyBraceTest.php
@@ -17,7 +17,7 @@ use Symfony\CS\Tokenizer\Tokens;
 /**
  * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  */
-class DollarCloseCurlyBracesTest extends AbstractTransformerTestBase
+class DollarCloseCurlyBraceTest extends AbstractTransformerTestBase
 {
     /**
      * @dataProvider provideProcessCases
