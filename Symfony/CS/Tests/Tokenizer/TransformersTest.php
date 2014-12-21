a052f08e3b576cd4522bfea32475c9d375c3bcfb
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2014-12-21 10:09:28 +0100

    TransformersTest - no need to test priority now

--- Symfony/CS/Tests/Tokenizer/TransformersTest.php
+++ Symfony/CS/Tests/Tokenizer/TransformersTest.php
@@ -47,28 +47,4 @@ class TransformersTest extends \PHPUnit_Framework_TestCase
 
         return $cases;
     }
-
-    /**
-     * @dataProvider getPriorityCases
-     */
-    public function testPriority(TransformerInterface $first, TransformerInterface $second)
-    {
-        $this->assertLessThan($first->getPriority(), $second->getPriority());
-    }
-
-    public function getPriorityCases()
-    {
-        $transformersObject = Transformers::create();
-        $transformers = array();
-
-        foreach ($transformersObject->getTransformers() as $transformer) {
-            $transformers[$transformer->getName()] = $transformer;
-        }
-
-        return array(
-            array($transformers['curly_brace'], $transformers['dollar_close_curly_brace']),
-            array($transformers['curly_brace'], $transformers['dynamic_prop_brace']),
-            array($transformers['curly_brace'], $transformers['dynamic_var_brace']),
-        );
-    }
 }
