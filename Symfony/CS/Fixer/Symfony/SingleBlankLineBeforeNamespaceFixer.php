5af3475ffd6ec9772c92ec7e8ad170624aa91b20
Graham Campbell <graham@mineuk.com>
2014-12-22 14:40:38 +0000

    Added lines before namespace fixers

---
+++ Symfony/CS/Fixer/Symfony/SingleBlankLineBeforeNamespaceFixer.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Fixer\Symfony;
+
+use Symfony\CS\AbstractLinesBeforeNamespaceFixer;
+use Symfony\CS\Tokenizer\Tokens;
+
+/**
+ * @author Graham Campbell <graham@mineuk.com>
+ */
+class SingleBlankLineBeforeNamespaceFixer extends AbstractLinesBeforeNamespaceFixer
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function fix(\SplFileInfo $file, $content)
+    {
+        $tokens = Tokens::fromCode($content);
+
+        foreach (array_keys($tokens->findGivenKind(T_NAMESPACE)) as $index) {
+            $this->fixLinesBeforeNamespace($tokens, $index, 2);
+        }
+
+        return $tokens->generateCode();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription()
+    {
+        return 'There should be exactly one blank line before a namespace declaration.';
+    }
+}
---
+++ Symfony/CS/Tests/Fixer/Symfony/SingleBlankLineBeforeNamespaceFixerTest.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Tests\Fixer\Symfony;
+
+use Symfony\CS\Tests\Fixer\AbstractFixerTestBase;
+
+/**
+ * @author Graham Campbell <graham@mineuk.com>
+ */
+class SingleBlankLineBeforeNamespaceFixerTest extends AbstractFixerTestBase
+{
+    /**
+     * @dataProvider provideExamples
+     *
+     * @param string      $expected
+     * @param string|null $input
+     */
+    public function testFix($expected, $input = null)
+    {
+        $this->makeTest($expected, $input);
+    }
+
+    /**
+     * @return array
+     */
+    public function provideExamples()
+    {
+        return array(
+            array("<?php\n\nnamespace X;"),
+            array("<?php\n\nnamespace X;", "<?php\n\n\n\nnamespace X;"),
+            array("<?php\r\n\r\nnamespace X;"),
+            array("<?php\r\n\nnamespace X;", "<?php\r\n\r\n\r\n\r\nnamespace X;"),
+        );
+    }
+
+    public function testFixExampleWithCommentTooMuch()
+    {
+        $expected = <<<'EOF'
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Fixer\Contrib;
+
+EOF;
+
+        $input = <<<'EOF'
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
+namespace Symfony\CS\Fixer\Contrib;
+
+EOF;
+
+        $this->makeTest($expected, $input);
+    }
+
+    public function testFixExampleWithCommentTooLittle()
+    {
+        $expected = <<<'EOF'
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Fixer\Contrib;
+
+EOF;
+
+        $input = <<<'EOF'
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Symfony\CS\Fixer\Contrib;
+
+EOF;
+
+        $this->makeTest($expected, $input);
+    }
+}
