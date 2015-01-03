81eed34c1f927441f051de91c8597763d2547d2c
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2015-01-03 19:53:51 +0100

    Tokens::isLambda - fix detection

--- Symfony/CS/Tokenizer/Tokens.php
+++ Symfony/CS/Tokenizer/Tokens.php
@@ -292,7 +292,7 @@ class Tokens extends \SplFixedArray
 
         if ($token->isWhitespace()) {
             $removeLastCommentLine($this[$index - 1], $indexOffset);
-            $token->setContent($whitespace);
+            $token->override(array(T_WHITESPACE, $whitespace, $token->getLine()));
 
             return false;
         }
@@ -833,7 +833,7 @@ class Tokens extends \SplFixedArray
             throw new \LogicException('No T_FUNCTION at given index');
         }
 
-        $nextIndex = $this->getNextNonWhitespace($index);
+        $nextIndex = $this->getNextMeaningfulToken($index);
         $nextToken = $this[$nextIndex];
 
         if (!$nextToken->equals('(')) {
@@ -842,7 +842,7 @@ class Tokens extends \SplFixedArray
 
         $endParenthesisIndex = $this->findBlockEnd(self::BLOCK_TYPE_PARENTHESIS_BRACE, $nextIndex);
 
-        $nextIndex = $this->getNextNonWhitespace($endParenthesisIndex);
+        $nextIndex = $this->getNextMeaningfulToken($endParenthesisIndex);
         $nextToken = $this[$nextIndex];
 
         if (!$nextToken->equalsAny(array('{', array(T_USE)))) {
