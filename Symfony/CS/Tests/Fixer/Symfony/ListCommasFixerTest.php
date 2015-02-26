04bd7d4b4357159a662fa83223e6e01f04f5fde3
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2015-02-26 21:13:11 +0100

    Add ListCommasFixer

---
+++ Symfony/CS/Tests/Fixer/Symfony/ListCommasFixerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Tests\Fixer\Symfony;
+
+use Symfony\CS\Tests\Fixer\AbstractFixerTestBase;
+
+/**
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ */
+class ListCommasFixerTest extends AbstractFixerTestBase
+{
+    /**
+     * @dataProvider provideCases
+     */
+    public function testFix($expected, $input = null)
+    {
+        $this->makeTest($expected, $input);
+    }
+
+    public function provideCases()
+    {
+        return array(
+            array(
+                '<?php
+    list($a, $b) = foo();
+    list($a, , $c, $d) = foo();
+    list($a, , $c) = foo();
+    list($a) = foo();
+    list($a , $b) = foo();
+    list($a, /* $b */, $c) = foo();
+',
+                '<?php
+    list($a, $b) = foo();
+    list($a, , $c, $d, ) = foo();
+    list($a, , $c, , ) = foo();
+    list($a, , , , , ) = foo();
+    list($a , $b , ) = foo();
+    list($a, /* $b */, $c, ) = foo();
+',
+            ),
+        );
+    }
+}
