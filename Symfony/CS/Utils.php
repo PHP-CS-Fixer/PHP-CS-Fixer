277cc8b7f30d0fb5f14d31020291fa531670a30f
Odín del Río <odin.delrio@atrapalo.com>
2015-03-12 18:22:31 +0100

    My author name added.

--- Symfony/CS/Utils.php
+++ Symfony/CS/Utils.php
@@ -16,6 +16,7 @@ use Symfony\CS\Tokenizer\Token;
 /**
  * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  * @author Graham Campbell <graham@mineuk.com>
+ * @author Odín del Río <odin.drp@gmail.com>
  *
  * @internal
  */
