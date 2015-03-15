7c6e22d32e43fc9e9bb95e148d1eb3d055f3c044
Gregor Harlan <mail@gh01.de>
2015-03-15 22:50:03 +0100

    OperatorsSpacesFixer enhancements and new UnaryOperatorsSpacesFixer

--- Symfony/CS/Tests/Tokenizer/TokensTest.php
+++ Symfony/CS/Tests/Tokenizer/TokensTest.php
@@ -17,6 +17,7 @@ use Symfony\CS\Tokenizer\Tokens;
 /**
  * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  * @author Max Voloshin <voloshin.dp@gmail.com>
+ * @author Gregor Harlan <gharlan@web.de>
  */
 class TokensTest extends \PHPUnit_Framework_TestCase
 {
@@ -198,6 +199,335 @@ preg_replace_callback(
             array(
                 '<?php "foo$bar"[1];',
                 array(5 => false),
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider provideIsUnarySuccessorOperator
+     */
+    public function testIsUnarySuccessorOperator($source, array $expected)
+    {
+        $tokens = Tokens::fromCode($source);
+
+        foreach ($expected as $index => $expected) {
+            $this->assertSame($expected, $tokens->isUnarySuccessorOperator($index));
+            if ($expected) {
+                $this->assertFalse($tokens->isUnaryPredecessorOperator($index));
+                $this->assertFalse($tokens->isBinaryOperator($index));
+            }
+        }
+    }
+
+    public function provideIsUnarySuccessorOperator()
+    {
+        return array(
+            array(
+                '<?php $a++;',
+                array(2 => true),
+            ),
+            array(
+                '<?php $a--',
+                array(2 => true),
+            ),
+            array(
+                '<?php $a ++;',
+                array(3 => true),
+            ),
+            array(
+                '<?php $a++ + 1;',
+                array(2 => true, 4 => false),
+            ),
+            array(
+                '<?php ${"a"}++',
+                array(5 => true),
+            ),
+            array(
+                '<?php $foo->bar++',
+                array(4 => true),
+            ),
+            array(
+                '<?php $foo->{"bar"}++',
+                array(6 => true),
+            ),
+            array(
+                '<?php $a["foo"]++',
+                array(5 => true),
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider provideIsUnaryPredecessorOperator
+     */
+    public function testIsUnaryPredecessorOperator($source, array $expected)
+    {
+        $tokens = Tokens::fromCode($source);
+
+        foreach ($expected as $index => $expected) {
+            $this->assertSame($expected, $tokens->isUnaryPredecessorOperator($index));
+            if ($expected) {
+                $this->assertFalse($tokens->isUnarySuccessorOperator($index));
+                $this->assertFalse($tokens->isBinaryOperator($index));
+            }
+        }
+    }
+
+    public function provideIsUnaryPredecessorOperator()
+    {
+        return array(
+            array(
+                '<?php ++$a;',
+                array(1 => true),
+            ),
+            array(
+                '<?php --$a',
+                array(1 => true),
+            ),
+            array(
+                '<?php -- $a;',
+                array(1 => true),
+            ),
+            array(
+                '<?php $a + ++$b;',
+                array(3 => false, 5 => true),
+            ),
+            array(
+                '<?php !!$a;',
+                array(1 => true, 2 => true),
+            ),
+            array(
+                '<?php $a = &$b;',
+                array(5 => true),
+            ),
+            array(
+                '<?php function &foo() {}',
+                array(3 => true),
+            ),
+            array(
+                '<?php @foo();',
+                array(1 => true),
+            ),
+            array(
+                '<?php foo(+ $a, -$b);',
+                array(3 => true, 8 => true),
+            ),
+
+        );
+    }
+
+    /**
+     * @dataProvider provideIsUnaryPredecessorOperator56
+     * @requires PHP 5.6
+     */
+    public function testIsUnaryPredecessorOperator56($source, array $expected)
+    {
+        $tokens = Tokens::fromCode($source);
+
+        foreach ($expected as $index => $expected) {
+            $this->assertSame($expected, $tokens->isUnaryPredecessorOperator($index));
+            if ($expected) {
+                $this->assertFalse($tokens->isUnarySuccessorOperator($index));
+                $this->assertFalse($tokens->isBinaryOperator($index));
+            }
+        }
+    }
+
+    public function provideIsUnaryPredecessorOperator56()
+    {
+        return array(
+            array(
+                '<?php function foo($a, ...$b);',
+                array(8 => true),
+            ),
+            array(
+                '<?php function foo(&...$b);',
+                array(5 => true, 6 => true),
+            ),
+            array(
+                '<?php function foo(array ...$b);',
+                array(7 => true),
+            ),
+            array(
+                '<?php foo(...$a);',
+                array(3 => true),
+            ),
+            array(
+                '<?php foo($a, ...$b);',
+                array(6 => true),
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider provideIsBinaryOperator
+     */
+    public function testIsBinaryOperator($source, array $expected)
+    {
+        $tokens = Tokens::fromCode($source);
+
+        foreach ($expected as $index => $expected) {
+            $this->assertSame($expected, $tokens->isBinaryOperator($index));
+            if ($expected) {
+                $this->assertFalse($tokens->isUnarySuccessorOperator($index));
+                $this->assertFalse($tokens->isUnaryPredecessorOperator($index));
+            }
+        }
+    }
+
+    public function provideIsBinaryOperator()
+    {
+        $cases = array(
+            array(
+                '<?php $a + $b;',
+                array(3 => true),
+            ),
+            array(
+                '<?php 1 + $b;',
+                array(3 => true),
+            ),
+            array(
+                '<?php 0.2 + $b;',
+                array(3 => true),
+            ),
+            array(
+                '<?php $a[1] + $b;',
+                array(6 => true),
+            ),
+            array(
+                '<?php FOO + $b;',
+                array(3 => true),
+            ),
+            array(
+                '<?php foo() + $b;',
+                array(5 => true),
+            ),
+            array(
+                '<?php ${"foo"} + $b;',
+                array(6 => true),
+            ),
+            array(
+                '<?php $a+$b;',
+                array(2 => true),
+            ),
+            array(
+                '<?php $a /* foo */  +  /* bar */  $b;',
+                array(5 => true),
+            ),
+            array(
+                '<?php $a =
+$b;',
+                array(3 => true),
+            ),
+
+            array(
+                '<?php $a
+= $b;',
+                array(3 => true),
+            ),
+            array(
+                '<?php $a = array("b" => "c", );',
+                array(3 => true, 9 => true, 12 => false),
+            ),
+
+            array(
+                '<?php $a * -$b;',
+                array(3 => true, 5 => false),
+            ),
+            array(
+                '<?php $a = -2 / +5;',
+                array(3 => true, 5 => false, 8 => true, 10 => false),
+            ),
+            array(
+                '<?php $a = &$b;',
+                array(3 => true, 5 => false),
+            ),
+            array(
+                '<?php $a++ + $b;',
+                array(2 => false, 4 => true),
+            ),
+            array(
+                '<?php __LINE__ - 1;',
+                array(3 => true),
+            ),
+            array(
+                '<?php `echo 1` + 1;',
+                array(5 => true),
+            ),
+        );
+
+        $operators = array(
+            '+', '-', '*', '/', '%', '<', '>', '|', '^', '&=', '&&', '||', '.=', '/=', '==', '>=', '===', '!=',
+            '<>', '!==', '<=', 'and', 'or', 'xor', '-=', '%=', '*=', '|=', '+=', '<<', '<<=', '>>', '>>=', '^',
+        );
+        foreach ($operators as $operator) {
+            $cases[] = array(
+                '<?php $a '.$operator.' $b;',
+                array(3 => true),
+            );
+        }
+
+        return $cases;
+    }
+
+    /**
+     * @dataProvider provideIsBinaryOperator56
+     * @requires PHP 5.6
+     */
+    public function testIsBinaryOperator56($source, array $expected)
+    {
+        $tokens = Tokens::fromCode($source);
+
+        foreach ($expected as $index => $expected) {
+            $this->assertSame($expected, $tokens->isBinaryOperator($index));
+            if ($expected) {
+                $this->assertFalse($tokens->isUnarySuccessorOperator($index));
+                $this->assertFalse($tokens->isUnaryPredecessorOperator($index));
+            }
+        }
+    }
+
+    public function provideIsBinaryOperator56()
+    {
+        return array(
+            array(
+                '<?php $a ** $b;',
+                array(3 => true),
+            ),
+            array(
+                '<?php $a **= $b;',
+                array(3 => true),
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider provideIsBinaryOperator70
+     * @requires PHP 7.0
+     */
+    public function testIsBinaryOperator70($source, array $expected)
+    {
+        $tokens = Tokens::fromCode($source);
+
+        foreach ($expected as $index => $expected) {
+            $this->assertSame($expected, $tokens->isBinaryOperator($index));
+            if ($expected) {
+                $this->assertFalse($tokens->isUnarySuccessorOperator($index));
+                $this->assertFalse($tokens->isUnaryPredecessorOperator($index));
+            }
+        }
+    }
+
+    public function provideIsBinaryOperator70()
+    {
+        return array(
+            array(
+                '<?php $a <=> $b;',
+                array(3 => true),
+            ),
+            array(
+                '<?php $a ?? $b;',
+                array(3 => true),
             ),
         );
     }
