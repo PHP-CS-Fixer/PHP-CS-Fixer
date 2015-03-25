PHP-CS-Fixer/PHP-CS-Fixer

commit 762f03cf02ab1834e3c961917f4237b491ba48e9

Show progress during fixing

Symfony/CS/Console/Command/FixCommand.php
@@ -16,9 +16,11 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Stopwatch\Stopwatch;
 use Symfony\CS\Fixer;
+use Symfony\CS\FixerFileProcessedEvent;
 use Symfony\CS\FixerInterface;
 use Symfony\CS\Config\Config;
 use Symfony\CS\ConfigInterface;
@@ -30,12 +32,31 @@ use Symfony\CS\StdinFileInfo;
 class FixCommand extends Command
 {
     /**
+     * EventDispatcher instance.
+     *
+     * @var EventDispatcher
+     */
+    protected $eventDispatcher;
+
+    /**
      * Stopwatch instance.
      *
-     * @var \Symfony\Component\Stopwatch\Stopwatch
+     * @var Stopwatch
      */
     protected $stopwatch;
+
+    /**
+     * Fixer instance.
+     *
+     * @var Fixer
+     */
     protected $fixer;
+
+    /**
+     * Config instance.
+     *
+     * @var Config
+     */
     protected $defaultConfig;
 
     /**
@@ -44,6 +65,7 @@ class FixCommand extends Command
      */
     public function __construct(Fixer $fixer = null, ConfigInterface $config = null)
     {
+        $this->eventDispatcher = new EventDispatcher();
         $this->stopwatch = new Stopwatch();
         $this->fixer = $fixer ?: new Fixer();
         $this->fixer->registerBuiltInFixers();
@@ -303,12 +325,30 @@ EOF
         }
 
         $config->fixers($fixers);
+        $verbosity = $output->getVerbosity();
+        $listenForFixerFileProcessedEvent = OutputInterface::VERBOSITY_VERY_VERBOSE <= $verbosity;
+
+        if ($listenForFixerFileProcessedEvent) {
+            $fileProcessedEventListener = function (FixerFileProcessedEvent $event) use ($output) {
+                $output->write($event->getStatusAsString());
+            };
+        }
+
+        if ($listenForFixerFileProcessedEvent) {
+            $this->fixer->setEventDispatcher($this->eventDispatcher);
+            $this->eventDispatcher->addListener(FixerFileProcessedEvent::NAME, $fileProcessedEventListener);
+        }
 
         $this->stopwatch->start('fixFiles');
         $changed = $this->fixer->fix($config, $input->getOption('dry-run'), $input->getOption('diff'));
         $this->stopwatch->stop('fixFiles');
 
-        $verbosity = $output->getVerbosity();
+        if ($listenForFixerFileProcessedEvent) {
+            $this->fixer->setEventDispatcher(null);
+            $this->eventDispatcher->removeListener(FixerFileProcessedEvent::NAME, $fileProcessedEventListener);
+            $output->writeln('');
+        }
+
         $i = 1;
 
         switch ($input->getOption('format')) {
@@ -330,7 +370,7 @@ EOF
                     $output->writeln('');
                 }
 
-                if (OutputInterface::VERBOSITY_VERY_VERBOSE <= $verbosity) {
+                if (OutputInterface::VERBOSITY_DEBUG <= $verbosity) {
                     $output->writeln('Fixing time per file:');
 
                     foreach ($this->stopwatch->getSectionEvents('fixFile') as $file => $event) {
@@ -386,7 +426,7 @@ EOF
                 $timeTotalXML->setAttribute('value', round($fixEvent->getDuration() / 1000, 3));
                 $timeXML->appendChild($timeTotalXML);
 
-                if (OutputInterface::VERBOSITY_VERY_VERBOSE <= $verbosity) {
+                if (OutputInterface::VERBOSITY_DEBUG <= $verbosity) {
                     $timeFilesXML = $dom->createElement('files');
                     $timeXML->appendChild($timeFilesXML);
                     $eventCounter = 1;
@@ -433,7 +473,7 @@ EOF
                     ),
                 );
 
-                if (OutputInterface::VERBOSITY_VERY_VERBOSE <= $verbosity) {
+                if (OutputInterface::VERBOSITY_DEBUG <= $verbosity) {
                     $jFileTime = array();
 
                     foreach ($this->stopwatch->getSectionEvents('fixFile') as $file => $event) {

Symfony/CS/Fixer.php
@@ -12,6 +12,7 @@
 namespace Symfony\CS;
 
 use SebastianBergmann\Diff\Differ;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo as FinderSplFileInfo;
 use Symfony\Component\Stopwatch\Stopwatch;
@@ -19,6 +20,7 @@ use Symfony\CS\Tokenizer\Tokens;
 
 /**
  * @author Fabien Potencier <fabien@symfony.com>
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  */
 class Fixer
 {
@@ -26,12 +28,25 @@ class Fixer
 
     protected $fixers = array();
     protected $configs = array();
+
+    /**
+     * Differ instance.
+     *
+     * @var Differ
+     */
     protected $diff;
+
+    /**
+     * EventDispatcher instance.
+     *
+     * @var EventDispatcher|null
+     */
+    protected $eventDispatcher;
 
     /**
      * Stopwatch instance.
      *
-     * @var \Symfony\Component\Stopwatch\Stopwatch|null
+     * @var Stopwatch|null
      */
     protected $stopwatch;
 
@@ -100,8 +115,8 @@ class Fixer
      * Fixes all files for the given finder.
      *
      * @param ConfigInterface $config A ConfigInterface instance
-     * @param Boolean         $dryRun Whether to simulate the changes or not
-     * @param Boolean         $diff   Whether to provide diff
+     * @param bool            $dryRun Whether to simulate the changes or not
+     * @param bool            $diff   Whether to provide diff
      *
      * @return array
      */
@@ -121,8 +136,16 @@ class Fixer
                 continue;
             }
 
+            if ($this->stopwatch) {
+                $this->stopwatch->start($this->getFileRelativePathname($file));
+            }
+
             if ($fixInfo = $this->fixFile($file, $fixers, $dryRun, $diff, $fileCacheManager)) {
                 $changed[$this->getFileRelativePathname($file)] = $fixInfo;
+            }
+
+            if ($this->stopwatch) {
+                $this->stopwatch->stop($this->getFileRelativePathname($file));
             }
         }
 
@@ -135,17 +158,14 @@ class Fixer
 
     public function fixFile(\SplFileInfo $file, array $fixers, $dryRun, $diff, FileCacheManager $fileCacheManager)
     {
-        $relativePath = $this->getFileRelativePathname($file);
-
-        if ($this->stopwatch) {
-            $this->stopwatch->start($relativePath);
-        }
-
         $new = $old = file_get_contents($file->getRealpath());
 
-        if (!$fileCacheManager->needFixing($relativePath, $old)) {
-            if ($this->stopwatch) {
-                $this->stopwatch->stop($relativePath);
+        if (!$fileCacheManager->needFixing($this->getFileRelativePathname($file), $old)) {
+            if ($this->eventDispatcher) {
+                $this->eventDispatcher->dispatch(
+                    FixerFileProcessedEvent::NAME,
+                    FixerFileProcessedEvent::create()->setStatus(FixerFileProcessedEvent::STATUS_SKIPPED)
+                );
             }
 
             return;
@@ -182,11 +202,14 @@ class Fixer
             }
         }
 
-        if ($this->stopwatch) {
-            $this->stopwatch->stop($relativePath);
-        }
+        $fileCacheManager->setFile($this->getFileRelativePathname($file), $new);
 
-        $fileCacheManager->setFile($relativePath, $new);
+        if ($this->eventDispatcher) {
+            $this->eventDispatcher->dispatch(
+                FixerFileProcessedEvent::NAME,
+                FixerFileProcessedEvent::create()->setStatus($fixInfo ? FixerFileProcessedEvent::STATUS_FIXED : FixerFileProcessedEvent::STATUS_NO_CHANGES)
+            );
+        }
 
         return $fixInfo;
     }
@@ -264,7 +287,22 @@ class Fixer
         return $fixers;
     }
 
-    public function setStopwatch(Stopwatch $stopwatch)
+    /**
+     * Set EventDispatcher instance.
+     *
+     * @param EventDispatcher|null $eventDispatcher
+     */
+    public function setEventDispatcher(EventDispatcher $eventDispatcher = null)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    /**
+     * Set Stopwatch instance.
+     *
+     * @param Stopwatch|null $stopwatch
+     */
+    public function setStopwatch(Stopwatch $stopwatch = null)
     {
         $this->stopwatch = $stopwatch;
     }

Symfony/CS/FixerFileProcessedEvent.php
@@ -1,0 +1,90 @@
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Event that is fired when file was processed by Fixer.
+ *
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ */
+class FixerFileProcessedEvent extends Event
+{
+    /**
+     * Event name.
+     */
+    const NAME = 'fixer.file_processed';
+
+    const STATUS_UNKNOWN = 0;
+    const STATUS_FIXED = 1;
+    const STATUS_NO_CHANGES = 2;
+    const STATUS_SKIPPED = 3;
+
+    /**
+     * File statuses map.
+     *
+     * @var array
+     */
+    private static $statusMap = array(
+        self::STATUS_UNKNOWN    => '?',
+        self::STATUS_FIXED      => 'F',
+        self::STATUS_NO_CHANGES => '.',
+        self::STATUS_SKIPPED    => '',
+    );
+
+    /**
+     * File status.
+     *
+     * @var int
+     */
+    private $status = self::STATUS_UNKNOWN;
+
+    /**
+     * Create instance.
+     *
+     * @return FixerFileProcessedEvent
+     */
+    public static function create()
+    {
+        return new static();
+    }
+
+    /**
+     * Get status.
+     *
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function getStatusAsString()
+    {
+        return self::$statusMap[$this->status];
+    }
+
+    /**
+     * Set status.
+     *
+     * @param int $status
+     *
+     * @return FixerFileProcessedEvent
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}

Symfony/CS/Tests/Fixer/Symfony/PhpdocParamsFixerTest.php
@@ -23,7 +23,7 @@ class PhpdocParamsFixerTest extends AbstractFixerTestBase
      * @param EngineInterface $templating
      * @param string          $format
      * @param integer         $code       An HTTP response status code
-     * @param Boolean         $debug
+     * @param bool            $debug
      * @param mixed           &$reference A parameter passed by reference
      */
 
@@ -35,7 +35,7 @@ EOF;
      * @param  EngineInterface $templating
      * @param string      $format
      * @param  integer  $code       An HTTP response status code
-     * @param    Boolean      $debug
+     * @param    bool         $debug
      * @param  mixed    &$reference     A parameter passed by reference
      */
 
@@ -53,8 +53,8 @@ EOF;
      * @param string          $format
      * @param integer         $code       An HTTP response status code
      *                                    See constants
-     * @param Boolean         $debug
-     * @param Boolean         $debug      See constants
+     * @param bool            $debug
+     * @param bool            $debug      See constants
      *                                    See constants
      * @param mixed           &$reference A parameter passed by reference
      */
@@ -68,8 +68,8 @@ EOF;
      * @param string      $format
      * @param  integer  $code       An HTTP response status code
      *                              See constants
-     * @param    Boolean      $debug
-     * @param    Boolean      $debug See constants
+     * @param    bool         $debug
+     * @param    bool         $debug See constants
      * See constants
      * @param  mixed    &$reference     A parameter passed by reference
      */
@@ -88,8 +88,8 @@ EOF;
      * @param string          $format
      * @param integer         $code       An HTTP response status code
      *                                    See constants
-     * @param Boolean         $debug
-     * @param Boolean         $debug      See constants
+     * @param bool            $debug
+     * @param bool            $debug      See constants
      *                                    See constants
      * @param mixed           &$reference A parameter passed by reference
      *
@@ -108,8 +108,8 @@ EOF;
      * @param string      $format
      * @param  integer  $code       An HTTP response status code
      *                              See constants
-     * @param    Boolean      $debug
-     * @param    Boolean      $debug See constants
+     * @param    bool         $debug
+     * @param    bool         $debug See constants
      * See constants
      * @param  mixed    &$reference     A parameter passed by reference
      *

commit 6fab8663592c9ed996b7b848abfba785f751c68d

Updated Symfony/Console to 2.3 - Symfony/Console 2.1 BC was broken in 762f03cf02ab1834e3c961917f4237b491ba48e9 and 833d8dfa47a8e7b07d8e225d8a8e8bc69a8dad2d - Skip ReadmeTest if Symfony/Console is less than 2.4 - Replaced isVerbose in FixCommand

Symfony/CS/Console/Command/FixCommand.php
@@ -339,6 +339,8 @@ EOF
             $config->setDir($path);
         }
 
+        $verbosity = $output->getVerbosity();
+
         // register custom fixers from config
         $this->fixer->registerCustomFixers($config->getCustomFixers());
 
@@ -349,7 +351,7 @@ EOF
             ->setOptions(array(
                 'level'     => $input->getOption('level'),
                 'fixers'    => $input->getOption('fixers'),
-                'progress'  => $output->isVerbose() && 'txt' === $input->getOption('format'),
+                'progress'  => (OutputInterface::VERBOSITY_VERBOSE <= $verbosity) && 'txt' === $input->getOption('format'),
             ))
             ->resolve();
 
@@ -384,7 +386,6 @@ EOF
             $output->writeln('Legend: '.implode(', ', array_unique($legend)));
         }
 
-        $verbosity = $output->getVerbosity();
         $i = 1;
 
         switch ($input->getOption('format')) {

Symfony/CS/Tests/ReadmeTest.php
@@ -22,6 +22,10 @@ class ReadmeTest extends \PHPUnit_Framework_TestCase
 {
     public function testIfReadmeFileIsCorrect()
     {
+        if (!class_exists('Symfony\Component\Console\Output\BufferedOutput')) {
+            $this->markTestSkipped('Unsupported symfony/console version, Symfony\Component\Console\Output\BufferedOutput was added in 2.4.');
+        }
+
         $input = new ArrayInput(array('readme'));
         $output = new BufferedOutput();
         $app = new Application();