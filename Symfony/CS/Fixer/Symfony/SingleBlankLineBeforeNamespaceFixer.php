95ba8a3812743757094ebe30e8f9e2d8a86ac461
Vladimir Reznichenko <kalessil@gmail.com>
2015-03-28 19:21:10 +0100

    code grooming

--- Symfony/CS/Fixer/Symfony/SingleBlankLineBeforeNamespaceFixer.php
+++ Symfony/CS/Fixer/Symfony/SingleBlankLineBeforeNamespaceFixer.php
@@ -26,8 +26,10 @@ class SingleBlankLineBeforeNamespaceFixer extends AbstractLinesBeforeNamespaceFi
     {
         $tokens = Tokens::fromCode($content);
 
-        foreach (array_keys($tokens->findGivenKind(T_NAMESPACE)) as $index) {
-            $this->fixLinesBeforeNamespace($tokens, $index, 2);
+        foreach ($tokens as $index => $token) {
+            if ($token->isGivenKind(T_NAMESPACE)) {
+                $this->fixLinesBeforeNamespace($tokens, $index, 2);
+            }
         }
 
         return $tokens->generateCode();
