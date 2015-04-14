ef5358165a72dbfe6e95b4793a4a41bfae7bbb4a
Falkirks <bpnoreply@gmail.com>
2015-04-14 15:51:49 -0700

    Grammar fix to README

--- Symfony/CS/Fixer/PSR2/ElseifFixer.php
+++ Symfony/CS/Fixer/PSR2/ElseifFixer.php
@@ -66,6 +66,6 @@ class ElseifFixer extends AbstractFixer
      */
     public function getDescription()
     {
-        return 'The keyword elseif should be used instead of else if so that all control keywords looks like single words.';
+        return 'The keyword elseif should be used instead of else if so that all control keywords look like single words.';
     }
 }
