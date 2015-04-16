6425aeb97ab921371182712a18c280d546e7769b
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2015-04-16 09:21:30 +0200

    prepared the 1.7 release

--- Symfony/CS/Fixer.php
+++ Symfony/CS/Fixer.php
@@ -24,7 +24,7 @@ use Symfony\CS\Tokenizer\Tokens;
  */
 class Fixer
 {
-    const VERSION = '1.7-DEV';
+    const VERSION = '1.7';
 
     protected $fixers = array();
     protected $configs = array();
