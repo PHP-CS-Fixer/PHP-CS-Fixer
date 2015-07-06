47897be1c87597b5eabfd39815bc02e1d87f4894
Ivan Scherbak <dev@funivan.com>
2015-07-06 12:36:07 +0300

    fix: #1270 fix phpdoc

--- Symfony/CS/Tests/FixerFileProcessedEvent/FixerFileProcessedEventTest.php
+++ Symfony/CS/Tests/FixerFileProcessedEvent/FixerFileProcessedEventTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the PHP CS utility.
  *
- * (c) Ivan Scherbak <dev@funivan.com>
+ * (c) Fabien Potencier <fabien@symfony.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
