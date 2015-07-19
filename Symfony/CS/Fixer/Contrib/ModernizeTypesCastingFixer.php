a2b8dc62c13a0e55eb13e75e225b8f42874cb547
Vladimir Reznichenko <kalessil@gmail.com>
2015-07-19 18:26:41 +0200

    ModernizeTypesCastingFixer: correct invariants - static variable

--- Symfony/CS/Fixer/Contrib/ModernizeTypesCastingFixer.php
+++ Symfony/CS/Fixer/Contrib/ModernizeTypesCastingFixer.php
@@ -20,13 +20,6 @@ use Symfony\CS\Tokenizer\Tokens;
  */
 final class ModernizeTypesCastingFixer extends AbstractFixer
 {
-    private static $sequencesInvariants = array(
-        T_INT_CAST => array(array(T_STRING, 'intval'), '('),
-        T_DOUBLE_CAST => array(array(T_STRING, 'floatval'), '('),
-        T_STRING_CAST => array(array(T_STRING, 'strval'), '('),
-        T_BOOL_CAST => array(array(T_STRING, 'boolval'), '('),
-    );
-
     /**
      * {@inheritdoc}
      */
@@ -40,6 +33,13 @@ final class ModernizeTypesCastingFixer extends AbstractFixer
      */
     public function fix(\SplFileInfo $file, Tokens $tokens)
     {
+        static $sequencesInvariants = array(
+            T_INT_CAST => array(array(T_STRING, 'intval'), '('),
+            T_DOUBLE_CAST => array(array(T_STRING, 'floatval'), '('),
+            T_STRING_CAST => array(array(T_STRING, 'strval'), '('),
+            T_BOOL_CAST => array(array(T_STRING, 'boolval'), '('),
+        );
+
         $castingTokens = array(
             T_INT_CAST => new Token(array(T_INT_CAST, '(int)')),
             T_DOUBLE_CAST => new Token(array(T_DOUBLE_CAST, '(float)')),
@@ -47,7 +47,7 @@ final class ModernizeTypesCastingFixer extends AbstractFixer
             T_BOOL_CAST => new Token(array(T_BOOL_CAST, '(bool)')),
         );
 
-        foreach (self::$sequencesInvariants as $operator => $sequenceNeeded) {
+        foreach ($sequencesInvariants as $operator => $sequenceNeeded) {
             $currIndex = 0;
             while (null !== $currIndex) {
                 $matches = $tokens->findSequence($sequenceNeeded, $currIndex, $tokens->count() - 1, false);
