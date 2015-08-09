de8c76a80f86e7213f6aaf78aacfd5cd4b197b19
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2015-08-09 23:34:44 +0200

    implement fixer

--- Symfony/CS/Fixer/Symfony/MethodTypehintSpace.php
+++ Symfony/CS/Fixer/Symfony/MethodTypehintSpace.php
@@ -12,6 +12,7 @@
 namespace Symfony\CS\Fixer\Symfony;
 
 use Symfony\CS\AbstractFixer;
+use Symfony\CS\Tokenizer\Token;
 use Symfony\CS\Tokenizer\Tokens;
 
 /**
@@ -25,6 +26,26 @@ class MethodTypehintSpace extends AbstractFixer
     public function fix(\SplFileInfo $file, $content)
     {
         $tokens = Tokens::fromCode($content);
+
+        for ($index = $tokens->count() - 1; $index >= 0; --$index) {
+            $token = $tokens[$index];
+
+            if (!$token->isGivenKind(T_FUNCTION)) {
+                continue;
+            }
+
+            $startParenthesisIndex = $tokens->getNextTokenOfKind($index, array('('));
+            $endParenthesisIndex = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_PARENTHESIS_BRACE, $startParenthesisIndex);
+
+            for ($iter = $endParenthesisIndex - 1; $iter > $startParenthesisIndex; --$iter) {
+                if (
+                    $tokens[$iter]->isGivenKind(T_VARIABLE) &&
+                    !$tokens[$iter - 1]->equalsAny(array(array(T_WHITESPACE), array(T_COMMENT), array(T_DOC_COMMENT), '('))
+                ) {
+                    $tokens->insertAt($iter, new Token(array(T_WHITESPACE, ' ', $tokens[$iter]->getLine())));
+                }
+            }
+        }
 
         return $tokens->generateCode();
     }
