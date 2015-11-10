89202dc9ca398fac28d02599d56e4cf60f5da67b
Lucas Manzke <lucas.manzke@spryker.com>
2015-11-10 02:10:05 +0100

    improve/add test cases

--- Symfony/CS/Tests/Fixer/Contrib/MethodArgumentDefaultValueFixerTest.php
+++ Symfony/CS/Tests/Fixer/Contrib/MethodArgumentDefaultValueFixerTest.php
@@ -31,27 +31,46 @@ final class MethodArgumentDefaultValueFixerTest extends AbstractFixerTestBase
      */
     public function provideExamples()
     {
-        $input1 = <<<'EOT'
-                    <?php
-                        public function eFunction($foo, $bar, \SplFileInfo $baz, $x = 'default';
-
-                        public function fFunction($foo, $bar = 'removedValue', \SplFileInfo $baz, $x = 'default');
-EOT;
-
-        $expected1 = <<<'EOT'
-                    <?php
-                        public function eFunction($foo, $bar, \SplFileInfo $baz, $x = 'default';
-
-                        public function fFunction($foo, $bar, \SplFileInfo $baz, $x = 'default');
-EOT;
-
         return array(
-            array('<?php public function bFunction($foo, $bar)', '<?php public function bFunction($foo = null, $bar)'),
-            array('<?php public function cFunction($foo, $bar, $baz)', '<?php public function cFunction($foo = false, $bar = \'bar\', $baz)'),
-            array('<?php public function dFunction($foo, $bar, $baz)', '<?php public function dFunction($foo = false, $bar, $baz)'),
-            array('<?php public function eFunction($foo, $bar, \SplFileInfo $baz, $x)', '<?php public function eFunction($foo = PHP_EOL, $bar, \SplFileInfo $baz = null, $x)'),
-            array('<?php public function eFunction($foo, $bar, \SplFileInfo $baz, $x = \'default\')', '<?php public function eFunction($foo, $bar = \'removedDefault\', \SplFileInfo $baz, $x = \'default\')'),
-            array($expected1, $input1),
+            array(
+                '<?php public function bFunction($foo, $bar) {}',
+                '<?php public function bFunction($foo = null, $bar) {}'
+            ),
+            array(
+                '<?php public function cFunction($foo, $bar, $baz) {}',
+                '<?php public function cFunction($foo = false, $bar = \'bar\', $baz) {}'
+            ),
+            array(
+                '<?php public function dFunction($foo, $bar, $baz) {}',
+                '<?php public function dFunction($foo = false, $bar, $baz) {}'
+            ),
+            array(
+                '<?php public function eFunction($foo, $bar, \SplFileInfo $baz, $x) {}',
+                '<?php public function eFunction($foo = PHP_EOL, $bar, \SplFileInfo $baz = null, $x) {}'
+            ),
+            array(
+                '<?php public function eFunction($foo, $bar, \SplFileInfo $baz, $x = \'default\') {}',
+                '<?php public function eFunction($foo, $bar = \'removedDefault\', \SplFileInfo $baz, $x = \'default\') {}'
+            ),
+            array(
+                <<<'EOT'
+                    <?php
+                        public function eFunction($foo, $bar, \SplFileInfo $baz, $x = 'default' {};
+
+                        public function fFunction($foo, $bar, \SplFileInfo $baz, $x = 'default') {};
+EOT
+                ,
+                <<<'EOT'
+                    <?php
+                        public function eFunction($foo, $bar, \SplFileInfo $baz, $x = 'default' {};
+
+                        public function fFunction($foo, $bar = 'removedValue', \SplFileInfo $baz, $x = 'default') {};
+EOT
+            ),
+            array(
+                '<?php function foo ($bar, $c) {}',
+                '<?php function foo ($bar /* a */ = /* b */ 1, $c) {}',
+            )
         );
     }
 }
