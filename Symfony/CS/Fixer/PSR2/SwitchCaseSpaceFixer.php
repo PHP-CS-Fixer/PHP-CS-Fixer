4887efd6d7f74eccda0c9c196c6f3bf41f495396
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-11-25 17:24:54 +0100

    [PSR-2] Switch case space fixer

---
+++ Symfony/CS/Fixer/PSR2/SwitchCaseSpaceFixer.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfony\CS\Fixer\PSR2;
+
+use Symfony\CS\AbstractFixer;
+use Symfony\CS\Tokenizer\Tokens;
+
+/**
+ * @author Sullivan Senechal <soullivaneuh@gmail.com>
+ */
+final class SwitchCaseSpaceFixer extends AbstractFixer
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function fix(\SplFileInfo $file, $content)
+    {
+        $tokens = Tokens::fromCode($content);
+
+        foreach ($tokens as $index => $token) {
+            if (!$token->isGivenKind(array(T_CASE, T_DEFAULT))) {
+                continue;
+            }
+
+            $ternariesCount = 0;
+            for ($colonIndex = $index + 1;; ++$colonIndex) {
+                // We have to skip ternary case for colons.
+                if ($tokens[$colonIndex]->equals('?')) {
+                    ++$ternariesCount;
+                }
+
+                if ($tokens[$colonIndex]->equals(':') && 0 === $ternariesCount) {
+                    break;
+                }
+
+                if ($tokens[$colonIndex]->equals(':')) {
+                    --$ternariesCount;
+                }
+            }
+
+            $valueIndex = $tokens->getPrevNonWhitespace($colonIndex);
+            if (2 + $valueIndex === $colonIndex) {
+                $tokens[$valueIndex + 1]->clear();
+            }
+        }
+
+        return $tokens->generateCode();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription()
+    {
+        return 'Removes extra spaces between colon and case value.';
+    }
+}
