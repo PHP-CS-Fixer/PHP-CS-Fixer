ed459179d96f5ff665b21cb8e8932e5a41ea89d7
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-11-26 11:19:38 +0100

    CS

--- Symfony/CS/Fixer/Contrib/PrintEchoParenthesesFixer.php
+++ Symfony/CS/Fixer/Contrib/PrintEchoParenthesesFixer.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PHP CS utility.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Symfony\CS\Fixer\Contrib;
 
 use Symfony\CS\AbstractFixer;
@@ -18,7 +27,7 @@ final class PrintEchoParenthesesFixer extends AbstractFixer
         $tokens = Tokens::fromCode($content);
 
         foreach ($tokens as $index => $token) {
-            if (!$token->isGivenKind([T_PRINT, T_ECHO])) {
+            if (!$token->isGivenKind(array(T_PRINT, T_ECHO))) {
                 continue;
             }
 
