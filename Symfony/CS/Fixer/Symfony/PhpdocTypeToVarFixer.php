bc7834ecfe3a5f4ead64c41b05904d6fba58d858
Graham Campbell <graham@alt-three.com>
2015-11-29 19:48:48 +0000

    Added PhpdocPropertyFixer, refactored Tag and Annotation

--- Symfony/CS/Fixer/Symfony/PhpdocTypeToVarFixer.php
+++ Symfony/CS/Fixer/Symfony/PhpdocTypeToVarFixer.php
@@ -11,48 +11,22 @@
 
 namespace Symfony\CS\Fixer\Symfony;
 
-use Symfony\CS\AbstractFixer;
-use Symfony\CS\DocBlock\DocBlock;
-use Symfony\CS\Tokenizer\Tokens;
+use Symfony\CS\AbstractPhpdocTagsFixer;
 
 /**
  * @author Graham Campbell <graham@mineuk.com>
  */
-final class PhpdocTypeToVarFixer extends AbstractFixer
+final class PhpdocTypeToVarFixer extends AbstractPhpdocTagsFixer
 {
     /**
      * {@inheritdoc}
      */
-    public function isCandidate(Tokens $tokens)
-    {
-        return $tokens->isTokenKindFound(T_DOC_COMMENT);
-    }
+    protected static $search = array('type');
 
     /**
      * {@inheritdoc}
      */
-    public function fix(\SplFileInfo $file, Tokens $tokens)
-    {
-        foreach ($tokens as $token) {
-            if (!$token->isGivenKind(T_DOC_COMMENT)) {
-                continue;
-            }
-
-            $doc = new DocBlock($token->getContent());
-            $annotations = $doc->getAnnotationsOfType('type');
-
-            if (empty($annotations)) {
-                continue;
-            }
-
-            foreach ($annotations as $annotation) {
-                $line = $doc->getLine($annotation->getStart());
-                $line->setContent(str_replace('@type', '@var', $line->getContent()));
-            }
-
-            $token->setContent($doc->getContent());
-        }
-    }
+    protected static $replace = 'var';
 
     /**
      * {@inheritdoc}
