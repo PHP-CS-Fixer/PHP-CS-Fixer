784f02feaff327836cb91a8543546507d7694148
Possum <possumfromspace@gmail.com>
2016-01-19 09:54:14 +0100

    Make EmptyReturnFixer indep. from lowercase_constants fixer.

--- Symfony/CS/Fixer/Symfony/EmptyReturnFixer.php
+++ Symfony/CS/Fixer/Symfony/EmptyReturnFixer.php
@@ -64,7 +64,7 @@ class EmptyReturnFixer extends AbstractFixer
         $content = ltrim($content, '(');
         $content = rtrim($content, ')');
 
-        return 'null' === $content;
+        return 'null' === strtolower($content);
     }
 
     /**
