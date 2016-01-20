2c9f8298181f059c5077abda78019b9a0c9a7cc0
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2016-01-20 20:00:28 +0100

    prepared the 1.11.1 release

--- Symfony/CS/Fixer.php
+++ Symfony/CS/Fixer.php
@@ -24,7 +24,7 @@ use Symfony\CS\Tokenizer\Tokens;
  */
 class Fixer
 {
-    const VERSION = '1.11.1-DEV';
+    const VERSION = '1.11.1';
 
     protected $fixers = array();
     protected $configs = array();
