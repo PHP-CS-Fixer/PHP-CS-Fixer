146a89190acda8e0062fb5450d2c1fb5ac6cf737
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2016-02-01 00:44:22 +0100

    Update authors

--- Symfony/CS/Console/Command/FixCommand.php
+++ Symfony/CS/Console/Command/FixCommand.php
@@ -1,9 +1,10 @@
 <?php
 
 /*
- * This file is part of the PHP CS utility.
+ * This file is part of the PHP CS Fixer.
  *
  * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
@@ -34,6 +35,7 @@ use Symfony\CS\Utils;
 
 /**
  * @author Fabien Potencier <fabien@symfony.com>
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  */
 class FixCommand extends Command
 {
--- Symfony/CS/Tests/Console/ApplicationTest.php
+++ Symfony/CS/Tests/Console/ApplicationTest.php
@@ -1,15 +1,16 @@
 <?php
 
 /*
- * This file is part of the PHP CS utility.
+ * This file is part of the PHP CS Fixer.
  *
  * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
 
-namespace Symfony\CS\Tests;
+namespace Symfony\CS\Tests\Console;
 
 use Symfony\CS\Console\Application;
 
@@ -21,6 +22,6 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
     public function testApplication()
     {
         $app = new Application();
-        $this->assertStringMatchesFormat('<info>PHP CS Fixer</info> version <comment>%d.%d%s</comment> by <comment>Fabien Potencier</comment>', $app->getLongVersion());
+        $this->assertStringMatchesFormat('<info>PHP CS Fixer</info> version <comment>%d.%d%s</comment> by <comment>Fabien Potencier</comment> and <comment>Dariusz Ruminski</comment>', $app->getLongVersion());
     }
 }
