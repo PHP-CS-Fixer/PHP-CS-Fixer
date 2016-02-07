e07bfcd32f09f686daa72eb7906dbeb375315580
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2016-02-07 23:25:46 +0100

    Finder - clean up code

--- src/Finder.php
+++ src/Finder.php
@@ -16,7 +16,7 @@ use Symfony\Component\Finder\Finder as BaseFinder;
 
 /**
  * @author Fabien Potencier <fabien@symfony.com>
- * @author Dariusz Rumiñski <dariusz.ruminski@gmail.com>
+ * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  */
 class Finder extends BaseFinder implements FinderInterface
 {
@@ -33,44 +33,10 @@ class Finder extends BaseFinder implements FinderInterface
             ->ignoreVCS(true)
             ->exclude('vendor')
         ;
-
-        $files = $this->getFilesToExclude();
-
-        if (!empty($files)) {
-            $this->filter(
-                function (\SplFileInfo $file) use ($files) {
-                    return !in_array($file->getRelativePathname(), $files, true);
-                }
-            );
-        }
     }
 
     public function setDir($dir)
     {
-        $this->in($this->getDirs($dir));
-    }
-
-    /**
-     * Gets the directories that needs to be scanned for files to validate.
-     *
-     * @param string $dir
-     *
-     * @return string[]
-     */
-    protected function getDirs($dir)
-    {
-        return array($dir);
-    }
-
-    /**
-     * Excludes files because modifying them would break.
-     *
-     * This is mainly useful for fixtures in unit tests.
-     *
-     * @return string[]
-     */
-    protected function getFilesToExclude()
-    {
-        return array();
+        $this->in($dir);
     }
 }
