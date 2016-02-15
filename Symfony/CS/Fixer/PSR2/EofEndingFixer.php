a24968889ef61edf1545ef70daeb63730af17462
Filippo Tessarotto <zoeslam@gmail.com>
2016-02-15 12:26:20 +0100

    EofEndingFixer - new line on end line comment is allowed

--- Symfony/CS/Fixer/PSR2/EofEndingFixer.php
+++ Symfony/CS/Fixer/PSR2/EofEndingFixer.php
@@ -37,9 +37,25 @@ class EofEndingFixer extends AbstractFixer
             return $content;
         }
 
+        $isSingleLineComment = function (Token $token) {
+            return $token->isComment() && '/*' !== substr($token->getContent(), 0, 2);
+        };
+        $clearSingleLineComment = function (Token $token) {
+            $content = $token->getContent();
+            $content = rtrim($content, "\n")."\n";
+            $token->setContent($content);
+        };
+
         if ($token->isWhitespace()) {
-            $lineBreak = false === strrpos($token->getContent(), "\r") ? "\n" : "\r\n";
-            $token->setContent($lineBreak);
+            if ($count > 1 && $isSingleLineComment($tokens[$count - 2])) {
+                $clearSingleLineComment($tokens[$count - 2]);
+                $token->clear();
+            } else {
+                $lineBreak = false === strrpos($token->getContent(), "\r") ? "\n" : "\r\n";
+                $token->setContent($lineBreak);
+            }
+        } elseif ($isSingleLineComment($token)) {
+            $clearSingleLineComment($token);
         } else {
             $tokens->insertAt($count, new Token(array(T_WHITESPACE, "\n")));
         }
