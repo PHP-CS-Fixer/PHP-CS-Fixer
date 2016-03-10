85b0e223d4a3a2b9cbeee3c599cb6b4110cfa27a
SpacePossum <possumfromspace@gmail.com>
2016-03-10 22:31:22 +0100

    Fix priority issue.

--- Symfony/CS/Tests/FixerTest.php
+++ Symfony/CS/Tests/FixerTest.php
@@ -208,6 +208,7 @@ class FixerTest extends \PHPUnit_Framework_TestCase
             array($fixers['short_echo_tag'], $fixers['echo_to_print']), // tested also in: echo_to_print,short_echo_tag.test
             array($fixers['short_bool_cast'], $fixers['spaces_cast']),
             array($fixers['unneeded_control_parentheses'], $fixers['trailing_spaces']), // tested also in: trailing_spaces,unneeded_control_parentheses.test
+            array($fixers['blankline_after_open_tag'], $fixers['no_blank_lines_before_namespace']), // tested also in: blankline_after_open_tag,no_blank_lines_before_namespace.test
         );
 
         $docFixerNames = array_filter(
