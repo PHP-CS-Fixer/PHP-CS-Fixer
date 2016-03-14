009072e8fdf3645e3e504e79bb7cc8c98f427889
SpacePossum <possumfromspace@gmail.com>
2016-03-14 12:16:43 +0100

    process feedback

--- Symfony/CS/Fixer/Symfony/NoEmptyStatementFixer.php
+++ Symfony/CS/Fixer/Symfony/NoEmptyStatementFixer.php
@@ -28,7 +28,6 @@ final class NoEmptyStatementFixer extends AbstractFixer
     {
         $tokens = Tokens::fromCode($content);
         for ($index = 0, $count = $tokens->count(); $index < $count; ++$index) {
-
             // skip T_FOR parenthesis to ignore duplicated `;` like `for ($i = 1; ; ++$i) {...}`
             if ($tokens[$index]->isGivenKind(T_FOR)) {
                 $index = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_PARENTHESIS_BRACE, $tokens->getNextMeaningfulToken($index)) + 1;
@@ -42,10 +41,7 @@ final class NoEmptyStatementFixer extends AbstractFixer
             $previousMeaningfulIndex = $tokens->getPrevMeaningfulToken($index);
 
             // A semicolon can always be removed if it follows a semicolon, '{' or opening tag.
-            if (
-                $tokens[$previousMeaningfulIndex]->equalsAny(array('{', ';')) ||
-                $tokens[$previousMeaningfulIndex]->isGivenKind(array(T_OPEN_TAG))
-            ) {
+            if ($tokens[$previousMeaningfulIndex]->equalsAny(array('{', ';', array(T_OPEN_TAG)))) {
                 $tokens->clearTokenAndMergeSurroundingWhitespace($index);
                 continue;
             }
