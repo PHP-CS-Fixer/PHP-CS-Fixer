ca6fb6f034d86108fc3be3b41e30097799d01629
Graham Campbell <graham@alt-three.com>
2016-03-26 22:49:37 +0000

    Revert bad change

--- tests/Runner/RunnerTest.php
+++ tests/Runner/RunnerTest.php
@@ -16,6 +16,7 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Differ\NullDiffer;
 use PhpCsFixer\Error\Error;
 use PhpCsFixer\Error\ErrorsManager;
+use PhpCsFixer\Fixer;
 use PhpCsFixer\Linter\Linter;
 use PhpCsFixer\Linter\NullLinter;
 use PhpCsFixer\Runner\Runner;
