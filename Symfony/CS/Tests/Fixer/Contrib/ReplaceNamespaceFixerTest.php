455adbd8614e9f869284fb4b091c7f25e14612e5
Aschwin Wesselius <aschwin.wesselius@centralpoint.eu>
2016-04-29 14:56:50 +0200

    Removing unneeded duplicate code

--- Symfony/CS/Tests/Fixer/Contrib/ReplaceNamespaceFixerTest.php
+++ Symfony/CS/Tests/Fixer/Contrib/ReplaceNamespaceFixerTest.php
@@ -148,44 +148,4 @@ use MessageBird\Objects\Verify;
 
         return $files[$filename];
     }
-
-//    protected function makeTest($expected, $input = null, \SplFileInfo $file = null, FixerInterface $fixer = null)
-//    {
-//        if ($expected === $input) {
-//            throw new \InvalidArgumentException('Input parameter must not be equal to expected parameter.');
-//        }
-//
-//        $fixer = $fixer ?: $this->getFixer();
-//        $file = $file ?: $this->getTestFile();
-//        $fileIsSupported = $fixer->supports($file);
-//
-//        if (null !== $input) {
-//            $fixedCode = $fileIsSupported ? $fixer->fix($file, $input) : $input;
-//
-//            $this->assertSame($expected, $fixedCode);
-//
-//            $tokens = Tokens::fromCode($fixedCode); // Load cached collection (used by the fixer)
-//            Tokens::clearCache();
-//            $expectedTokens = Tokens::fromCode($fixedCode); // Load the expected collection based on PHP parsing
-//            $this->assertTokens($expectedTokens, $tokens);
-//        }
-//
-//        $this->assertSame($expected, $fileIsSupported ? $fixer->fix($file, $expected) : $expected);
-//    }
-
-//    private function assertTokens(Tokens $expectedTokens, Tokens $tokens)
-//    {
-        //        foreach ($expectedTokens as $index => $expectedToken) {
-//            $token = $tokens[$index];
-//
-//            $expectedPrototype = $expectedToken->getPrototype();
-//            if (is_array($expectedPrototype)) {
-//                unset($expectedPrototype[2]); // don't compare token lines as our token mutations don't deal with line numbers
-//            }
-//
-//            $this->assertTrue($token->equals($expectedPrototype), sprintf('The token at index %d should be %s, got %s', $index, json_encode($expectedPrototype), $token->toJson()));
-//        }
-
-//        $this->assertSame($expectedTokens->count(), $tokens->count(), 'The collection should have the same length than the expected one');
-//    }
 }
