2d47f02eea26386deef3e44cc5630e404fef9adb
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2016-07-04 22:19:11 +0200

    Update PhpdocAnnotationWithoutDotFixerTest.php

--- Symfony/CS/Tests/Fixer/Symfony/PhpdocAnnotationWithoutDotFixerTest.php
+++ Symfony/CS/Tests/Fixer/Symfony/PhpdocAnnotationWithoutDotFixerTest.php
@@ -66,8 +66,8 @@ class PhpdocAnnotationWithoutDotFixerTest extends AbstractFixerTestBase
                 // invalid char inside line won't crash the fixer
                 '<?php
     /**
-     * @var string This: '.chr(174).' is and odd character.
-     * @var string This: '.chr(174).' is and odd character 2nd time。
+     * @var string This: '.chr(174).' is an odd character.
+     * @var string This: '.chr(174).' is an odd character 2nd time。
      */',
             ),
         );
