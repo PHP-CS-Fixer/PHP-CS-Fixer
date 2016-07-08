734e58b74f2ba2ab9a66122a6c0b598edd6f9833
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2016-07-09 01:52:24 +0200

    bumped version

--- Symfony/CS/Fixer.php
+++ Symfony/CS/Fixer.php
@@ -24,7 +24,7 @@ use Symfony\CS\Tokenizer\Tokens;
  */
 class Fixer
 {
-    const VERSION = '1.11.5';
+    const VERSION = '1.11.6-DEV';
 
     protected $fixers = array();
     protected $configs = array();
