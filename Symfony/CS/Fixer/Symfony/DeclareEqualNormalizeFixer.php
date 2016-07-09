c802135975b75673b0dec170ee6212d0c1ee12ee
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2016-07-09 23:07:25 +0200

    Update DeclareEqualNormalizeFixer.php

--- Symfony/CS/Fixer/Symfony/DeclareEqualNormalizeFixer.php
+++ Symfony/CS/Fixer/Symfony/DeclareEqualNormalizeFixer.php
@@ -18,7 +18,7 @@ use Symfony\CS\Tokenizer\Tokens;
 /**
  * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  */
-class DeclareEqualNormalizeFixer extends AbstractFixer
+final class DeclareEqualNormalizeFixer extends AbstractFixer
 {
     /**
      * {@inheritdoc}
