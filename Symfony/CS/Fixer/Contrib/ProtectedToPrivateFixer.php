edf7a7c31ee81b14f330449ccfd457c6bbb01376
Filippo Tessarotto <zoeslam@gmail.com>
2016-07-19 17:06:02 +0200

    ProtectedToPrivateFixer: missin param doc

--- Symfony/CS/Fixer/Contrib/ProtectedToPrivateFixer.php
+++ Symfony/CS/Fixer/Contrib/ProtectedToPrivateFixer.php
@@ -67,6 +67,7 @@ final class ProtectedToPrivateFixer extends AbstractFixer
      * Decide whether or not skip the fix for given class.
      *
      * @param Tokens $tokens the Tokens instance
+     * @param int    $classIndex the class start index
      *
      * @return bool
      */
