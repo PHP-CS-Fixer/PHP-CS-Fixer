20a1aadb9d1d72b01bcbccfcf8984c4e89d15959
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2016-10-27 13:52:22 +0200

    Update TextReporter.php

--- src/Report/TextReporter.php
+++ src/Report/TextReporter.php
@@ -115,7 +115,7 @@ final class TextReporter implements ReporterInterface
      */
     private function getFooter($time, $memory, $isDryRun)
     {
-        if ($time === 0 || $memory === 0) {
+        if (0 === $time || 0 === $memory) {
             return '';
         }
 
