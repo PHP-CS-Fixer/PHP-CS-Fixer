f3baf72eb2f58bf275b372540f5b47d25aed910f
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2016-12-01 07:18:06 +0100

    prepared the 2.0.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -26,7 +26,7 @@ use Symfony\Component\Console\Application as BaseApplication;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.0.0-DEV';
+    const VERSION = '2.0.0';
 
     /**
      * Constructor.
