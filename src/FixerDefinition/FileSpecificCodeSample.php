700dc3eb66380820c98b98511a6eec84cb99b2d1
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2017-02-06 12:22:09 +0100

    Update FileSpecificCodeSample.php

--- src/FixerDefinition/FileSpecificCodeSample.php
+++ src/FixerDefinition/FileSpecificCodeSample.php
@@ -30,9 +30,9 @@ final class FileSpecificCodeSample implements FileSpecificCodeSampleInterface
     private $splFileInfo;
 
     /**
-     * @param string      $code
+     * @param string       $code
      * @param \SplFileInfo $splFileInfo
-     * @param null|array  $configuration
+     * @param null|array   $configuration
      */
     public function __construct(
         $code,
