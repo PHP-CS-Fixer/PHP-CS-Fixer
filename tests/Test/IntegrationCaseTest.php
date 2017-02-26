0dfd494bea099d93c381de87b0d28160bb99c926
Julien Falque <julien.falque@gmail.com>
2017-02-26 22:29:09 +0100

    Add deprecation notices tests

---
+++ tests/Test/IntegrationCaseTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Tests\Test;
+
+use PhpCsFixer\RuleSet;
+use PhpCsFixer\Test\IntegrationCase;
+
+/**
+ * @internal
+ */
+final class IntegrationCaseTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @group legacy
+     * @expectedDeprecation The "PhpCsFixer\Test\IntegrationCase::shouldCheckPriority" method is deprecated. You should stop using it, as it will be removed in 3.0 version.
+     */
+    public function testLegacyShouldCheckPriority()
+    {
+        $integrationCase = new IntegrationCase(
+            'foo',
+            'Foo',
+            array(),
+            array(),
+            array(),
+            new RuleSet(),
+            'Bar',
+            'Baz'
+        );
+
+        $integrationCase->shouldCheckPriority();
+    }
+}
