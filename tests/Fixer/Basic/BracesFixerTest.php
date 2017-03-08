d620332570fdd291c4b2d9c30a7b8c9085a141c1
Jan Tojnar <jtojnar@gmail.com>
2017-03-08 21:17:07 +0100

    Add SpacePossum's test

--- tests/Fixer/Basic/BracesFixerTest.php
+++ tests/Fixer/Basic/BracesFixerTest.php
@@ -1867,6 +1867,44 @@ class Foo
     }',
                 self::$configurationOopPositionSameLine,
             ),
+            array(
+                '<?php
+class Something # a
+{
+    public function sth() //
+    {
+        return function (int $foo) use ($bar) { return $bar; };
+    }
+}
+
+function C() /**/ //    # /**/
+{
+}
+
+function D() /**
+*
+*/ {
+}',
+                '<?php
+class Something # a
+{
+    public function sth() //
+    {
+        return function (int $foo) use ($bar) { return $bar; };
+    }
+}
+
+function C() /**/ //    # /**/
+{
+}
+
+function D() /**
+*
+*/
+{
+}',
+                self::$configurationOopPositionSameLine,
+            ),
         );
     }
 
