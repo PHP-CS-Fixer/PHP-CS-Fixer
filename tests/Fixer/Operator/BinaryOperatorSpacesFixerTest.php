81275c378b9b5f832b7d60863525f34457eac9bb
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-04-02 23:29:24 +0200

    add covers annotation
    Conflicts:
    	tests/Fixer/PhpUnit/PhpUnitTestClassRequiresCoversFixerTest.php

--- tests/Fixer/Operator/BinaryOperatorSpacesFixerTest.php
+++ tests/Fixer/Operator/BinaryOperatorSpacesFixerTest.php
@@ -22,7 +22,7 @@ use PhpCsFixer\Test\AbstractFixerTestCase;
  *
  * @internal
  *
- * @covers \PhpCsFixer\Fixer\Operator\AbstractAlignFixerHelper
+ * @covers \PhpCsFixer\AbstractAlignFixerHelper
  * @covers \PhpCsFixer\Fixer\Operator\AlignDoubleArrowFixerHelper
  * @covers \PhpCsFixer\Fixer\Operator\AlignEqualsFixerHelper
  * @covers \PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer
