282faebf8a3e871cd7d8de43d27dc5a78cd8b2a0
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-04-11 21:46:49 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -26,7 +26,7 @@ use Symfony\Component\Console\Application as BaseApplication;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.2.1';
+    const VERSION = '2.2.2-DEV';
 
     /**
      * Constructor.
