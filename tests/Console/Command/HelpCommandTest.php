f9b2c23e9921d8cc21e870a40b367ae57552556c
SpacePossum <possumfromspace@gmail.com>
2017-05-26 12:50:05 +0200

    Phar release - fix readme generation

---
+++ tests/Console/Command/HelpCommandTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Tests\Console\Command;
+
+use PhpCsFixer\Console\Application;
+use PhpCsFixer\Console\Command\HelpCommand;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @covers \PhpCsFixer\Console\Command\HelpCommand
+ */
+final class HelpCommandTest extends TestCase
+{
+    public function testGetLatestReleaseVersionFromChangeLog()
+    {
+        $helpVersion = HelpCommand::getLatestReleaseVersionFromChangeLog();
+        $appVersion = Application::VERSION;
+        $this->assertTrue(
+            version_compare($helpVersion, $appVersion, '<='),
+            sprintf(
+                'Expected version from change log "%s" <= as application version "%s".',
+                $helpVersion,
+                $appVersion
+            )
+        );
+    }
+}
