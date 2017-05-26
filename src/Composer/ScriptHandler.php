5c1c8b90e5ceae8caafec93dcf3dbb1b87fd400c
Dmitry Danilson <patchranger@gmail.com>
2017-05-26 17:06:29 +0700

    +pre-commit git hook to auto fix CS

---
+++ src/Composer/ScriptHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Composer;
+
+use Composer\Script\Event;
+
+class ScriptHandler
+{
+    /**
+     * Enables automatic code style fixing by pre-commit hook.
+     *
+     * @param Event $event
+     */
+    public static function enableAutoCSFix(Event $event)
+    {
+        if (file_exists('./.git')) {
+            $phpCsFixerPath = file_exists('./bin/php-cs-fixer')
+                ? './vendor/friendsofphp/php-cs-fixer/Symfony/CS'
+                : '.';
+            copy("{$phpCsFixerPath}/build/hooks/pre-commit", './.git/hooks/pre-commit');
+            chmod('./.git/hooks/pre-commit', 0775);
+        }
+    }
+}
