569778aab661196e3dbca0081b2f14579f7c6ef2
sebastianseidelmann <sebastian.seidelmann@wfp2.com>
2017-06-27 12:06:27 +0200

    [TASK] add the second parameter to rule set interface for passing the custom set definitions

--- src/RuleSetInterface.php
+++ src/RuleSetInterface.php
@@ -21,9 +21,9 @@ namespace PhpCsFixer;
  */
 interface RuleSetInterface
 {
-    public function __construct(array $set = []);
+    public function __construct(array $set = [], array $customSetDefinitions = []);
 
-    public static function create(array $set = []);
+    public static function create(array $set = [], array $customSetDefinitions = []);
 
     /**
      * Get configuration for given rule.
