c295b6c6b1f90fa40b94cf76f82749de5a07e907
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-07-29 15:46:35 +0200

    adjust setUp

--- tests/functional/fixture/integration/setUp.sh
+++ tests/functional/fixture/integration/setUp.sh
@@ -4,10 +4,12 @@ git config user.email test
 git add .
 git commit -m "init" -q
 
-git checkout -b change
+git checkout -b case1
 touch dir\ a/file.php
 rm -r dir\ c
 echo "" >> dir\ b/file\ b.php
 echo "echo 1;" >> dir\ b/file\ b.php
 git add .
-git commit -m "change" -q
+git commit -m "case1" -q
+
+git checkout master
