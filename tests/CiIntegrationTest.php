1ed579dc9d7badfb3c87bfd38bc0e0dabfd5cc6e
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-07-29 21:36:44 +0200

    fix cs

--- tests/CiIntegrationTest.php
+++ tests/CiIntegrationTest.php
@@ -160,7 +160,7 @@ Legend: ?-unknown, I-invalid file syntax, file ignored, S-Skipped, .-no changes,
                 "Cannot execute `%s`:\n%s\nCode: %s\nExit text: %s\nError output: %s\nDetails:\n%s",
                 $command,
                 './'.static::$tmpFileName === $command
-                    ? implode('', array_map(function($line) { return "$ $line"; }, file(static::$tmpFilePath)))."\n"
+                    ? implode('', array_map(function ($line) { return "$ $line"; }, file(static::$tmpFilePath)))."\n"
                     : '',
                 $result['code'],
                 $process->getExitCodeText(),
