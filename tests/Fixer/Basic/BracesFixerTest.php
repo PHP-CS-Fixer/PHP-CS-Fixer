1186d53b7aeeec1cca8e931d9df27714e1d14c6c
Marcin Nabiałek <dev@nabialek.org>
2017-08-25 15:14:36 +0200

    Bugfix braces

--- tests/Fixer/Basic/BracesFixerTest.php
+++ tests/Fixer/Basic/BracesFixerTest.php
@@ -691,6 +691,30 @@ function foo()
     $baz  = 2;    /* baz */     // next comment
 }',
             ),
+            array(
+                '<?php
+function test()
+{
+//    $closure = function ($callback) use ($query) {
+//        doSomething();
+//
+//        return true;
+//    };
+    $a = 3;
+}',
+            ),
+            array(
+                '<?php
+function test()
+{
+//    $closure = function ($callback) use ($query) {
+//        doSomething();
+//        '.'
+//        return true;
+//    };
+    $a = 3;
+}',
+            ),
         );
     }
 
