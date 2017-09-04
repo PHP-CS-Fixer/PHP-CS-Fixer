fc97de3ba4f4888d709381c462aa676aedd71d0f
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-09-04 22:44:26 +0200

    build.sh - don't use travis tools

--- dev-tools/build.sh
+++ dev-tools/build.sh
@@ -7,11 +7,12 @@ composer config platform.php 2> /dev/null || composer config platform.php 5.6.0
 # require suggested packages
 composer require --no-update symfony/polyfill-mbstring
 
+# install package deps without dev-deps / remove already installed dev-deps
 composer update --no-interaction --no-progress --no-dev --prefer-stable
 composer info -D | sort
 
-# install box2
-composer global show kherge/box -q || travis_retry composer global require --no-interaction --no-progress kherge/box:^2.7
+# install box2 globally
+composer global show kherge/box -q || composer global require --no-interaction --no-progress kherge/box:^2.7
 
 # build phar file
 php -d phar.readonly=false $(composer config home)/vendor/bin/box build
