0f51ab96e9c72cb9285dc24ffdc1bc159e08ab62
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-09-18 21:02:54 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -31,7 +31,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.6.0';
+    const VERSION = '2.6.1-DEV';
     const VERSION_CODENAME = 'Local Tavern';
 
     public function __construct()
