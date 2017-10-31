bb7c4d54fef67a1250ce3ca5d91e8c2beff824c8
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-10-31 15:26:36 +0100

    fix after rebase

--- tests/Cache/FileHandlerTest.php
+++ tests/Cache/FileHandlerTest.php
@@ -154,13 +154,11 @@ final class FileHandlerTest extends TestCase
 
         $handler = new FileHandler($dir);
 
-        $this->setExpectedExceptionRegExp(
-            'Symfony\Component\Filesystem\Exception\IOException',
-            sprintf(
-                '#^%s$#',
-                preg_quote('Cannot write cache file "'.realpath($dir).'" as the location exists as directory.', '#')
-        )
-        );
+        $this->expectException(\Symfony\Component\Filesystem\Exception\IOException::class);
+        $this->expectExceptionMessageRegExp(sprintf(
+            '#^%s$#',
+            preg_quote('Cannot write cache file "'.realpath($dir).'" as the location exists as directory.', '#')
+        ));
 
         $handler->write(new Cache(new Signature(
             PHP_VERSION,
@@ -183,13 +181,11 @@ final class FileHandlerTest extends TestCase
 
         $handler = new FileHandler($file);
 
-        $this->setExpectedExceptionRegExp(
-            'Symfony\Component\Filesystem\Exception\IOException',
-            sprintf(
-                '#^%s$#',
-                preg_quote('Cannot write to file "'.realpath($file).'" as it is not writable.', '#')
-        )
-        );
+        $this->expectException(\Symfony\Component\Filesystem\Exception\IOException::class);
+        $this->expectExceptionMessageRegExp(sprintf(
+            '#^%s$#',
+            preg_quote('Cannot write to file "'.realpath($file).'" as it is not writable.', '#')
+        ));
 
         $handler->write(new Cache(new Signature(
             PHP_VERSION,
