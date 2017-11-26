24e50fc609b3d63faae69a64d06020248c9c0b30
Filippo Tessarotto <zoeslam@gmail.com>
2017-11-26 12:08:13 +0100

    Add extended description

--- src/Fixer/StringNotation/EscapeImplicitBackslashesFixer.php
+++ src/Fixer/StringNotation/EscapeImplicitBackslashesFixer.php
@@ -60,7 +60,15 @@ EOF;
                     $codeSamble,
                     ['heredoc_syntax' => false]
                 ),
-            ]
+            ],
+            'In PHP, double-quoted strings and heredocs some chars like `n`, `$` or `u` have special meanings if preceeded by a backslash, '
+            .'(and some are special only if followed by other special chars) while a backslash preceding other chars are interpreted like a plain '
+            .'backslash. The precise list of those special chars is hard to remember and to identify quickly: this fixer escapes backslashes '
+            .'that do not start a special interpretation with the char after them.'
+            .PHP_EOL
+            .'It is possible to fix also single-quoted strings: in this case there is no special chars apart from single-quote and backslash '
+            .'itself, so the fixer simply ensure that all backslashes are escaped. Both single and double backslashes are allowed in single-quoted '
+            .'strings, so the purpose in this context is mainly to have a uniformed way to have them written all over the codebase.'
         );
     }
 
