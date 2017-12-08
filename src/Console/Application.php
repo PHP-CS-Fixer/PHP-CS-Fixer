454ddbe65da6a9297446f442bad244e8a99a9a38
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-12-08 17:36:20 +0100

    prepared the 2.9.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -35,8 +35,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.9.0-DEV';
-    const VERSION_CODENAME = '';
+    const VERSION = '2.9.0';
+    const VERSION_CODENAME = 'Speechless';
 
     /**
      * @var ToolInfo
