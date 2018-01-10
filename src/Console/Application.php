dcf644315abc557e006ce068d299f85d17ec2157
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2018-01-10 17:43:26 +0100

    prepared the 2.2.14 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -35,7 +35,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.2.14-DEV';
+    const VERSION = '2.2.14';
 
     /**
      * @var ToolInfo
