93b6694bada0fed87cf2668447f6e3b52197c074
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2018-01-12 10:37:42 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -35,7 +35,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.10.0';
+    const VERSION = '2.10.1-DEV';
     const VERSION_CODENAME = 'Bowling Bear';
 
     /**
