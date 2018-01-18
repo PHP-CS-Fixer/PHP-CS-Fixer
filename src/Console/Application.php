0662bded545850f67e9b7474d559d2a3a3e4e1d7
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2018-01-18 12:47:38 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -35,7 +35,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.2.14';
+    const VERSION = '2.2.15-DEV';
 
     /**
      * @var ToolInfo
