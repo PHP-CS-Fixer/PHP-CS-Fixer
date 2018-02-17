6e8a4baa653c0130dfa22e2373ce810d05fe9ac4
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2018-02-17 15:58:20 +0300

    Make shell scripts POSIX-compatible

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 command -v php >/dev/null 2>&1 || { echo "I require `php` but it's not available. Aborting." >&2; exit 255; }
 command -v grep >/dev/null 2>&1 || { echo "I require `grep` but it's not available. Aborting." >&2; exit 255; }
 command -v awk >/dev/null 2>&1 || { echo "I require `awk` but it's not available. Aborting." >&2; exit 255; }
 
-if [ "" == "$1" ] || [ "" == "$2" ];
+if [ "" = "$1" ] || [ "" = "$2" ];
 then
     echo "Usage: bash benchmark.sh BRANCH1 BRANCH2 ...BRANCHN"
     exit 1;
@@ -12,10 +12,10 @@ fi
 
 for BRANCH in $@
 do
-    git checkout $BRANCH &> /dev/null &&
-    git reset --hard &> /dev/null &&
-    echo -n $BRANCH
-    (for i in {1..10}; do php php-cs-fixer fix --dry-run 2> /dev/null ; done) | grep -i seconds | awk '
+    git checkout $BRANCH > /dev/null 2>&1 &&
+    git reset --hard > /dev/null 2>&1 &&
+    printf '%s' $BRANCH
+    (for i in $(seq 1 10); do php php-cs-fixer fix --dry-run 2> /dev/null ; done) | grep -i seconds | awk '
     {
         total += $5;
         ++count;
