1634a2c250bf4640f1c5c963f63b413c2d966c8a
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2018-02-22 17:49:33 +0100

    prepared the 2.10.3 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -35,7 +35,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.10.3-DEV';
+    const VERSION = '2.10.3';
     const VERSION_CODENAME = 'Bowling Bear';
 
     /**
