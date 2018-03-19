418153a096727ab0c2917afbaa6ced8fec3b9892
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2018-03-19 19:58:31 +0300

    Add ShellCheck to CI

--- dev-tools/check-shell-scripts.sh
+++ dev-tools/check-shell-scripts.sh
@@ -9,3 +9,6 @@ SHELL_SCRIPTS=$(find ./ -type f -not -path '*/vendor/*' -iname '*.sh')
 
 # shellcheck disable=SC2086
 checkbashisms $SHELL_SCRIPTS
+
+# shellcheck disable=SC2086
+shellcheck $SHELL_SCRIPTS
