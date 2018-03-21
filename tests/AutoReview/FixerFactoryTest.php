02e9a36c97348cced56c21a4eb100a53976aac43
Filippo Tessarotto <zoeslam@gmail.com>
2018-03-21 10:09:30 +0100

    ArrayIndentationFixer: priority bug with BinaryOperatorSpacesFixer and MethodChainingIndentationFixer

--- tests/AutoReview/FixerFactoryTest.php
+++ tests/AutoReview/FixerFactoryTest.php
@@ -58,6 +58,8 @@ final class FixerFactoryTest extends TestCase
         }
 
         return [
+            [$fixers['array_indentation'], $fixers['binary_operator_spaces']],
+            [$fixers['array_indentation'], $fixers['method_chaining_indentation']],
             [$fixers['array_syntax'], $fixers['binary_operator_spaces']],
             [$fixers['array_syntax'], $fixers['ternary_operator_spaces']],
             [$fixers['backtick_to_shell_exec'], $fixers['escape_implicit_backslashes']],
