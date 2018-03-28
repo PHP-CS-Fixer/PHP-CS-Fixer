cedfd1a47ccd756fa95b30c1612855528802d51d
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2018-03-28 16:37:22 +0200

    Remove SameStringsConstraint and XmlMatchesXsdConstraint

--- tests/Test/AbstractFixerTestCase.php
+++ tests/Test/AbstractFixerTestCase.php
@@ -18,7 +18,7 @@ use PhpCsFixer\FixerFactory;
 use PhpCsFixer\Linter\Linter;
 use PhpCsFixer\Linter\LinterInterface;
 use PhpCsFixer\RuleSet;
-use PhpCsFixer\Tests\Test\Constraint\SameStringsConstraint;
+use PhpCsFixer\PhpunitGenericConstraints\Constraint\SameStringsConstraint;
 use PhpCsFixer\Tests\TestCase;
 use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
@@ -141,7 +141,7 @@ abstract class AbstractFixerTestCase extends TestCase
 
             $this->assertThat(
                 $tokens->generateCode(),
-                new SameStringsConstraint($expected),
+                self::createSameStringsConstraint($expected),
                 'Code build on input code must match expected code.'
             );
             $this->assertTrue($tokens->isChanged(), 'Tokens collection built on input code must be marked as changed after fixing.');
@@ -173,7 +173,7 @@ abstract class AbstractFixerTestCase extends TestCase
 
         $this->assertThat(
             $tokens->generateCode(),
-            new SameStringsConstraint($expected),
+            self::createSameStringsConstraint($expected),
             'Code build on expected code must not change.'
         );
         $this->assertFalse($tokens->isChanged(), 'Tokens collection built on expected code must not be marked as changed after fixing.');
@@ -262,5 +262,21 @@ abstract class AbstractFixerTestCase extends TestCase
         $this->fixerClassName = get_class($fixers[0]);
 
         return $this->fixerClassName;
+    }
+
+    private static function createSameStringsConstraint($expected) {
+        $candidates = array_filter(array(
+            'PhpCsFixer\PhpunitGenericConstraints\Constraint\SameStringsConstraint',
+            'PHPUnit\Framework\Constraint\IsIdentical',
+            'PHPUnit_Framework_Constraint_IsIdentical',
+        ), function ($className) { return class_exists($className); });
+
+        if (empty($candidates)) {
+            throw new \RuntimeException('PHPUnit not installed?!');
+        }
+
+        $candidate = $candidates[0];
+
+        return new $candidate($expected);
     }
 }
--- tests/Test/AbstractIntegrationTestCase.php
+++ tests/Test/AbstractIntegrationTestCase.php
@@ -22,7 +22,7 @@ use PhpCsFixer\FixerFactory;
 use PhpCsFixer\Linter\Linter;
 use PhpCsFixer\Linter\LinterInterface;
 use PhpCsFixer\Runner\Runner;
-use PhpCsFixer\Tests\Test\Constraint\SameStringsConstraint;
+use PhpCsFixer\PhpunitGenericConstraints\Constraint\SameStringsConstraint;
 use PhpCsFixer\Tests\TestCase;
 use PhpCsFixer\Tokenizer\Tokens;
 use PhpCsFixer\WhitespacesFixerConfig;
@@ -253,7 +253,7 @@ abstract class AbstractIntegrationTestCase extends TestCase
         $fixedInputCode = file_get_contents($tmpFile);
         $this->assertThat(
             $fixedInputCode,
-            new SameStringsConstraint($expected),
+            self::createSameStringsConstraint($expected),
             sprintf(
                 "Expected changes do not match result for \"%s\" in \"%s\".\nFixers applied:\n%s.",
                 $case->getTitle(),
@@ -387,5 +387,21 @@ abstract class AbstractIntegrationTestCase extends TestCase
         }
 
         return $linter;
+    }
+
+    private static function createSameStringsConstraint($expected) {
+        $candidates = array_filter(array(
+            'PhpCsFixer\PhpunitGenericConstraints\Constraint\SameStringsConstraint',
+            'PHPUnit\Framework\Constraint\IsIdentical',
+            'PHPUnit_Framework_Constraint_IsIdentical',
+        ), function ($className) { return class_exists($className); });
+
+        if (empty($candidates)) {
+            throw new \RuntimeException('PHPUnit not installed?!');
+        }
+
+        $candidate = $candidates[0];
+
+        return new $candidate($expected);
     }
 }
