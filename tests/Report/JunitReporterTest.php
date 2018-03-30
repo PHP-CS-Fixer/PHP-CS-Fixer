d527116e5dc247f89a4264963f28bb48214b4c53
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2018-03-30 18:39:18 +0200

    Update JunitReporterTest.php

--- tests/Report/JunitReporterTest.php
+++ tests/Report/JunitReporterTest.php
@@ -39,7 +39,7 @@ final class JunitReporterTest extends AbstractReporterTestCase
     {
         // @TODO 2.11 remove me
         if (!class_exists('PhpCsFixer\PhpunitConstraintXmlMatchesXsd\Constraint\XmlMatchesXsd')) {
-            self::markTestSkipped('Cannot execute test, install php-cs-fixer/phpunit-generic-constraints first.');
+            self::markTestSkipped('Cannot execute test, install `php-cs-fixer/phpunit-constraint-xmlmatchesxsd` first.');
         }
 
         self::$xsd = file_get_contents(__DIR__.'/../../doc/junit-10.xsd');
