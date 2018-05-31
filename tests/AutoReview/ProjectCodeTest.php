b9f3067f3dc18d532488bd6ac089c72758dcc194
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2018-05-31 02:17:50 +0200

    groom message

--- tests/AutoReview/ProjectCodeTest.php
+++ tests/AutoReview/ProjectCodeTest.php
@@ -359,7 +359,7 @@ final class ProjectCodeTest extends TestCase
     public function provideDataProviderMethodNameCases()
     {
         if ((extension_loaded('xdebug') || 'phpdbg' === PHP_SAPI) && false === getenv('CI')) {
-            $this->markTestSkipped('Test too slow when Xdebug is loaded or running under phpdbg.');
+            $this->markTestSkipped('Data provider too slow when Xdebug is loaded or running under phpdbg.');
         }
 
         $data = [];
@@ -401,7 +401,7 @@ final class ProjectCodeTest extends TestCase
     public function provideClassesWherePregFunctionsAreForbiddenCases()
     {
         if ((extension_loaded('xdebug') || 'phpdbg' === PHP_SAPI) && false === getenv('CI')) {
-            $this->markTestSkipped('Test too slow when Xdebug is loaded or running under phpdbg.');
+            $this->markTestSkipped('Data provider too slow when Xdebug is loaded or running under phpdbg.');
         }
 
         return array_map(
