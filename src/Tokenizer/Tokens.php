6f1f9ad381c5b5f9f4650fcebde2d583efcca07f
Markus Staab <markus.staab@redaxo.de>
2018-10-15 20:51:19 +0200

    simplify

--- src/Tokenizer/Tokens.php
+++ src/Tokenizer/Tokens.php
@@ -883,9 +883,8 @@ class Tokens extends \SplFixedArray
         // that way we get around additional overhead this class adds with overridden offset* methods.
         for ($i = $oldSize + $itemsCnt - 1; $i >= $index; --$i) {
             if ($this->offsetExists($i - $itemsCnt)) {
-                $oldItem = parent::offsetGet($i - $itemsCnt);
-                $this->blockEndCache = [];
-                parent::offsetSet($i, $oldItem);
+                $oldItem = $this[$i - $itemsCnt];
+                $this->offsetSet($i, $oldItem);
             } else {
                 $this->offsetSet($i, new Token(''));
             }
