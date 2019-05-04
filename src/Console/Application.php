810bd174184b4e577bc875df2a8067a84c43c997
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2019-05-04 23:32:05 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -35,7 +35,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.12.9';
+    const VERSION = '2.12.10-DEV';
     const VERSION_CODENAME = 'Long Journey';
 
     /**
