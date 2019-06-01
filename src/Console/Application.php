53be916a89f09e0b38b8c47696c44641caa37b70
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2019-06-01 12:30:02 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -35,7 +35,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.14.4';
+    const VERSION = '2.14.5-DEV';
     const VERSION_CODENAME = 'Sunrise';
 
     /**
