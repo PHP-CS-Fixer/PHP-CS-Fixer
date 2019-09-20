eb7ef2ea4cfa020ad3a81bb692e2ab2f75324345
Filippo Tessarotto <zoeslam@gmail.com>
2019-09-20 08:24:38 +0200

    Split PHP-version specific tests

--- tests/Fixer/ClassNotation/StaticPrivateMethodFixerTest.php
+++ tests/Fixer/ClassNotation/StaticPrivateMethodFixerTest.php
@@ -55,6 +55,269 @@ class Foo
     private static function baz()
     {
         if (true) {
+            return 1;
+        }
+    }
+}
+',
+                '<?php
+class Foo
+{
+    public $baz;
+
+    public function bar()
+    {
+        $var = $this->baz;
+        $var = $this->baz();
+        if (true) {
+            $var = $this->baz();
+        }
+    }
+
+    private function baz()
+    {
+        if (true) {
+            return 1;
+        }
+    }
+}
+',
+            ],
+            'handle-multiple-classes' => [
+                '<?php
+class Foo
+{
+    private static function baz() { return 1; }
+
+    public function xyz()
+    {
+        return 1;
+    }
+}
+class Bar
+{
+    public function baz() { return 1; }
+
+    abstract protected function xyz1();
+    protected abstract function xyz2();
+    abstract function xyz3();
+}
+',
+                '<?php
+class Foo
+{
+    private function baz() { return 1; }
+
+    public function xyz()
+    {
+        return 1;
+    }
+}
+class Bar
+{
+    public function baz() { return 1; }
+
+    abstract protected function xyz1();
+    protected abstract function xyz2();
+    abstract function xyz3();
+}
+',
+            ],
+            'inverse-order-keywords-already-ok' => [
+                '<?php
+class Foo
+{
+    static private function inverseOrder() { return 1; }
+}
+',
+            ],
+            'skip-methods-containing-closures' => [
+                '<?php
+class Foo
+{
+    private function bar()
+    {
+        return function() {};
+    }
+
+    private function baz()
+    {
+        return static function() {};
+    }
+}
+',
+            ],
+            'skip-instance-references' => [
+                '<?php
+class Foo
+{
+    private function bar()
+    {
+        if (true) {
+            return $this;
+        }
+    }
+}
+',
+            ],
+            'skip-debug_backtrace' => [
+                '<?php
+class Foo
+{
+    private function bar()
+    {
+        return debug_backtrace()[1][\'object\'];
+    }
+}
+',
+            ],
+            'fix-references-inside-non-static-closures' => [
+                '<?php
+class Foo
+{
+    public $baz;
+
+    public function bar()
+    {
+        $var = function() {
+            $var = $this->baz;
+            $var = self::baz();
+        };
+        // Non valid in runtime, but valid syntax
+        $var = static function() {
+            $var = $this->baz();
+        };
+    }
+
+    private static function baz()
+    {
+        return 1;
+    }
+}
+',
+                '<?php
+class Foo
+{
+    public $baz;
+
+    public function bar()
+    {
+        $var = function() {
+            $var = $this->baz;
+            $var = $this->baz();
+        };
+        // Non valid in runtime, but valid syntax
+        $var = static function() {
+            $var = $this->baz();
+        };
+    }
+
+    private function baz()
+    {
+        return 1;
+    }
+}
+',
+            ],
+            'skip-magic-methods' => [
+                '<?php
+class Foo
+{
+    private function __clone() {}
+    private function __construct() {}
+    private function __destruct() {}
+    private function __serialize() {}
+    private function __set_state() {}
+    private function __sleep() {}
+    private function __unserialize() {}
+    private function __wakeup() {}
+}
+',
+            ],
+            'bug-multiple-methods' => [
+                self::generate50Samples(true),
+                self::generate50Samples(false),
+            ],
+            'fix-self' => [
+                '<?php
+class Foo
+{
+    private static function baz()
+    {
+        return self::baz();
+    }
+}
+',
+                '<?php
+class Foo
+{
+    private function baz()
+    {
+        return $this->baz();
+    }
+}
+',
+            ],
+            'bug-trait' => [
+                '<?php
+
+class Foo
+{
+    use A, B, C {
+        asd as lol;
+    }
+
+    private static function bar() {}
+}
+',
+                '<?php
+
+class Foo
+{
+    use A, B, C {
+        asd as lol;
+    }
+
+    private function bar() {}
+}
+',
+            ],
+        ];
+    }
+
+    /**
+     * @param string      $expected
+     * @param null|string $input
+     *
+     * @dataProvider provideFix70Cases
+     * @requires PHP 7.0
+     */
+    public function testFix70($expected, $input = null)
+    {
+        $this->doTest($expected, $input);
+    }
+
+    public function provideFix70Cases()
+    {
+        return [
+            'main-use-case' => [
+                '<?php
+class Foo
+{
+    public $baz;
+
+    public function bar()
+    {
+        $var = $this->baz;
+        $var = self::baz();
+        if (true) {
+            $var = self::baz();
+        }
+    }
+
+    private static function baz()
+    {
+        if (true) {
             return new class() {
                 public function baz()
                 {
@@ -143,54 +406,6 @@ class Bar
 }
 ',
             ],
-            'inverse-order-keywords-already-ok' => [
-                '<?php
-class Foo
-{
-    static private function inverseOrder() { return 1; }
-}
-',
-            ],
-            'skip-methods-containing-closures' => [
-                '<?php
-class Foo
-{
-    private function bar()
-    {
-        return function() {};
-    }
-
-    private function baz()
-    {
-        return static function() {};
-    }
-}
-',
-            ],
-            'skip-instance-references' => [
-                '<?php
-class Foo
-{
-    private function bar()
-    {
-        if (true) {
-            return $this;
-        }
-    }
-}
-',
-            ],
-            'skip-debug_backtrace' => [
-                '<?php
-class Foo
-{
-    private function bar()
-    {
-        return debug_backtrace()[1][\'object\'];
-    }
-}
-',
-            ],
             'fix-references-inside-non-static-closures' => [
                 '<?php
 class Foo
@@ -248,69 +463,6 @@ class Foo
     {
         return 1;
     }
-}
-',
-            ],
-            'skip-magic-methods' => [
-                '<?php
-class Foo
-{
-    private function __clone() {}
-    private function __construct() {}
-    private function __destruct() {}
-    private function __serialize() {}
-    private function __set_state() {}
-    private function __sleep() {}
-    private function __unserialize() {}
-    private function __wakeup() {}
-}
-',
-            ],
-            'bug-multiple-methods' => [
-                self::generate50Samples(true),
-                self::generate50Samples(false),
-            ],
-            'fix-self' => [
-                '<?php
-class Foo
-{
-    private static function baz()
-    {
-        return self::baz();
-    }
-}
-',
-                '<?php
-class Foo
-{
-    private function baz()
-    {
-        return $this->baz();
-    }
-}
-',
-            ],
-            'bug-trait' => [
-                '<?php
-
-class Foo
-{
-    use A, B, C {
-        asd as lol;
-    }
-
-    private static function bar() {}
-}
-',
-                '<?php
-
-class Foo
-{
-    use A, B, C {
-        asd as lol;
-    }
-
-    private function bar() {}
 }
 ',
             ],
