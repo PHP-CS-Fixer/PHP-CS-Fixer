343c2df2b590502c3f0009625edf799b7ad65fc0
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2019-11-25 23:11:02 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -35,7 +35,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.16.1';
+    const VERSION = '2.16.2-DEV';
     const VERSION_CODENAME = 'Yellow Bird';
 
     /**
