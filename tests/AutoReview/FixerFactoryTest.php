5d9d0e6f3ad84c7d6fa75981fbeab99a990878a7
SpacePossum <possumfromspace@gmail.com>
2020-01-13 07:40:56 +0000

    NoUnusedImportsFixer - do not match variable name as import

--- tests/AutoReview/FixerFactoryTest.php
+++ tests/AutoReview/FixerFactoryTest.php
@@ -478,6 +478,8 @@ final class FixerFactoryTest extends TestCase
             }
 
             static::assertSame($sortedDescription, $casesDescription);
+        } else {
+            $this->addToAssertionCount(1);
         }
     }
 
