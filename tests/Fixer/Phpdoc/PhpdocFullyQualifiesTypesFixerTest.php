d72e31077701858976e71975db4243e52d71ce9d
Filippo Tessarotto <zoeslam@gmail.com>
2020-01-29 14:58:14 +0100

    Import test cases from FullyQualifiedStrictTypesFixerTest

--- tests/Fixer/Phpdoc/PhpdocFullyQualifiesTypesFixerTest.php
+++ tests/Fixer/Phpdoc/PhpdocFullyQualifiesTypesFixerTest.php
@@ -35,23 +35,175 @@ final class PhpdocFullyQualifiesTypesFixerTest extends AbstractFixerTestCase
     public function provideTestFixMethods()
     {
         return [
-            'Docblock' => [
+            'Import common strict types' => [
                 '<?php
 
 use Foo\Bar;
 
-/**
- * @param Bar $foo
- */
-function foo($foo) {}',
+class SomeClass
+{
+    /**
+     * @param Bar $foo
+     */
+    public function doSomething(\Foo\Bar $foo)
+    {
+    }
+}',
                 '<?php
 
 use Foo\Bar;
 
+class SomeClass
+{
+    /**
+     * @param \Foo\Bar $foo
+     */
+    public function doSomething(\Foo\Bar $foo)
+    {
+    }
+}',
+            ],
+            'Test namespace fixes' => [
+                '<?php
+
+namespace Foo\Bar;
+
+class SomeClass
+{
+    /**
+     * @param SomeClass $foo
+     * @param Buz $buz
+     * @param Zoof\Buz $barbuz
+     */
+    public function doSomething(\Foo\Bar\SomeClass $foo, \Foo\Bar\Buz $buz, \Foo\Bar\Zoof\Buz $barbuz)
+    {
+    }
+}',
+                '<?php
+
+namespace Foo\Bar;
+
+class SomeClass
+{
+    /**
+     * @param \Foo\Bar\SomeClass $foo
+     * @param \Foo\Bar\Buz $buz
+     * @param \Foo\Bar\Zoof\Buz $barbuz
+     */
+    public function doSomething(\Foo\Bar\SomeClass $foo, \Foo\Bar\Buz $buz, \Foo\Bar\Zoof\Buz $barbuz)
+    {
+    }
+}',
+            ],
+            'Partial class name looks like FQCN' => [
+                '<?php
+
+namespace One;
+
+use Two\Three;
+
+class Two
+{
+    /**
+     * Note that for this example, the following classes exist:
+     *
+     * - One\Two
+     * - One\Two\Three
+     * - Two\Three
+     */
+    public function three(Two\Three $three, Three $other)
+    {
+    }
+}',
+            ],
+//            'Test multi namespace fixes' => [
+//                '<?php
+//namespace Foo\Other {
+//}
+//
+//namespace Foo\Bar {
+//    class SomeClass
+//    {
+//        /**
+//         * @param \Foo\Bar\SomeClass $foo
+//         * @param \Foo\Bar\Buz $buz
+//         * @param \Foo\Bar\Zoof\Buz $barbuz
+//         */
+//        public function doSomething(\Foo\Bar\SomeClass $foo, \Foo\Bar\Buz $buz, \Foo\Bar\Zoof\Buz $barbuz)
+//        {
+//        }
+//    }
+//}',
+//            ],
+            'Test partial namespace and use imports' => [
+                '<?php
+
+namespace Ping\Pong;
+
+use Foo\Bar;
+use Ping;
+use Ping\Pong\Pang;
+use Ping\Pong\Pyng\Pung;
+
+class SomeClass
+{
+    /**
+     * @param Ping\Something $something,
+     * @param Pung\Pang $pungpang,
+     * @param Pung $pongpung,
+     * @param Pang\Pung $pangpung,
+     * @param Pyng\Pung\Pong $pongpyngpangpang,
+     * @param Bar\Baz\Buz $bazbuz
+     */
+    public function doSomething(
+        \Ping\Something $something,
+        \Ping\Pong\Pung\Pang $pungpang,
+        \Ping\Pong\Pung $pongpung,
+        \Ping\Pong\Pang\Pung $pangpung,
+        \Ping\Pong\Pyng\Pung\Pong $pongpyngpangpang,
+        \Foo\Bar\Baz\Buz $bazbuz
+    ){}
+}',
+                '<?php
+
+namespace Ping\Pong;
+
+use Foo\Bar;
+use Ping;
+use Ping\Pong\Pang;
+use Ping\Pong\Pyng\Pung;
+
+class SomeClass
+{
+    /**
+     * @param \Ping\Something $something,
+     * @param \Ping\Pong\Pung\Pang $pungpang,
+     * @param \Ping\Pong\Pung $pongpung,
+     * @param \Ping\Pong\Pang\Pung $pangpung,
+     * @param \Ping\Pong\Pyng\Pung\Pong $pongpyngpangpang,
+     * @param \Foo\Bar\Baz\Buz $bazbuz
+     */
+    public function doSomething(
+        \Ping\Something $something,
+        \Ping\Pong\Pung\Pang $pungpang,
+        \Ping\Pong\Pung $pongpung,
+        \Ping\Pong\Pang\Pung $pangpung,
+        \Ping\Pong\Pyng\Pung\Pong $pongpyngpangpang,
+        \Foo\Bar\Baz\Buz $bazbuz
+    ){}
+}',
+            ],
+            'Test reference' => [
+                '<?php
 /**
- * @param \Foo\Bar $foo
+ * @return Exception
  */
-function foo($foo) {}',
+function withReference(\Exception &$e) {}',
+                '<?php
+/**
+ * @return \Exception
+ */
+function withReference(\Exception &$e) {}',
             ],
         ];
     }
