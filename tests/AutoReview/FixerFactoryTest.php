d58e3cb4227a0e14dcf4ce9fe2a6a5064ab3edfb
Kuba Werłos <werlos@gmail.com>
2020-06-15 00:09:50 +0200

    SimplifiedNullReturnFixer - must run before VoidReturnFixer

--- tests/AutoReview/FixerFactoryTest.php
+++ tests/AutoReview/FixerFactoryTest.php
@@ -225,6 +225,7 @@ final class FixerFactoryTest extends TestCase
             [$fixers['protected_to_private'], $fixers['ordered_class_elements']],
             [$fixers['return_assignment'], $fixers['blank_line_before_statement']],
             [$fixers['simplified_null_return'], $fixers['no_useless_return']],
+            [$fixers['simplified_null_return'], $fixers['void_return']],
             [$fixers['single_import_per_statement'], $fixers['multiline_whitespace_before_semicolons']],
             [$fixers['single_import_per_statement'], $fixers['no_leading_import_slash']],
             [$fixers['single_import_per_statement'], $fixers['no_singleline_whitespace_before_semicolons']],
