4fc4617fbc68b4bcbbbdd178337dea6684116b9b
Jennifer Konikowski <jennifer.konikowski@testdouble.com>
2020-08-07 07:41:34 -0400

    add fixer and tests for variable names

---
+++ tests/Fixer/Casing/VariableCaseFixerTest.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Tests\Fixer\Casing;
+
+use PhpCsFixer\Tests\Test\AbstractFixerTestCase;
+
+/**
+ * @author Jennifer Konikowski <jennifer@testdouble.com>
+ *
+ * @internal
+ *
+ * @covers \PhpCsFixer\Fixer\Casing\VariableCaseFixer
+ */
+final class VariableCaseFixerTest extends AbstractFixerTestCase
+{
+     /**
+     * @param string      $expected
+     * @param null|string $input
+     *
+     * @dataProvider provideCamelCaseFixCases
+     */
+    public function testCamelCaseFix($expected, $input = null)
+    {
+        $this->doTest($expected, $input);
+    }
+
+    public function provideCamelCaseFixCases()
+    {
+        return [
+            [
+                '<?php $testVariable = 2;',
+                '<?php $test_variable = 2;',
+            ],
+            [
+                '<?php function foo_bar() { $testVariable = 2;}',
+                '<?php function foo_bar() { $test__variable = 2;}'
+            ],
+            [
+                '<?php echo $testModel->this_field;',
+                '<?php echo $test_model->this_field;'
+            ]
+        ];
+    }
+
+    /**
+     * @param string      $expected
+     * @param null|string $input
+     *
+     * @dataProvider provideSnakeCaseFixCases
+     */
+    public function testSnakeCaseFix($expected, $input = null)
+    {
+        $this->fixer->configure(['case' => 'snake_case']);
+        $this->doTest($expected, $input);
+    }
+
+    public function provideSnakeCaseFixCases()
+    {
+        return [
+            [
+                '<?php $test_variable = 2;',
+                '<?php $testVariable = 2;',
+            ],
+            [
+                '<?php function fooBar() { $test_variable = 2;}',
+                '<?php function fooBar() { $testVariable = 2;}'
+            ],
+            [
+                '<?php echo $test_model->thisField;',
+                '<?php echo $testModel->thisField;'
+            ]
+        ];
+    }
+}
