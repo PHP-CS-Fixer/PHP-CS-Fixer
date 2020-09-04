cfc55ec516bc409c434a5f1325631f05b890e193
SpacePossum <possumfromspace@gmail.com>
2020-09-04 17:12:53 +0200

    add spaces

--- src/Fixer/LanguageConstruct/NoUnsetOnPropertyFixer.php
+++ src/Fixer/LanguageConstruct/NoUnsetOnPropertyFixer.php
@@ -34,8 +34,8 @@ final class NoUnsetOnPropertyFixer extends AbstractFixer
             [new CodeSample("<?php\nunset(\$this->a);\n")],
             null,
             'Risky when relying on attributes to be removed using `unset` rather than be set to `null`.'.
-            'Changing variables to `null` instead of unsetting means these still show up when looping over class variables.'.
-            'With PHP 7.4, this rule might introduce `null` assignments to properties whose type declaration does not allow it.'
+            ' Changing variables to `null` instead of unsetting means these still show up when looping over class variables.'.
+            ' With PHP 7.4, this rule might introduce `null` assignments to properties whose type declaration does not allow it.'
         );
     }
 
