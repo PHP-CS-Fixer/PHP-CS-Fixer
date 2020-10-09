41d61ca0568d54241d7ee89a8255ba7f51fdd6d3
SpacePossum <possumfromspace@gmail.com>
2020-10-09 14:30:42 +0200

    test

--- dev-tools/check_file_permissions.sh
+++ dev-tools/check_file_permissions.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -eu
 
 files_with_wrong_permissions=$(
@@ -12,7 +12,6 @@ files_with_wrong_permissions=$(
 if [ "$files_with_wrong_permissions" ]
 then
     printf '\033[97;41mWrong permissions detected:\033[0m\n'
-    e=$(printf '\033')
     echo "${files_with_wrong_permissions}"
     exit 3
 fi
