0f8286f9601a211c83bebf7f3125610479ec084a
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2020-12-06 23:38:21 +0100

    prepared the 2.16.8 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -34,7 +34,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.16.8-DEV';
+    const VERSION = '2.16.8';
     const VERSION_CODENAME = 'Yellow Bird';
 
     /**
