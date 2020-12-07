f8243f0cbf99f2b90e26ad2bd460e951ae25a9fd
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2020-12-07 21:46:20 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -34,7 +34,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.17.0';
+    const VERSION = '2.17.1-DEV';
     const VERSION_CODENAME = 'Desert Beast';
 
     /**
