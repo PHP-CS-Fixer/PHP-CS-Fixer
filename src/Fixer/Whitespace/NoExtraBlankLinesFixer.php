f6c6bff48d01de62538bcd4686a996f1af9500f1
SpacePossum <possumfromspace@gmail.com>
2020-12-28 17:29:10 +0100

    NoExtraBlankLinesFixer - PHP8 throw support

--- src/Fixer/Whitespace/NoExtraBlankLinesFixer.php
+++ src/Fixer/Whitespace/NoExtraBlankLinesFixer.php
@@ -104,7 +104,7 @@ final class NoExtraBlankLinesFixer extends AbstractFixer implements Configuratio
             T_DEFAULT => 'fixAfterToken',
             T_RETURN => 'fixAfterToken',
             T_SWITCH => 'fixAfterToken',
-            T_THROW => 'fixAfterToken',
+            T_THROW => 'fixAfterThrowToken',
             T_USE => 'removeBetweenUse',
             T_WHITESPACE => 'removeMultipleBlankLines',
             CT::T_USE_TRAIT => 'removeBetweenUse',
@@ -406,6 +406,13 @@ switch($a) {
         }
 
         $this->removeEmptyLinesAfterLineWithTokenAt($index);
+    }
+
+    private function fixAfterThrowToken($index)
+    {
+        if ($this->tokens[$this->tokens->getPrevMeaningfulToken($index)]->equalsAny([';', '{', '}', ':', [T_OPEN_TAG]])) {
+            $this->fixAfterToken($index);
+        }
     }
 
     /**
