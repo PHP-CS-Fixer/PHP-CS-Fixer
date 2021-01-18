cbc5b50bfa2688a1afca20e5a8c71f058e9ccbef
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2021-01-18 04:31:06 +0100

    prepared the 2.18.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -34,8 +34,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.18.0-DEV';
-    const VERSION_CODENAME = '';
+    const VERSION = '2.18.0';
+    const VERSION_CODENAME = 'Remote Void';
 
     /**
      * @var ToolInfo
