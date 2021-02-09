0206d4acfd9e51c8565cc010d5b15f6d567de994
Lars Grevelink <lars.grevelink@mobian.global>
2021-02-09 12:15:44 +0100

    Initial version of the alphabetical array sort fixer

---
+++ src/Fixer/ArrayNotation/AlphabeticalArraySortFixer.php
@@ -0,0 +1,321 @@
+<?php
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Fixer\ArrayNotation;
+
+use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\Fixer\ConfigurationDefinitionFixerInterface;
+use PhpCsFixer\FixerConfiguration\FixerConfigurationResolver;
+use PhpCsFixer\FixerConfiguration\FixerOptionBuilder;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\VersionSpecification;
+use PhpCsFixer\FixerDefinition\VersionSpecificCodeSample;
+use PhpCsFixer\Tokenizer\CT;
+use PhpCsFixer\Tokenizer\Tokens;
+
+/**
+ * @author Lars Grevelink <lars.grevelink@mobian.global>
+ */
+final class AlphabeticalArraySortFixer extends AbstractFixer implements ConfigurationDefinitionFixerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefinition()
+    {
+        // Return a definition of the fixer, it will be used in the documentation.
+        return new FixerDefinition(
+            'Sorts array by alphabetical order.',
+            [
+                new CodeSample(
+                    "<?php\n\$sample = array('b' => '2', 'a' => '1', 'd' => '5');\n"
+                ),
+                new VersionSpecificCodeSample(
+                    "<?php\n\$sample = array('b' => '2', 'a' => '1', 'd' => '5');\n",
+                    new VersionSpecification(50400),
+                    ['sort_special_key_mode' => 'special_case_on_bottom']
+                ),
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isCandidate(Tokens $tokens)
+    {
+        return $tokens->isTokenKindFound(T_ARRAY) || $tokens->isTokenKindFound(CT::T_ARRAY_SQUARE_BRACE_OPEN);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function applyFix(\SplFileInfo $file, Tokens $tokens)
+    {
+        $this->sortTokens($tokens);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function createConfigurationDefinition()
+    {
+        return new FixerConfigurationResolver([
+            (new FixerOptionBuilder('sort_special_key_mode', 'In which way to sort the special keys'))
+                ->setAllowedValues(['special_case_on_bottom', 'special_case_on_top'])
+                ->setDefault('special_case_on_bottom')
+                ->getOption(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function sortTokens(Tokens $tokens)
+    {
+        $lastProcessedIndex = null;
+        foreach ($tokens as $index => $token) {
+            if ($lastProcessedIndex !== null && $index < $lastProcessedIndex) {
+                continue;
+            }
+
+            if ($token->isGivenKind(T_ARRAY) || $token->isGivenKind(CT::T_ARRAY_SQUARE_BRACE_OPEN)) {
+                $clonedTokens = clone $tokens;
+
+                [$startIndex, $endIndex] = $this->getArrayIndexes($tokens, $index);
+
+                $content = [];
+                while ($startIndex !== null && $startIndex < $endIndex) {
+                    $startIndex = $tokens->getNextMeaningfulToken($startIndex);
+                    if ($tokens[$startIndex]->isGivenKind(T_ARRAY) || $tokens[$startIndex]->isGivenKind(CT::T_ARRAY_SQUARE_BRACE_OPEN)) {
+                        $lastProcessedIndex = $startIndex = $this->sortNestedTokens($clonedTokens, $startIndex);
+                    }
+
+                    if ($tokens[$startIndex]->isGivenKind(T_DOUBLE_ARROW)) {
+                        [$key, $keyTokenIndex] = $this->getKeyAndEndPosition($clonedTokens, $startIndex);
+
+                        $valueTokenIndex = $tokens->getNextMeaningfulToken($startIndex);
+
+                        if ($tokens[$valueTokenIndex]->isGivenKind(T_ARRAY) || $tokens[$valueTokenIndex]->isGivenKind(CT::T_ARRAY_SQUARE_BRACE_OPEN)) {
+                            $startIndex = $this->sortNestedTokens($clonedTokens, $valueTokenIndex);
+                        }
+
+                        $valueRange = $startIndex;
+                        while ($valueRange = $tokens->getNextTokenOfKind($valueRange, [',', '('])) {
+                            if ($tokens[$valueRange]->equals('(')) {
+                                $valueRange = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_PARENTHESIS_BRACE, $valueRange);
+                            } else {
+                                break;
+                            }
+                        }
+
+                        if (!$valueRange || $valueRange > $endIndex) {
+                            $valueRange = $endIndex;
+                        }
+
+                        $valueEndIndex = $tokens->getPrevMeaningfulToken($valueRange);
+
+                        $content[$key] = [$keyTokenIndex, $valueEndIndex];
+
+                        $lastProcessedIndex = $startIndex = $valueEndIndex;
+                    }
+                }
+
+                $sortedContentKeys = array_keys($content);
+
+                usort($sortedContentKeys, [$this, 'sortByKey']);
+
+                $sorting = array_combine(array_keys($content), $sortedContentKeys);
+
+                if (count($content) === 0) {
+                    $tokens->overrideRange(0, $tokens->count() - 1, $clonedTokens);
+                } else {
+                    foreach (array_reverse($sorting, true) as $original => $replacement) {
+                        [$startOriginalIndex, $endOriginalIndex] = $content[$original];
+                        [$startReplacementIndex, $endReplacementIndex] = $content[$replacement];
+
+                        $newTokens = array_slice($clonedTokens->toArray(), $startReplacementIndex, $endReplacementIndex - $startReplacementIndex + 1);
+
+                        $tokens->overrideRange($startOriginalIndex, $endOriginalIndex, $newTokens);
+                    }
+                }
+
+                $tokens->clearEmptyTokens();
+            }
+        }
+    }
+
+    /**
+     * Get the key and the end position index.
+     *
+     * @param Tokens $clonedTokens
+     * @param int $startIndex
+     *
+     * @return array
+     */
+    private function getKeyAndEndPosition(Tokens $clonedTokens, int $startIndex)
+    {
+        $prevItemEndIndex = $clonedTokens->getPrevMeaningfulToken($clonedTokens->getPrevMeaningfulToken($startIndex));
+        $prevItemEndToken = $clonedTokens[$prevItemEndIndex];
+
+        if ($prevItemEndToken->equals('(')) {
+            $hasSpecialKeys = !$clonedTokens[$clonedTokens->getPrevMeaningfulToken($prevItemEndIndex)]->isGivenKind(T_ARRAY);
+        } else {
+            $hasSpecialKeys = !($prevItemEndToken->isGivenKind([CT::T_ARRAY_SQUARE_BRACE_OPEN, T_ARRAY]) || $prevItemEndToken->equals(','));
+        }
+
+        if ($hasSpecialKeys) {
+            $keyTokenIndex = $startIndex;
+
+            for ($i = $startIndex; $i >= 0; $i--) {
+                if ($clonedTokens[$i]->equals(')')) {
+                    $i = $clonedTokens->findBlockStart(Tokens::BLOCK_TYPE_PARENTHESIS_BRACE, $i);
+
+                    continue;
+                }
+
+                if ($clonedTokens[$i]->isGivenKind(CT::T_ARRAY_SQUARE_BRACE_CLOSE)) {
+                    $i = $clonedTokens->findBlockStart(Tokens::BLOCK_TYPE_ARRAY_SQUARE_BRACE, $i);
+
+                    continue;
+                }
+
+                if ($clonedTokens[$i]->isGivenKind(T_ARRAY)) {
+                    $keyTokenIndex = $clonedTokens->getNextMeaningfulToken($clonedTokens->getNextMeaningfulToken($i));
+
+                    break;
+                }
+
+                if ($clonedTokens[$i]->equals(',') || $clonedTokens[$i]->isGivenKind(CT::T_ARRAY_SQUARE_BRACE_OPEN)) {
+                    $keyTokenIndex = $clonedTokens->getNextMeaningfulToken($i);
+
+                    break;
+                }
+            }
+
+            $keyTokens = Tokens::fromArray(array_slice($clonedTokens->toArray(), $keyTokenIndex, $startIndex - $keyTokenIndex - 1));
+
+            $key = implode('', array_map(function ($item) {
+                return $item->getContent();
+            }, $keyTokens->toArray()));
+        } else {
+            $keyTokenIndex = $clonedTokens->getPrevMeaningfulToken($startIndex);
+            $key = $clonedTokens[$keyTokenIndex]->getContent();
+        }
+
+        return [$key, $keyTokenIndex];
+    }
+
+    /**
+     * Sort nested array tokens and return the end index.
+     *
+     * @param Tokens $clonedTokens
+     * @param int $index
+     *
+     * @return int
+     */
+    private function sortNestedTokens(Tokens $clonedTokens, int $index)
+    {
+        [$nestedTokenStartIndex, $nestedTokenEndIndex] = $this->getArrayIndexes($clonedTokens, $index);
+
+        $nestedArrayTokens = Tokens::fromArray(array_slice($clonedTokens->toArray(), $nestedTokenStartIndex, $nestedTokenEndIndex - $nestedTokenStartIndex + 1));
+
+        $this->sortTokens($nestedArrayTokens);
+
+        $clonedTokens->overrideRange($nestedTokenStartIndex, $nestedTokenEndIndex, $nestedArrayTokens);
+
+        if ($nestedArrayTokens->count() !== $clonedTokens->count()) {
+            $clonedTokens->clearEmptyTokens();
+        }
+
+        return $nestedTokenEndIndex;
+    }
+
+    /**
+     * Get start and end index of an array.
+     *
+     * @param Tokens $tokens
+     * @param int $startIndex
+     *
+     * @return array
+     */
+    private function getArrayIndexes(Tokens $tokens, int $startIndex)
+    {
+        if ($tokens[$startIndex]->isGivenKind(T_ARRAY)) {
+            $startParentheses = $tokens->getNextTokenOfKind($startIndex, ['(']);
+            $endIndex = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_PARENTHESIS_BRACE, $startParentheses);
+        } else {
+            $endIndex = $tokens->findBlockEnd(Tokens::BLOCK_TYPE_ARRAY_SQUARE_BRACE, $startIndex);
+        }
+
+        return [
+            $startIndex,
+            $endIndex,
+        ];
+    }
+
+    private function isSpecialKey(string $value)
+    {
+        $tokens = Tokens::fromCode($value);
+
+        if ($tokens->count() > 1) {
+            return true;
+        }
+
+        return !preg_match('/^(\'|").+(\'|")$/i', $value);
+    }
+
+    /**
+     * @param string $a
+     * @param string $b
+     *
+     * @return int
+     */
+    protected function sortByKey(string $a, string $b)
+    {
+        $sortMode = $this->configuration['sort_special_key_mode'];
+
+        $aIsSpecial = $this->isSpecialKey($a);
+        $bIsSpecial = $this->isSpecialKey($b);
+
+        if ($sortMode === 'special_case_on_top') {
+            if ($aIsSpecial && $bIsSpecial) {
+                return 0;
+            }
+
+            if ($aIsSpecial) {
+                return -1;
+            }
+
+            if ($bIsSpecial) {
+                return 1;
+            }
+        }
+
+        if ($sortMode === 'special_case_on_bottom') {
+            if ($aIsSpecial && $bIsSpecial) {
+                return 0;
+            }
+
+            if ($aIsSpecial) {
+                return 1;
+            }
+
+            if ($bIsSpecial) {
+                return -1;
+            }
+        }
+
+        return strcmp($a, $b);
+    }
+}
---
+++ tests/Fixer/ArrayNotation/AlphabeticalArraySortFixerTest.php
@@ -0,0 +1,233 @@
+<?php
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Tests\Fixer\ArrayNotation;
+
+use PhpCsFixer\Tests\Test\AbstractFixerTestCase;
+
+/**
+ * @internal
+ *
+ * @covers \PhpCsFixer\Fixer\Comment\RemoveCommentsFixer
+ */
+final class AlphabeticalArraySortFixerTest extends AbstractFixerTestCase
+{
+    /**
+     * @param string $expected
+     * @param string|null $input
+     * @param array $config
+     *
+     * @dataProvider provideFixCases
+     */
+    public function testFix($expected, $input = null, array $config = [])
+    {
+        if (count($config) !== 0) {
+            $this->fixer->configure($config);
+        }
+
+        $this->doTest($expected, $input);
+    }
+
+    public function provideFixCases()
+    {
+        return [
+            [
+                '<?php [
+                    "a.*" => "int",
+                    "b" => "array",
+                    "c.*" => "array",
+                    "d" => "int",
+                    "d.*" => "string",
+                    "b" . "a" => "bla",
+                    "a" . "a" => "bla",
+                ];',
+                '<?php [
+                    "c.*" => "array",
+                    "b" => "array",
+                    "d.*" => "string",
+                    "b" . "a" => "bla",
+                    "a" . "a" => "bla",
+                    "d" => "int",
+                    "a.*" => "int",
+                ];',
+                [
+                    'sort_special_key_mode' => 'special_case_on_bottom',
+                ],
+            ],
+            [
+                '<?php [
+                    "b" . "a" => "bla",
+                    "a" . "a" => "bla",
+                    "a.*" => "int",
+                    "b" => "array",
+                    "c.*" => "array",
+                    "d" => "int",
+                    "d.*" => "string",
+                ];',
+                '<?php [
+                    "c.*" => "array",
+                    "b" => "array",
+                    "d.*" => "string",
+                    "b" . "a" => "bla",
+                    "a" . "a" => "bla",
+                    "d" => "int",
+                    "a.*" => "int",
+                ];',
+                [
+                    'sort_special_key_mode' => 'special_case_on_top',
+                ],
+            ],
+            [
+                '<?php [
+                    "b" => 2,
+                    things()() => Arr::get($something, $else) . Arr::get($different, $things),
+                    Arr::get($something, $else) . Arr::get($different, $things) => 1,
+                ];',
+                '<?php [
+                    things()() => Arr::get($something, $else) . Arr::get($different, $things),
+                    "b" => 2,
+                    Arr::get($something, $else) . Arr::get($different, $things) => 1,
+                ];',
+            ],
+            [
+                '<?php array("a" => "value2", "b" => "value3", "c" => "value1");',
+                '<?php array("c" => "value1", "a" => "value2", "b" => "value3");',
+            ],
+            [
+                '<?php ["a" => "value2", "b" => "value3", "c" => "value1"];',
+                '<?php ["c" => "value1", "a" => "value2", "b" => "value3"];',
+            ],
+            [
+                '<?php ["a" => "value3", "b" => "value4", "c" => "value5", foo() . " baz" => "value1", bar() => "value2"];',
+                '<?php [foo() . " baz" => "value1", "b" => "value4", bar() => "value2", "a" => "value3", "c" => "value5"];',
+            ],
+            [
+                '<?php ["a" => "value3", "b" => "value4", kar() => "value4", foo() . " baz" => "value1", bar() => "value2"];',
+                '<?php ["b" => "value4", kar() => "value4", foo() . " baz" => "value1", "a" => "value3", bar() => "value2"];',
+            ],
+
+            [
+                '<?php array("a" => "hey how, am i doing", "b" => "value3", "c" => "value1");',
+                '<?php array("c" => "value1", "a" => "hey how, am i doing", "b" => "value3");',
+            ],
+            [
+                '<?php array("a" => Arr::get($bla, "environment.bla"), "b" => "value3", "c" => "value1");',
+                '<?php array("c" => "value1", "a" => Arr::get($bla, "environment.bla"), "b" => "value3");',
+            ],
+            [
+                '<?php array(
+                        "a" => "value2",
+                    "b" => "value3",
+                    "c" => "value1"
+                );',
+                '<?php array(
+                        "c" => "value1",
+                    "a" => "value2",
+                    "b" => "value3"
+                );',
+            ],
+            [
+                '<?php [
+                        "a" => "value2",
+                    "b" => "value3",
+                    "c" => "value1"
+                    ];',
+                '<?php [
+                        "c" => "value1",
+                    "a" => "value2",
+                    "b" => "value3"
+                    ];',
+            ],
+            [
+                '<?php array("a" => array("l" => "10", "m" => "20"), "b" => "2", "d" => "5");',
+                '<?php array("b" => "2", "a" => array("m" => "20", "l" => "10"), "d" => "5");',
+            ],
+
+            [
+                '<?php ["a" => ["l" => "10", "m" => "20"], "b" => "2", "d" => "5"];',
+                '<?php ["b" => "2", "a" => ["m" => "20", "l" => "10"], "d" => "5"];',
+            ],
+            [
+                '<?php ["a" => array("l" => "10", "m" => "20"), "b" => "2", "d" => "5"];',
+                '<?php ["b" => "2", "a" => array("m" => "20", "l" => "10"), "d" => "5"];',
+            ],
+            [
+                '<?php ["a" => array(
+                        "l" => "10",
+                        "m" => "20"),
+                        "b" => "2",
+                        "d" => "5"
+                    ];',
+                '<?php ["b" => "2",
+                        "a" => array(
+                        "m" => "20",
+                        "l" => "10"),
+                        "d" => "5"
+                    ];',
+            ],
+            [
+                '<?php array("a" => array("l" => "10", "m" => "20", "z" => array("a" => "hame", "b" => "kame")), "b" => "2", "d" => "5");',
+                '<?php array("b" => "2", "a" => array("m" => "20", "z" => array("b" => "kame", "a" => "hame"), "l" => "10"), "d" => "5");',
+            ],
+            [
+                '<?php [1, "test1" => 0,2,3, "test2" => 0];',
+                '<?php [1, "test2" => 0,2,3, "test1" => 0];',
+            ],
+            [
+                '<?php [1,2,3, "test1" => 0, "test2" => 0];',
+                '<?php [1,2,3, "test2" => 0, "test1" => 0];',
+            ],
+            [
+                '<?php [["test1" => 0, "test2" => 0],["test1" => 0, "test2" => 0]];',
+                '<?php [["test2" => 0, "test1" => 0],["test2" => 0, "test1" => 0]];',
+            ],
+            [
+                '<?php [
+                        "a" => [
+                    "b" => [
+                        "c" => sprintf("%s %s", Arr::get($data, "name"), Arr::get($data, "last_name")),
+                        "d" => self::VARIABLE,
+                    ],
+                    "product" => [
+                        "Colour" => self::VARIABLE,
+                    ],
+                ],
+            ];',
+                '<?php [
+                        "a" => [
+                    "b" => [
+                        "d" => self::VARIABLE,
+                        "c" => sprintf("%s %s", Arr::get($data, "name"), Arr::get($data, "last_name")),
+                    ],
+                    "product" => [
+                        "Colour" => self::VARIABLE,
+                    ],
+                ],
+            ];',
+            ],
+            [
+                '<?php [
+                    "a" => $date->copy()->addDays(60)->setTime(12, 0, 0)->toDateTimeString(),
+
+                    // insert comment here.
+                    "b" => self::VARIABLE,
+                ];',
+                '<?php [
+                    "b" => self::VARIABLE,
+
+                    // insert comment here.
+                    "a" => $date->copy()->addDays(60)->setTime(12, 0, 0)->toDateTimeString(),
+                ];',
+            ],
+        ];
+    }
+}
