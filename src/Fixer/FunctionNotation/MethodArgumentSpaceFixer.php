ca36da50c7453133bf4f2f9a0e481314e84f5372
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2021-03-17 19:22:03 +0100

    DX: test relation between function_declaration and method_argument_space

--- src/Fixer/FunctionNotation/MethodArgumentSpaceFixer.php
+++ src/Fixer/FunctionNotation/MethodArgumentSpaceFixer.php
@@ -129,7 +129,7 @@ SAMPLE
      * {@inheritdoc}
      *
      * Must run before ArrayIndentationFixer.
-     * Must run after BracesFixer, CombineNestedDirnameFixer, ImplodeCallFixer, MethodChainingIndentationFixer, NoUselessSprintfFixer, PowToExponentiationFixer.
+     * Must run after BracesFixer, CombineNestedDirnameFixer, FunctionDeclarationFixer, ImplodeCallFixer, MethodChainingIndentationFixer, NoUselessSprintfFixer, PowToExponentiationFixer.
      */
     public function getPriority()
     {
