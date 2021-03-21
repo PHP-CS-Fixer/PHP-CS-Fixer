4e7704234f1ef90fabd82729cc178fc35008537b
Kuba Werłos <werlos@gmail.com>
2021-03-21 17:12:33 +0100

    Priority fix: SingleSpaceAfterConstructFixer must run before BracesFixer

--- tests/AutoReview/FixerFactoryTest.php
+++ tests/AutoReview/FixerFactoryTest.php
@@ -266,6 +266,7 @@ final class FixerFactoryTest extends TestCase
             [$fixers['single_import_per_statement'], $fixers['no_unused_imports']],
             [$fixers['single_import_per_statement'], $fixers['space_after_semicolon']],
             [$fixers['single_line_throw'], $fixers['concat_space']],
+            [$fixers['single_space_after_construct'], $fixers['braces']],
             [$fixers['single_trait_insert_per_statement'], $fixers['braces']],
             [$fixers['single_trait_insert_per_statement'], $fixers['space_after_semicolon']],
             [$fixers['standardize_increment'], $fixers['increment_style']],
