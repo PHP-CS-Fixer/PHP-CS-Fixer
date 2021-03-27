698b241f3f95a91db8527ed0e770b0d4480bb8c5
sy-records <52o@qq52o.cn>
2021-03-27 14:54:49 +0800

    do not override getConfigurationDefinition
    https://github.com/FriendsOfPHP/PHP-CS-Fixer/commit/f3a01feb7b321e1f5b1b9dd01aa2315710e1cc06

--- src/Fixer/Operator/OperatorLinebreakFixer.php
+++ src/Fixer/Operator/OperatorLinebreakFixer.php
@@ -95,27 +95,6 @@ function foo() {
     /**
      * {@inheritdoc}
      */
-    public function getConfigurationDefinition()
-    {
-        return new FixerConfigurationResolver([
-            (new FixerOptionBuilder('only_booleans', 'whether to limit operators to only boolean ones'))
-                ->setAllowedTypes(['bool'])
-                ->setDefault(false)
-                ->getOption(),
-            (new FixerOptionBuilder('position', 'whether to place operators at the beginning or at the end of the line'))
-                ->setAllowedValues(['beginning', 'end'])
-                ->setDefault($this->position)
-                ->getOption(),
-            (new FixerOptionBuilder('ignored_operators', 'Which operators to ignore'))
-                ->setAllowedTypes(['array'])
-                ->setDefault([])
-                ->getOption(),
-        ]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function configure(array $configuration = null)
     {
         parent::configure($configuration);
