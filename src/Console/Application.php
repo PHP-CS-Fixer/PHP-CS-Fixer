919e0c6cdbd4fa7c76e69ca9e9701407b728a645
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2021-04-06 20:38:47 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -34,7 +34,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    const VERSION = '2.18.5';
+    const VERSION = '2.18.6-DEV';
     const VERSION_CODENAME = 'Remote Void';
 
     /**
