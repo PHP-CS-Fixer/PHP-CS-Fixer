de44afe5583b419ccbdc3b796a9713d56831d0ec
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2021-05-03 22:15:14 +0200

    UX: Display deprecations to end-user

--- src/Utils.php
+++ src/Utils.php
@@ -24,6 +24,8 @@ use PhpCsFixer\Tokenizer\Token;
  */
 final class Utils
 {
+    private static $deprecations = [];
+
     /**
      * Calculate a bitmask for given constant names.
      *
@@ -195,6 +197,12 @@ final class Utils
             throw new $exceptionClass("{$message} This check was performed as `PHP_CS_FIXER_FUTURE_MODE` env var is set.");
         }
 
+        self::$deprecations[] = $message;
         @trigger_error($message, E_USER_DEPRECATED);
+    }
+
+    public static function getTriggeredDeprecations()
+    {
+        return self::$deprecations;
     }
 }
