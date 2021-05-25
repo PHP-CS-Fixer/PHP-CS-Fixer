f3ac3970fa2ce8db6956cd647e36b50edb1553ad
Kuba Werłos <werlos@gmail.com>
2021-05-25 16:22:10 +0200

    TokensAnalyzer - fix for union types

--- src/Tokenizer/TokensAnalyzer.php
+++ src/Tokenizer/TokensAnalyzer.php
@@ -570,7 +570,6 @@ final class TokensAnalyzer
                 T_SR => true,                   // >>
                 T_SR_EQUAL => true,             // >>=
                 T_XOR_EQUAL => true,            // ^=
-                CT::T_TYPE_ALTERNATION => true, // |
             ];
 
             // @TODO: drop condition when PHP 7.0+ is required
