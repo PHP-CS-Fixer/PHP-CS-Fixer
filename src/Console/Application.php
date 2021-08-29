dbf8ac24cd9000d7f238739d98cc5dcbb22ffd3b
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2021-08-29 21:48:25 +0200

    prepared the 3.0.3 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,7 +39,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.0.3-DEV';
+    public const VERSION = '3.0.3';
     public const VERSION_CODENAME = 'Constitution';
 
     /**
