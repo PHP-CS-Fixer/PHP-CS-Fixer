29f37e3356286297c025e6e71778e6da78f808e9
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2021-10-05 09:28:14 +0200

    prepared the 3.2.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,8 +39,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.2.0-DEV';
-    public const VERSION_CODENAME = 'River';
+    public const VERSION = '3.2.0';
+    public const VERSION_CODENAME = 'Mountains';
 
     /**
      * @var ToolInfo
