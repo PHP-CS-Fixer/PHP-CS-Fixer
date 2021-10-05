dca212adc0f12b081eb7566af0025f9ab1665c7b
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2021-10-05 09:37:12 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,8 +39,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.2.0';
-    public const VERSION_CODENAME = 'Mountains';
+    public const VERSION = '3.2.2-DEV';
+    public const VERSION_CODENAME = '';
 
     /**
      * @var ToolInfo
