e7a09208e14d05ce9899fa1fb5a5ca81fc1ffbda
Niklas Lampén <niklas@emptymonkey.com>
2021-10-29 13:14:42 +0300

    Make test style consistent for new tests

--- tests/RuleSet/RuleSetsTest.php
+++ tests/RuleSet/RuleSetsTest.php
@@ -197,7 +197,6 @@ Integration of %s.
     public function testRegisterRuleSetOK(): void
     {
         RuleSets::registerRuleSet('@MyRules', SampleRulesOk::class);
-
         static::assertContains('@MyRules', RuleSets::getSetDefinitionNames());
     }
 
