aedee685b43ecd7f0f50396ae989dda7d3f8c1a4
Niklas Lampén <niklas@emptymonkey.com>
2021-10-29 13:04:48 +0300

    Fix code style for SampleRulesOk, SampleRulesBad

--- tests/RuleSet/SampleRulesBad.php
+++ tests/RuleSet/SampleRulesBad.php
@@ -1,5 +1,16 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace PhpCsFixer\Tests\RuleSet;
 
@@ -26,9 +37,6 @@ class SampleRulesBad
         return 'Description';
     }
 
-    /**
-     * @inheritDoc
-     */
     public function getRules(): array
     {
         return [
