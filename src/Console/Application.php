66b2a5d4ff7fed2a51d21fdaf5087449a37c7861
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2021-11-15 18:29:23 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,7 +39,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.3.0';
+    public const VERSION = '3.3.1-DEV';
     public const VERSION_CODENAME = 'Trinacria';
 
     /**
