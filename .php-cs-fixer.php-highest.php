22510eacd3b20244d1d5b6c8a31f060679dd03be
Kuba Werłos <werlos@gmail.com>
2021-12-20 11:43:15 +0100

    DX: format dot files

--- .php-cs-fixer.php-highest.php
+++ .php-cs-fixer.php-highest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of PHP CS Fixer.
  *
@@ -13,10 +15,11 @@
 if (PHP_VERSION_ID < 80100 || PHP_VERSION_ID >= 80200) {
     fwrite(STDERR, "PHP CS Fixer's config for PHP-HIGHEST can be executed only on highest supported PHP version - 8.1.*.\n");
     fwrite(STDERR, "Running it on lower PHP version would prevent calling migration rules.\n");
+
     exit(1);
 }
 
-$config = require '.php-cs-fixer.dist.php';
+$config = require __DIR__.'/.php-cs-fixer.dist.php';
 
 $config->setRules(array_merge($config->getRules(), [
     '@PHP81Migration' => true,
--- .php-cs-fixer.php-lowest.php
+++ .php-cs-fixer.php-lowest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of PHP CS Fixer.
  *
@@ -13,10 +15,11 @@
 if (PHP_VERSION_ID < 70205 || PHP_VERSION_ID >= 70300) {
     fwrite(STDERR, "PHP CS Fixer's config for PHP-LOWEST can be executed only on lowest supported PHP version - ~7.2.5.\n");
     fwrite(STDERR, "Running it on higher PHP version would falsy expect more changes, eg `mixed` type on PHP 8.\n");
+
     exit(1);
 }
 
-$config = require '.php-cs-fixer.dist.php';
+$config = require __DIR__.'/.php-cs-fixer.dist.php';
 
 $config->getFinder()->notPath([
     // @TODO 4.0 change interface to be fully typehinted and remove the exceptions from this list
