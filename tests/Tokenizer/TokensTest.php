fd9d8b310b82997283f7d23137b5473967045cd7
Kuba Werłos <werlos@gmail.com>
2022-01-13 22:14:06 +0100

    Update tests/Tokenizer/TokensTest.php
    Co-authored-by: Dariusz Rumiński <dariusz.ruminski@gmail.com>

--- tests/Tokenizer/TokensTest.php
+++ tests/Tokenizer/TokensTest.php
@@ -362,6 +362,8 @@ PHP;
         yield [false, " <?php\n"];
         yield [false, "<?php\n?> "];
         yield [false, "<?php\n?><?php\n"];
+        yield [false, 'Hello<?php echo "World!"; ?>'];
+        yield [false, '<?php echo "Hello"; ?> World!'];
         // short open tag
         yield [(bool) ini_get('short_open_tag'), "<?\n"];
         yield [(bool) ini_get('short_open_tag'), "<?\n?>"];
