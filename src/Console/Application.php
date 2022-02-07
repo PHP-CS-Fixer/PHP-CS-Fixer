0f56327b246fe4ecd25476785dfe18003cfd485d
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2022-02-07 19:10:27 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,7 +39,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.6.0';
+    public const VERSION = '3.6.1-DEV';
     public const VERSION_CODENAME = 'Roe Deer';
 
     /**
