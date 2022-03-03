c459cb867e1571758e9ffd2e75b6b340c68c48fe
SpacePossum <possumfromspace@gmail.com>
2022-03-03 12:53:37 +0100

    even more problems

--- tests/Tokenizer/Analyzer/ReferenceAnalyzerTest.php
+++ tests/Tokenizer/Analyzer/ReferenceAnalyzerTest.php
@@ -38,10 +38,11 @@ final class ReferenceAnalyzerTest extends TestCase
     {
         $analyzer = new ReferenceAnalyzer();
 
-        $tokens = Tokens::fromCode('<?php function foo(&$bar = BAZ & QUX) {};');
+        $tokens = Tokens::fromCode('<?php function foo(&$bar = BAZ & QUX & H) {};');
 
         static::assertTrue($analyzer->isReference($tokens, 5));
         static::assertFalse($analyzer->isReference($tokens, 12));
+        static::assertFalse($analyzer->isReference($tokens, 16));
     }
 
     /**
@@ -82,6 +83,10 @@ class Foo {
 
         yield ['<?php function foo(int &$bar) {};'];
 
+        yield ['<?php $a = static function(int &$bar) {};'];
+
+        yield ['<?php $foo = fn (&$v) => $v;'];
+
         yield ['<?php function foo(string &$bar) {};'];
 
         yield ['<?php foreach($foos as &$foo) {}'];
@@ -89,6 +94,51 @@ class Foo {
         yield ['<?php foreach($foos as $key => &$foo) {}'];
 
         yield ['<?php function foo(?int &$bar) {};'];
+
+        yield ['<?php function &foo() {}'];
+
+        yield ['<?php function &foo(int &$a) {}'];
+
+        yield ['<?php function foo(&...$b) {}'];
+
+        yield ['<?php function replace(string $pattern, int $limit = -1, ?int &$count = null): string{}'];
+
+        yield ['<?php function replace(?int &$count){}'];
+
+        yield ['<?php function replace(?A\B &$count){}'];
+
+        yield ['<?php function A(\A\B\C\FOO & $bar){}'];
+
+        yield 'multiple references' => ['<?php function foo(&$a, array &$b, Bar &$c, \Bar &$d, \A\B &$e, A\B &$f) {}'];
+
+        yield 'multiple (not) references' => ['<?php function foo(&$a, array $b, Bar &$c, \Bar $d, \A\B &$e, A\B $f) {}'];
+
+        yield 'const `int`' => [
+            '<?php
+
+declare(strict_types=1);
+
+namespace A {
+    const FOO = 123;
+}
+
+namespace {
+    $bar = 0;
+    $a = 1;
+
+    echo A\FOO & $bar;
+
+    function foo($a, $b): void
+    {
+        var_dump($a, $b);
+    }
+
+    const int = 3;
+
+    foo($a, int & $bar);
+}
+',
+        ];
     }
 
     /**
@@ -146,12 +196,41 @@ class Foo {
         yield ['<?php if ($foo instanceof Bar & 0b01010101) {}'];
 
         yield ['<?php function foo(?int $bar = BAZ & QUX) {};'];
+
+        yield ['<?php echo FOO & BAR | BAZ;'];
+
+        yield ['<?php echo FOO & $bar;'];
+
+        yield ['<?php echo BAZ::FOO & $bar;'];
+
+        yield ['<?php $b = A & BAZ & QUX;'];
+
+        yield ['<?php function foo($bar = A & BAZ & QUX) {};'];
+
+        yield ['<?php foo($a, FOO & $bar);'];
+
+        yield ['<?php foo(FOO & $bar);'];
+
+        yield ['<?php foo(\FOO & $bar);'];
+
+        yield ['<?php foo(A\FOO & $bar);'];
+
+        yield ['<?php foo(\A\FOO & $bar);'];
+
+        yield ['<?php echo A\FOO & $bar;'];
+
+        yield ['<?php echo (A\FOO & $bar);'];
+
+        yield ['<?php echo \A\FOO & $bar;'];
+
+        yield ['<?php echo A\B\C\FOO & $bar;'];
+
+        yield ['<?php echo \A\B\C\FOO & $bar;'];
     }
 
     private function doTestCode(bool $expected, string $code): void
     {
         $analyzer = new ReferenceAnalyzer();
-
         $tokens = Tokens::fromCode($code);
 
         foreach ($tokens as $index => $token) {
