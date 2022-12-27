d0eee4fa99f2f3157b1a932ae287bcacf3a1299b
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2022-12-27 23:53:37 +0100

    Docs: PhpUnitTestCaseIndicator - fix docs

--- src/Indicator/PhpUnitTestCaseIndicator.php
+++ src/Indicator/PhpUnitTestCaseIndicator.php
@@ -62,7 +62,12 @@ final class PhpUnitTestCaseIndicator
     }
 
     /**
-     * @return \Generator array of [int start, int end] indices from sooner to later classes
+     * Returns an indices of PHPUnit classes in reverse appearance order.
+     * Order is important - it's reverted, so if we inject tokens into collection,
+     * we do it for bottom of file first, and then to the top of the file, so we
+     * mitigate risk of not visiting whole collcetion (final indices).
+     *
+     * @return \Generator array of [int start, int end] indices from later to earlier classes
      */
     public function findPhpUnitClasses(Tokens $tokens): \Generator
     {
