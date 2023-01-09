a2b38b4970a5cd572c24cebb58ebc5d06ba6d54b
Chris Smith <chris.smith@widerplan.com>
2023-01-09 17:14:51 +0000

    Add tests for PHPDoc alignment fixer failing with `\Closure`

--- tests/Fixer/Phpdoc/PhpdocAlignFixerTest.php
+++ tests/Fixer/Phpdoc/PhpdocAlignFixerTest.php
@@ -1454,4 +1454,44 @@ class Foo {}
              */
         ');
     }
+
+    public function testClosureTypesContainingBackslash(): void
+    {
+        $this->doTest('<?php
+            /**
+             * @var string                            $input
+             * @var \Closure                          $fn
+             * @var \Closure(bool):int                $fn2
+             * @var Closure                           $fn3
+             * @var Closure(string):string            $fn4
+             * @var array<string,array<string,mixed>> $data
+             */
+            /**
+             * @param string                            $input
+             * @param \Closure                          $fn
+             * @param \Closure(bool):int                $fn2
+             * @param Closure                           $fn3
+             * @param Closure(string):string            $fn4
+             * @param array<string,array<string,mixed>> $data
+             */
+            /**
+             * @var string                   $value
+             * @var \Closure(string): string $callback
+             * @var Closure(int): bool       $callback2
+             */
+            /**
+             * @param string                   $value
+             * @param \Closure(string): string $callback
+             * @param Closure(int): bool       $callback2
+             */
+            /**
+             * @var Closure(array<int,bool>): bool $callback1
+             * @var \Closure(string): string       $callback2
+             */
+            /**
+             * @param Closure(array<int,bool>): bool $callback1
+             * @param \Closure(string): string       $callback2
+             */
+        ');
+    }
 }
