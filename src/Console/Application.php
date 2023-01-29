438e62496ec05f7eaaa1cd366a1a738cc1aaf267
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-01-30 00:33:50 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,7 +39,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.14.0';
+    public const VERSION = '3.14.1-DEV';
     public const VERSION_CODENAME = 'Oliva';
 
     private ToolInfo $toolInfo;
