10ae36c6fd3f07ecb27e037b4337fdd68ad70df3
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-03-12 23:19:18 +0100

    ReadmeTest

--- tests/AutoReview/ReadmeTest.php
+++ tests/AutoReview/ReadmeTest.php
@@ -44,7 +44,7 @@ final class ReadmeTest extends TestCase
 
         static::assertEqualsCanonicalizing([
             '    if (\PHP_VERSION_ID === 80000) {'."\n",
-            '    if (\PHP_VERSION_ID < 70400 || \PHP_VERSION_ID >= 80200) {'."\n",
+            '    if (\PHP_VERSION_ID < 70400 || \PHP_VERSION_ID >= 80300) {'."\n",
         ], $phpVersionIdLines, 'Seems supported PHP versions changed in "./php-cs-fixer" - edit the README.md (and this test file) to match them!');
     }
 }
