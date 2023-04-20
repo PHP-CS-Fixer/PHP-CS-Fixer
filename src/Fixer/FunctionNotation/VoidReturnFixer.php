bfd06e75979c370109e85df8c8f5feb4a14637db
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2023-04-21 06:49:05 +0800

    bug: VoidReturnFixer - fix for intervening attributes (#6863)

--- src/Fixer/FunctionNotation/VoidReturnFixer.php
+++ src/Fixer/FunctionNotation/VoidReturnFixer.php
@@ -236,16 +236,26 @@ final class VoidReturnFixer extends AbstractFixer
      */
     private function findReturnAnnotations(Tokens $tokens, int $index): array
     {
-        do {
-            $index = $tokens->getPrevNonWhitespace($index);
-        } while ($tokens[$index]->isGivenKind([
+        $previousTokens = [
             T_ABSTRACT,
             T_FINAL,
             T_PRIVATE,
             T_PROTECTED,
             T_PUBLIC,
             T_STATIC,
-        ]));
+        ];
+
+        if (\defined('T_ATTRIBUTE')) { // @TODO: drop condition when PHP 8.0+ is required
+            $previousTokens[] = T_ATTRIBUTE;
+        }
+
+        do {
+            $index = $tokens->getPrevNonWhitespace($index);
+
+            if ($tokens[$index]->isGivenKind(CT::T_ATTRIBUTE_CLOSE)) {
+                $index = $tokens->getPrevTokenOfKind($index, [[T_ATTRIBUTE]]);
+            }
+        } while ($tokens[$index]->isGivenKind($previousTokens));
 
         if (!$tokens[$index]->isGivenKind(T_DOC_COMMENT)) {
             return [];
