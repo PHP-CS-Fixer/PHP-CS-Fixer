aa3955e815252fe5359c40f33296932ade65489d
Kuba Werłos <werlos@gmail.com>
2023-05-01 02:00:13 +0200

    bug: FullyQualifiedStrictTypesFixer - fix for FQCN type with class with the same name being imported (#6923)
    Co-authored-by: Greg Korba <greg@codito.dev>

--- tests/Fixer/Import/FullyQualifiedStrictTypesFixerTest.php
+++ tests/Fixer/Import/FullyQualifiedStrictTypesFixerTest.php
@@ -527,7 +527,7 @@ class SomeClass
     public function doSomething(
         \Ping\Something $something,
         Pung\Pang $other,
-        Pung $other1,
+        \Ping\Pong\Pung $other1,
         Pang\Pung $other2,
         Pyng\Pung\Pong $other3,
         \Foo\Bar\Baz\Buz $other4
@@ -575,6 +575,18 @@ namespace {
     function withReference(\Exception &$e) {}
     }
 ',
+        ];
+
+        yield 'Test FQCN is not removed when class with the same name, but different namespace, is imported' => [
+            '<?php namespace Foo;
+                use Bar\TheClass;
+                class Test
+                {
+                    public function __construct(
+                        \Foo\TheClass $x
+                    ) {}
+                }
+            ',
         ];
     }
 
