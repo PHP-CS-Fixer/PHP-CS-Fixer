7fc8eeff977d5cd1a5b2808c6d21845ba77fab02
Kuba Werłos <werlos@gmail.com>
2023-05-01 15:48:14 +0200

    minor: do not add empty line in PHPDoc when adding annotation in PHPUnit class (#6928)

--- src/Fixer/PhpUnit/PhpUnitInternalClassFixer.php
+++ src/Fixer/PhpUnit/PhpUnitInternalClassFixer.php
@@ -84,7 +84,6 @@ final class PhpUnitInternalClassFixer extends AbstractPhpUnitFixer implements Wh
             $tokens,
             $classIndex,
             'internal',
-            true,
             ['internal']
         );
     }
