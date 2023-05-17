30c3c3b23e699eedae87029f09ea198420539840
Julien Deniau <julien@deniau.me>
2023-05-17 22:02:24 +0200

    Update `@PER-CS2.0` to match short closure space requirement

--- src/RuleSet/Sets/PERCS2x0Set.php
+++ src/RuleSet/Sets/PERCS2x0Set.php
@@ -35,6 +35,9 @@ final class PERCS2x0Set extends AbstractRuleSetDescription
         return [
             '@PER-CS1.0' => true,
             'concat_space' => ['spacing' => 'one'],
+            'function_declaration' => [
+                'closure_fn_spacing' => 'none',
+            ],
             'method_argument_space' => true,
             'single_line_empty_body' => true,
         ];
