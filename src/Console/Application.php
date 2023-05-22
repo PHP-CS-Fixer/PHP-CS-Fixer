3f0ed862f22386c55a767461ef5083bddceeed79
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-05-22 21:59:32 +0200

    prepared the 3.17.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,8 +39,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.16.1-DEV';
-    public const VERSION_CODENAME = 'BoY42';
+    public const VERSION = '3.17.0';
+    public const VERSION_CODENAME = 'Brazilian Kangaroo';
 
     private ToolInfo $toolInfo;
 
