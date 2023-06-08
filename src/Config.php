608d14624c6b077094e226b6cc0aeffdf5c62f78
Niklas Lampén <niklas.lampen@supermetrics.com>
2023-06-08 16:14:26 +0300

    Fix description of registerCustomRuleSets

--- src/Config.php
+++ src/Config.php
@@ -191,7 +191,7 @@ class Config implements ConfigInterface, ParallelAwareConfigInterface, Unsupport
     }
 
     /**
-     * Registers custom rule sets to be used the same way as built-in rule sets
+     * Registers custom rule sets to be used the same way as built-in rule sets.
      *
      * `$ruleSets` must follow `'@RuleName' => RuleSetDescriptionInterface::class` convention.
      *
