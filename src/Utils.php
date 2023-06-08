cb25d90c2d6259af3fd98adefcedb036d0942b79
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2023-06-08 09:45:52 +0200

    DX: officially deprecate internal Utils anti-pattern class (#7039)
    Co-authored-by: Greg Korba <wirone@gmail.com>

--- src/Utils.php
+++ src/Utils.php
@@ -23,6 +23,8 @@ use PhpCsFixer\Tokenizer\Token;
  * @author Odín del Río <odin.drp@gmail.com>
  *
  * @internal
+ *
+ * @deprecated This is a God Class anti-pattern. Don't expand it. It is fine to use logic that is already here (that's why we don't trigger deprecation warnings), but over time logic should be moved to dedicated, single-responsibility classes.
  */
 final class Utils
 {
