b123395c9fa3a70801f816f13606c0f3a7ada8df
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-06-19 00:25:45 +0200

    prepared the 3.18.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,8 +39,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.17.1-DEV';
-    public const VERSION_CODENAME = 'Brazilian Kangaroo';
+    public const VERSION = '3.18.0';
+    public const VERSION_CODENAME = 'Triangle';
 
     private ToolInfo $toolInfo;
 
