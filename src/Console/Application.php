5bff214027e7e6796384ceae964cdd573968341e
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-06-25 18:44:14 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,7 +39,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.19.2';
+    public const VERSION = '3.19.3-DEV';
     public const VERSION_CODENAME = 'Triangle';
 
     private ToolInfo $toolInfo;
