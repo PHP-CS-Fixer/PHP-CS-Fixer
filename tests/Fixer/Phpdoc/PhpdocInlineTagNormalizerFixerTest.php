295f3be79b6f64622d65c76bbef824f47e6965ba
Kuba Werłos <werlos@gmail.com>
2023-08-12 17:34:02 +0200

    Add fix

--- tests/Fixer/Phpdoc/PhpdocInlineTagNormalizerFixerTest.php
+++ tests/Fixer/Phpdoc/PhpdocInlineTagNormalizerFixerTest.php
@@ -168,14 +168,6 @@ final class PhpdocInlineTagNormalizerFixerTest extends AbstractFixerTestCase
         yield 'do not rename tags' => [
             '<?php
                 /**
-                 * {@iD ontWantToBeChanged}
-                 * @Route("/conventions/{@id cc}", name="api_v1_convention_read_by_idcc")
-                 *
-                 * {@id}
-                 */
-            ',
-            '<?php
-                /**
                  * { @iDontWantToBeChanged }
                  * @Route("/conventions/@{idcc}", name="api_v1_convention_read_by_idcc")
                  *
