35af3cbbacfa91e164b252a28ec0b644f1ed4e78
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-08-14 14:27:35 +0200

    prepared the 3.23.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,8 +39,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.22.1-DEV';
-    public const VERSION_CODENAME = 'Chips & Pizza';
+    public const VERSION = '3.23.0';
+    public const VERSION_CODENAME = 'First Steps';
 
     private ToolInfo $toolInfo;
 
