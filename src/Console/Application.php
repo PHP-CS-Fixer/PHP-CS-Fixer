e0fc6a6139936f0e3129a7663d19279c3b13f1be
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-08-14 14:28:39 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,7 +39,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.23.0';
+    public const VERSION = '3.23.1-DEV';
     public const VERSION_CODENAME = 'First Steps';
 
     private ToolInfo $toolInfo;
