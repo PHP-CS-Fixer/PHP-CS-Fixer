0fb2d17f64e65dbe5e4db912967fb4f1e582757c
Greg Korba <greg@codito.dev>
2023-08-29 15:14:45 +0200

    Include suggested Doctrine packages in PHAR build

--- dev-tools/build.sh
+++ dev-tools/build.sh
@@ -4,6 +4,10 @@ set -eu
 # ensure that deps will work on lowest supported PHP version
 composer config platform.php 7.4
 
+# Include Doctrine Annotations and Lexer in the PHAR build (for people that use `@DoctrineAnnotation rule set).
+# Only add requirement to the composer.json, it will be installed later.
+composer require doctrine/annotations:"^2" doctrine/lexer:"^2 || ^3" --no-update
+
 # install package deps without dev-deps / remove already installed dev-deps
 # box can ignore dev-deps, but dev-deps, when installed, may lower version of prod-deps
 composer update --no-interaction --no-progress --no-dev --prefer-stable --optimize-autoloader
