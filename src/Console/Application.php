e73ccaae1208f017bb7860986eebb3da48bd25d6
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-09-17 16:37:54 +0200

    prepared the 3.27.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,8 +39,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.26.2-DEV';
-    public const VERSION_CODENAME = 'Crank Cake';
+    public const VERSION = '3.27.0';
+    public const VERSION_CODENAME = 'Jump & Run';
 
     private ToolInfo $toolInfo;
 
