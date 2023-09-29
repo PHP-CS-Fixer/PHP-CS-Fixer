48da0d66c27e8bafdbd845ff52942cd256b7ac90
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-09-29 14:40:52 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -39,7 +39,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.31.0';
+    public const VERSION = '3.31.1-DEV';
     public const VERSION_CODENAME = 'Jump & Run';
 
     private ToolInfo $toolInfo;
