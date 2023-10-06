4d399aafeb38cd86d4f4ed8d170fcbace2438552
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2023-10-06 23:52:33 +0200

    chore: no need for `phpunitgoodpractices/traits` anymore (#7362)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,7 +15,6 @@ declare(strict_types=1);
 namespace PhpCsFixer\Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
-use PHPUnitGoodPractices\Polyfill\PolyfillTrait;
 use PHPUnitGoodPractices\Traits\ExpectationViaCodeOverAnnotationTrait;
 use PHPUnitGoodPractices\Traits\ExpectOverSetExceptionTrait;
 use PHPUnitGoodPractices\Traits\IdentityOverEqualityTrait;
@@ -45,7 +44,6 @@ abstract class TestCase extends InterimTestCase
     use ExpectDeprecationTrait;
     use ExpectOverSetExceptionTrait;
     use IdentityOverEqualityTrait;
-    use PolyfillTrait;
     use ProphecyOverMockObjectTrait;
     use ProphesizeOnlyInterfaceTrait;
 }
