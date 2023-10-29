3291e9afed6bb99a41b352c91885030c8a42c1cf
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-10-29 21:51:55 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -40,7 +40,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.37.1';
+    public const VERSION = '3.37.2-DEV';
     public const VERSION_CODENAME = 'ParaCache';
 
     private ToolInfo $toolInfo;
