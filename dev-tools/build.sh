e9e9a2d9eca28b83fb49cfa867be854f84d8f915
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-11-03 01:33:22 +0100

    build.sh - same shell flags as everywhere else

--- dev-tools/build.sh
+++ dev-tools/build.sh
@@ -6,7 +6,7 @@ composer config platform.php 7.4
 
 # install package deps without dev-deps / remove already installed dev-deps
 # box can ignore dev-deps, but dev-deps, when installed, may lower version of prod-deps
-composer update --no-interaction --no-progress --optimize-autoloader --no-scripts --no-dev
+composer update --optimize-autoloader --no-interaction --no-progress --no-scripts --no-dev
 composer info -D | sort
 
 composer show -d dev-tools humbug/box -q || composer update -d dev-tools --no-interaction --no-progress
@@ -16,4 +16,4 @@ dev-tools/vendor/bin/box compile
 
 # revert changes to composer
 git checkout composer.json
-composer update --no-interaction --no-progress -q
+composer update --optimize-autoloader --no-interaction --no-progress --no-scripts -q
