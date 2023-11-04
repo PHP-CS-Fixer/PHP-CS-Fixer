74f6ba0c4d4fbe5a5bea0b8a1e5ae42741cf0c45
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2023-11-04 22:40:25 +0100

    CI: general restructure (#7407)
    Co-authored-by: Greg Korba <wirone@gmail.com>

--- tests/Smoke/AbstractSmokeTestCase.php
+++ tests/Smoke/AbstractSmokeTestCase.php
@@ -27,14 +27,4 @@ use PhpCsFixer\Tests\TestCase;
  *
  * @group covers-nothing
  */
-abstract class AbstractSmokeTestCase extends TestCase
-{
-    protected static function markTestSkippedOrFail(string $message): void
-    {
-        if (getenv('PHP_CS_FIXER_TEST_ALLOW_SKIPPING_SMOKE_TESTS')) {
-            self::markTestSkipped($message);
-        }
-
-        self::fail($message.' Failing as test is obligatory because of `PHP_CS_FIXER_TEST_ALLOW_SKIPPING_SMOKE_TESTS=0`.');
-    }
-}
+abstract class AbstractSmokeTestCase extends TestCase {}
