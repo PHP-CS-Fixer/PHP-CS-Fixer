c0efa687fca34be699e6e7afb99bf9220eb95334
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-11-15 21:30:59 +0100

    DX: GroupImportFixer - usort callback should return -1|0|1

--- src/Fixer/Import/GroupImportFixer.php
+++ src/Fixer/Import/GroupImportFixer.php
@@ -89,7 +89,7 @@ final class GroupImportFixer extends AbstractFixer
             $namespaceA = $this->getNamespaceNameWithSlash($a);
             $namespaceB = $this->getNamespaceNameWithSlash($b);
 
-            $namespaceDifference = \strlen($namespaceA) - \strlen($namespaceB);
+            $namespaceDifference = \strlen($namespaceA) <=> \strlen($namespaceB);
 
             return 0 !== $namespaceDifference ? $namespaceDifference : strcmp($a->getFullName(), $b->getFullName());
         });
