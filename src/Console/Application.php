ab4b588a7e57b898f3775ebeb14915a30bc53289
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-11-22 12:20:25 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -40,7 +40,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.39.0';
+    public const VERSION = '3.39.1-DEV';
     public const VERSION_CODENAME = 'Getting Ready';
 
     private ToolInfo $toolInfo;
