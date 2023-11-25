c72756a792092ac52d07416ccde57eadeb7098a6
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-11-25 19:38:49 +0100

    chore: officially support PHP 8.3

--- .php-cs-fixer.php-highest.php
+++ .php-cs-fixer.php-highest.php
@@ -12,8 +12,8 @@ declare(strict_types=1);
  * with this source code in the file LICENSE.
  */
 
-if (PHP_VERSION_ID < 8_02_00 || PHP_VERSION_ID >= 8_03_00) {
-    fwrite(STDERR, "PHP CS Fixer's config for PHP-HIGHEST can be executed only on highest supported PHP version - 8.2.*.\n");
+if (PHP_VERSION_ID < 8_03_00 || PHP_VERSION_ID >= 8_04_00) {
+    fwrite(STDERR, "PHP CS Fixer's config for PHP-HIGHEST can be executed only on highest supported PHP version - 8.3.*.\n");
     fwrite(STDERR, "Running it on lower PHP version would prevent calling migration rules.\n");
 
     exit(1);
@@ -22,7 +22,7 @@ if (PHP_VERSION_ID < 8_02_00 || PHP_VERSION_ID >= 8_03_00) {
 $config = require __DIR__.'/.php-cs-fixer.dist.php';
 
 $config->setRules(array_merge($config->getRules(), [
-    '@PHP82Migration' => true,
+    '@PHP83Migration' => true,
     '@PHP80Migration:risky' => true,
 ]));
 
