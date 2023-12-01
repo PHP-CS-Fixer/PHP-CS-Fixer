97b84c554c2de7213a2b96445fb50ca2541d1517
Greg Korba <greg@codito.dev>
2023-12-01 23:21:37 +0100

    Make PHPStan fail to see the effect

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -25,9 +25,6 @@ final class Cache implements CacheInterface
 {
     private SignatureInterface $signature;
 
-    /**
-     * @var array<string, string>
-     */
     private array $hashes = [];
 
     public function __construct(SignatureInterface $signature)
