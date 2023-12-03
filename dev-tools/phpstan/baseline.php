f3ec3456d0aef884b075f317eda6230f8239a272
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-12-03 21:44:53 +0100

    update stan and baseline

--- dev-tools/phpstan/baseline.php
+++ dev-tools/phpstan/baseline.php
@@ -2,56 +2,6 @@
 
 $ignoreErrors = [];
 $ignoreErrors[] = [
-	'message' => '#^Property PhpCsFixer\\\\Console\\\\Command\\\\CheckCommand\\:\\:\\$defaultDescription has no type specified\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Console/Command/CheckCommand.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Property PhpCsFixer\\\\Console\\\\Command\\\\CheckCommand\\:\\:\\$defaultName has no type specified\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Console/Command/CheckCommand.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Property PhpCsFixer\\\\Console\\\\Command\\\\DescribeCommand\\:\\:\\$defaultName has no type specified\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Console/Command/DescribeCommand.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Property PhpCsFixer\\\\Console\\\\Command\\\\DocumentationCommand\\:\\:\\$defaultName has no type specified\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Console/Command/DocumentationCommand.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Property PhpCsFixer\\\\Console\\\\Command\\\\FixCommand\\:\\:\\$defaultDescription has no type specified\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Console/Command/FixCommand.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Property PhpCsFixer\\\\Console\\\\Command\\\\FixCommand\\:\\:\\$defaultName has no type specified\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Console/Command/FixCommand.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Property PhpCsFixer\\\\Console\\\\Command\\\\HelpCommand\\:\\:\\$defaultName has no type specified\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Console/Command/HelpCommand.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Property PhpCsFixer\\\\Console\\\\Command\\\\ListFilesCommand\\:\\:\\$defaultName has no type specified\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Console/Command/ListFilesCommand.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Property PhpCsFixer\\\\Console\\\\Command\\\\ListSetsCommand\\:\\:\\$defaultName has no type specified\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Console/Command/ListSetsCommand.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Property PhpCsFixer\\\\Console\\\\Command\\\\SelfUpdateCommand\\:\\:\\$defaultName has no type specified\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Console/Command/SelfUpdateCommand.php',
-];
-$ignoreErrors[] = [
 	'message' => '#^Short ternary operator is not allowed\\. Use null coalesce operator if applicable or consider using long ternary\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../../src/Console/Command/SelfUpdateCommand.php',
@@ -505,11 +455,6 @@ $ignoreErrors[] = [
 	'message' => '#^Variable \\$index might not be defined\\.$#',
 	'count' => 5,
 	'path' => __DIR__ . '/../../src/Tokenizer/Tokens.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Parameter \\#1 \\$level of method Symfony\\\\Component\\\\Console\\\\Output\\\\Output\\:\\:setVerbosity\\(\\) expects 16\\|32\\|64\\|128\\|256, int given\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../tests/Console/Output/ErrorOutputTest.php',
 ];
 $ignoreErrors[] = [
 	'message' => '#^Parameter \\#1 \\$index \\(int\\) of method PhpCsFixer\\\\Tests\\\\Test\\\\TokensWithObservedTransformers\\:\\:offsetSet\\(\\) should be contravariant with parameter \\$offset \\(int\\|null\\) of method ArrayAccess\\<int,PhpCsFixer\\\\Tokenizer\\\\Token\\|null\\>\\:\\:offsetSet\\(\\)$#',
