ce68d1e98ba8958de4d6e268ebfe5993b15adf9d
Greg Korba <greg@codito.dev>
2023-12-07 17:54:33 +0100

    Support for FQCNs in rules configuration
    It's fully backward compatible, but allows using FQCNs as a keys in rules' config, instead of short names. This enables code navigation, usage discoverability etc.
    
     Conflicts:
    	src/Documentation/RuleSetDocumentationGenerator.php
    	src/RuleSet/Sets/PhpCsFixerSet.php

--- src/RuleSet/Sets/PhpCsFixerSet.php
+++ src/RuleSet/Sets/PhpCsFixerSet.php
@@ -14,7 +14,9 @@ declare(strict_types=1);
 
 namespace PhpCsFixer\RuleSet\Sets;
 
+use PhpCsFixer\Fixer;
 use PhpCsFixer\RuleSet\AbstractRuleSetDefinition;
+use PhpCsFixer\RuleSet\RuleSet;
 
 /**
  * @internal
@@ -25,10 +27,10 @@ final class PhpCsFixerSet extends AbstractRuleSetDefinition
 {
     public function getRules(): array
     {
-        return [
+        return RuleSet::normaliseConfig([
             '@PER-CS' => true,
             '@Symfony' => true,
-            'blank_line_before_statement' => [
+            Fixer\Whitespace\BlankLineBeforeStatementFixer::class => [
                 'statements' => [
                     'break',
                     'case',
@@ -50,25 +52,25 @@ final class PhpCsFixerSet extends AbstractRuleSetDefinition
                     'yield_from',
                 ],
             ],
-            'combine_consecutive_issets' => true,
-            'combine_consecutive_unsets' => true,
-            'empty_loop_body' => true,
-            'explicit_indirect_variable' => true,
-            'explicit_string_variable' => true,
-            'fully_qualified_strict_types' => [
+            Fixer\LanguageConstruct\CombineConsecutiveIssetsFixer::class => true,
+            Fixer\LanguageConstruct\CombineConsecutiveUnsetsFixer::class => true,
+            Fixer\ControlStructure\EmptyLoopBodyFixer::class => true,
+            Fixer\LanguageConstruct\ExplicitIndirectVariableFixer::class => true,
+            Fixer\StringNotation\ExplicitStringVariableFixer::class => true,
+            Fixer\Import\FullyQualifiedStrictTypesFixer::class => [
                 'import_symbols' => true,
             ],
-            'heredoc_to_nowdoc' => true,
-            'method_argument_space' => [
+            Fixer\StringNotation\HeredocToNowdocFixer::class => true,
+            Fixer\FunctionNotation\MethodArgumentSpaceFixer::class => [
                 'after_heredoc' => true,
                 'on_multiline' => 'ensure_fully_multiline',
             ],
-            'method_chaining_indentation' => true,
-            'multiline_comment_opening_closing' => true,
-            'multiline_whitespace_before_semicolons' => [
+            Fixer\Whitespace\MethodChainingIndentationFixer::class => true,
+            Fixer\Comment\MultilineCommentOpeningClosingFixer::class => true,
+            Fixer\Semicolon\MultilineWhitespaceBeforeSemicolonsFixer::class => [
                 'strategy' => 'new_line_for_chained_calls',
             ],
-            'no_extra_blank_lines' => [
+            Fixer\Whitespace\NoExtraBlankLinesFixer::class => [
                 'tokens' => [
                     'attribute',
                     'break',
@@ -85,35 +87,35 @@ final class PhpCsFixerSet extends AbstractRuleSetDefinition
                     'use',
                 ],
             ],
-            'no_superfluous_elseif' => true,
-            'no_superfluous_phpdoc_tags' => [
+            Fixer\ControlStructure\NoSuperfluousElseifFixer::class => true,
+            Fixer\Phpdoc\NoSuperfluousPhpdocTagsFixer::class => [
                 'allow_hidden_params' => true,
                 'allow_mixed' => true, // @TODO revalidate to keep `true` or unify into `false`
                 'remove_inheritdoc' => true,
             ],
-            'no_whitespace_in_empty_array' => true,
-            'operator_linebreak' => true,
-            'ordered_class_elements' => true,
-            'ordered_types' => [
+            Fixer\ArrayNotation\NoWhitespaceInEmptyArrayFixer::class => true,
+            Fixer\Operator\OperatorLinebreakFixer::class => true,
+            Fixer\ClassNotation\OrderedClassElementsFixer::class => true,
+            Fixer\ClassNotation\OrderedTypesFixer::class => [
                 'null_adjustment' => 'always_last',
             ],
-            'php_unit_data_provider_method_order' => true,
-            'php_unit_internal_class' => true,
-            'php_unit_test_class_requires_covers' => true,
-            'phpdoc_add_missing_param_annotation' => true,
-            'phpdoc_no_duplicate_types' => true,
-            'phpdoc_no_empty_return' => true,
-            'phpdoc_order_by_value' => true,
-            'phpdoc_types_order' => true,
-            'return_assignment' => true,
-            'self_static_accessor' => true,
-            'single_line_comment_style' => true,
-            'single_line_empty_body' => true,
-            'single_line_throw' => false,
-            'string_implicit_backslashes' => true,
-            'trailing_comma_in_multiline' => ['after_heredoc' => true, 'elements' => ['array_destructuring', 'arrays']],
-            'whitespace_after_comma_in_array' => ['ensure_single_space' => true],
-        ];
+            Fixer\PhpUnit\PhpUnitDataProviderMethodOrderFixer::class => true,
+            Fixer\PhpUnit\PhpUnitInternalClassFixer::class => true,
+            Fixer\PhpUnit\PhpUnitTestClassRequiresCoversFixer::class => true,
+            Fixer\Phpdoc\PhpdocAddMissingParamAnnotationFixer::class => true,
+            Fixer\Phpdoc\PhpdocNoDuplicateTypesFixer::class => true,
+            Fixer\Phpdoc\PhpdocNoEmptyReturnFixer::class => true,
+            Fixer\Phpdoc\PhpdocOrderByValueFixer::class => true,
+            Fixer\Phpdoc\PhpdocTypesOrderFixer::class => true,
+            Fixer\ReturnNotation\ReturnAssignmentFixer::class => true,
+            Fixer\ClassNotation\SelfStaticAccessorFixer::class => true,
+            Fixer\Comment\SingleLineCommentStyleFixer::class => true,
+            Fixer\Basic\SingleLineEmptyBodyFixer::class => true,
+            Fixer\FunctionNotation\SingleLineThrowFixer::class => false,
+            Fixer\StringNotation\StringImplicitBackslashesFixer::class => true,
+            Fixer\ControlStructure\TrailingCommaInMultilineFixer::class => ['after_heredoc' => true, 'elements' => ['array_destructuring', 'arrays']],
+            Fixer\ArrayNotation\WhitespaceAfterCommaInArrayFixer::class => ['ensure_single_space' => true],
+        ]);
     }
 
     public function getDescription(): string
