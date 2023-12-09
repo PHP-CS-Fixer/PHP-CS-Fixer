ea3804ea7af7662f58bad631b801d2637885aaa5
Kuba Werłos <werlos@gmail.com>
2023-12-09 12:33:56 +0100

    Refactor DescribeCommandTest to use anonymous class for "testCommandDescribesCustomFixer"

--- tests/Fixtures/DescribeCommand/.php-cs-fixer.fixture.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use PhpCsFixer\Config;
-use PhpCsFixer\Finder;
-use PhpCsFixer\Tests\Fixtures\DescribeCommand\DescribeFixtureFixer;
-
-return (new Config())
-    ->setFinder((new Finder())->in(__DIR__))
-    ->registerCustomFixers([new DescribeFixtureFixer()]);
