f9539db4cc90531212286fe6f16351694a20f0d9
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-12-11 23:44:12 +0100

    phpstan

--- tests/Fixer/LanguageConstruct/ClassKeywordFixerTest.php
+++ tests/Fixer/LanguageConstruct/ClassKeywordFixerTest.php
@@ -31,6 +31,9 @@ final class ClassKeywordFixerTest extends AbstractFixerTestCase
         $this->doTest($expected, $input);
     }
 
+    /**
+     * @return iterable<array{0: string, 1?: ?string}>
+     */
     public static function provideFixCases(): iterable
     {
         yield [
