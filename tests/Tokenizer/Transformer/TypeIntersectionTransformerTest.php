7869dedd74d713e14f6d66f95e0da20a54968efc
Kuba Werłos <werlos@gmail.com>
2023-12-16 15:20:32 +0100

    Fix

--- tests/Tokenizer/Transformer/TypeIntersectionTransformerTest.php
+++ tests/Tokenizer/Transformer/TypeIntersectionTransformerTest.php
@@ -63,26 +63,28 @@ final class TypeIntersectionTransformerTest extends AbstractTransformerTestCase
             ',
         ];
 
-        yield 'ensure T_AMPERSAND_FOLLOWED_BY_VAR_OR_VARARG is not modified' => [
-            '<?php $a = $b&$c;',
-            [
-                6 => T_AMPERSAND_FOLLOWED_BY_VAR_OR_VARARG,
-            ],
-        ];
+        if (\defined('T_AMPERSAND_FOLLOWED_BY_VAR_OR_VARARG')) { // @TODO: drop condition when PHP 8.1+ is required
+            yield 'ensure T_AMPERSAND_FOLLOWED_BY_VAR_OR_VARARG is not modified' => [
+                '<?php $a = $b&$c;',
+                [
+                    6 => T_AMPERSAND_FOLLOWED_BY_VAR_OR_VARARG,
+                ],
+            ];
 
-        yield 'do not fix, close/open' => [
-            '<?php fn() => 0 ?><?php $a = FOO|BAR|BAZ&$x;',
-            [
-                20 => T_AMPERSAND_FOLLOWED_BY_VAR_OR_VARARG,
-            ],
-        ];
+            yield 'do not fix, close/open' => [
+                '<?php fn() => 0 ?><?php $a = FOO|BAR|BAZ&$x;',
+                [
+                    20 => T_AMPERSAND_FOLLOWED_BY_VAR_OR_VARARG,
+                ],
+            ];
 
-        yield 'do not fix, foreach' => [
-            '<?php while(foo()){} $a = FOO|BAR|BAZ&$x;',
-            [
-                19 => T_AMPERSAND_FOLLOWED_BY_VAR_OR_VARARG,
-            ],
-        ];
+            yield 'do not fix, foreach' => [
+                '<?php while(foo()){} $a = FOO|BAR|BAZ&$x;',
+                [
+                    19 => T_AMPERSAND_FOLLOWED_BY_VAR_OR_VARARG,
+                ],
+            ];
+        }
 
         yield 'arrow function' => [
             '<?php $a = fn(int&null $item): int&null => $item * 2;',
