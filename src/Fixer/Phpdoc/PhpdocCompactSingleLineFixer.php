2fd9a477a1cc8cb136e06b14daf6b081bffab7f4
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2023-12-18 22:18:30 +0100

    Add rule to convert single line PhpDoc to compact notation

---
+++ src/Fixer/Phpdoc/PhpdocCompactSingleLineFixer.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Fixer\Phpdoc;
+
+use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Preg;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+
+/**
+ * @author Frank Prins
+ */
+final class PhpdocCompactSingleLineFixer extends AbstractFixer
+{
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition(
+            'PHPDoc blocks should be single line when only one line of content is present',
+            [new CodeSample("<?php /**\n * This is only one line of content\n  */\n")]
+        );
+    }
+
+    public function getPriority(): int
+    {
+        return 4;
+    }
+
+    public function isCandidate(Tokens $tokens): bool
+    {
+        return $tokens->isTokenKindFound(T_DOC_COMMENT);
+    }
+
+    protected function applyFix(\SplFileInfo $file, Tokens $tokens): void
+    {
+        foreach ($tokens as $index => $token) {
+            if (!$token->isGivenKind(T_DOC_COMMENT)) {
+                continue;
+            }
+
+            if (Preg::match('#^/\*\*\n*\s*\*.*\n\*/$#', $token->getContent())) {
+                $tokens[$index] = new Token([T_DOC_COMMENT, Preg::replace('#^/\*\*\n*\s*\*(.*)\n\*/$#', '/** $1 */', $token->getContent())]);
+            }
+        }
+    }
+}
---
+++ tests/Fixer/Phpdoc/PhpdocCompactSingleLineFixerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Tests\Fixer\Phpdoc;
+
+use PhpCsFixer\Tests\Test\AbstractFixerTestCase;
+
+/**
+ * @author Frank Prins
+ *
+ * @internal
+ *
+ * @covers \PhpCsFixer\Fixer\Phpdoc\PhpdocCompactSingleLineFixer
+ */
+final class PhpdocCompactSingleLineFixerTest extends AbstractFixerTestCase
+{
+    /** @dataProvider provideFixCases */
+    public function testFix(string $expected, ?string $input = null): void
+    {
+        $this->doTest($expected, $input);
+    }
+
+    public static function provideFixCases(): iterable
+    {
+        yield ['<?php /** @var Foo $foo */'];
+
+        yield ['<?php /** bar */'];
+
+        yield [
+            '<?php
+/**
+ * @var Foo $foo
+ */
+            ',
+            '<?php
+/** @var Foo @foo */
+            '
+        ];
+
+        yield [
+            '<?php
+/**
+ * bar
+ */
+            ',
+            '<?php
+/** bar */
+            '
+        ];
+
+        yield [
+            '<?php
+/**
+ * @var Foo $foo
+ * @var Bar $bar
+ */
+            '
+        ];
+    }
+}
