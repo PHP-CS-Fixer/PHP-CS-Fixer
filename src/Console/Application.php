f6f9b7fea94b206883e96857370b72e9ff30f0ac
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2023-12-29 21:21:43 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -40,7 +40,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class Application extends BaseApplication
 {
-    public const VERSION = '3.44.0';
+    public const VERSION = '3.44.1-DEV';
     public const VERSION_CODENAME = 'Three Keys';
 
     private ToolInfo $toolInfo;
