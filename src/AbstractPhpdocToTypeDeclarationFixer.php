3cf87eae174c9be75aad72ca2ff54425fd2a3c30
Andreas Möller <am@localheinz.com>
2024-01-04 20:08:13 +0100

    feat: Enable union_types by default on PHP 8.0.0 and above
    Co-authored-by: Dariusz Rumiński <dariusz.ruminski@gmail.com>

--- src/AbstractPhpdocToTypeDeclarationFixer.php
+++ src/AbstractPhpdocToTypeDeclarationFixer.php
@@ -77,7 +77,7 @@ abstract class AbstractPhpdocToTypeDeclarationFixer extends AbstractFixer implem
                 ->getOption(),
             (new FixerOptionBuilder('union_types', 'Fix also union types.'))
                 ->setAllowedTypes(['bool'])
-                ->setDefault(true)
+                ->setDefault(PHP_VERSION_ID >= 8_00_00)
                 ->getOption(),
         ]);
     }
