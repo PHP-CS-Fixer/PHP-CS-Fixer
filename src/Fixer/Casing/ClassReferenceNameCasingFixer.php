12c42b7d2e899383ea93b1efceb417dfbd0adb64
ErickSkrauch <erickskrauch@ely.by>
2024-01-08 16:40:37 +0100

    fix: `ClassReferenceNameCasingFixer` capitalizes the property name after the nullsafe operator (#7696)

--- src/Fixer/Casing/ClassReferenceNameCasingFixer.php
+++ src/Fixer/Casing/ClassReferenceNameCasingFixer.php
@@ -84,6 +84,10 @@ final class ClassReferenceNameCasingFixer extends AbstractFixer
                 T_TRAIT,
             ];
 
+            if (\defined('T_NULLSAFE_OBJECT_OPERATOR')) {  // @TODO: drop condition when PHP 8.0+ is required
+                $notBeforeKinds[] = T_NULLSAFE_OBJECT_OPERATOR;
+            }
+
             if (\defined('T_ENUM')) { // @TODO: drop condition when PHP 8.1+ is required
                 $notBeforeKinds[] = T_ENUM;
             }
