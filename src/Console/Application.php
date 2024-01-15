184dd992fe49169a18300dba4435212db55220f7
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2024-01-15 16:35:57 +0100

    prepared the 3.47.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -41,8 +41,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends BaseApplication
 {
     public const NAME = 'PHP CS Fixer';
-    public const VERSION = '3.46.1-DEV';
-    public const VERSION_CODENAME = 'Three Keys';
+    public const VERSION = '3.47.0';
+    public const VERSION_CODENAME = 'Big Changes';
 
     private ToolInfo $toolInfo;
 
