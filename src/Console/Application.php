0e2de6024a58238a86c0181edaabf5af39f6234b
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2024-01-19 22:45:03 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -41,7 +41,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends BaseApplication
 {
     public const NAME = 'PHP CS Fixer';
-    public const VERSION = '3.48.0';
+    public const VERSION = '3.48.1-DEV';
     public const VERSION_CODENAME = 'Small Changes';
 
     private ToolInfo $toolInfo;
