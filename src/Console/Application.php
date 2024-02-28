127fa74f010da99053e3f5b62672615b72dd6efd
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2024-02-28 20:50:06 +0100

    prepared the 3.51.0 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -41,7 +41,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends BaseApplication
 {
     public const NAME = 'PHP CS Fixer';
-    public const VERSION = '3.50.1-DEV';
+    public const VERSION = '3.51.0';
     public const VERSION_CODENAME = 'Insomnia';
 
     private ToolInfo $toolInfo;
