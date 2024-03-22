98ffdf4953d1e5a5b92779c051021f816bf3039a
Greg Korba <greg@codito.dev>
2024-03-23 00:19:24 +0100

    Remove superfluous `isBuiltInType`
    It was here because in initial version of the PR there was a support for custom output formats. We can re-introduce it when needed.

--- src/Console/Output/Progress/ProgressOutputFactory.php
+++ src/Console/Output/Progress/ProgressOutputFactory.php
@@ -56,11 +56,6 @@ final class ProgressOutputFactory
 
     public function supports(string $outputType): bool
     {
-        return $this->isBuiltInType($outputType);
-    }
-
-    private function isBuiltInType(string $outputType): bool
-    {
         return \in_array($outputType, ProgressOutputType::all(), true);
     }
 }
