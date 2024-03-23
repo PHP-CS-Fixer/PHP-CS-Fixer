580af8be502fcf5fb86b815e2512800f9f60e597
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2024-03-23 21:49:22 +0100

    CS

--- tests/PregTest.php
+++ tests/PregTest.php
@@ -179,8 +179,8 @@ final class PregTest extends TestCase
     }
 
     /**
-     * @param string|list<string> $pattern
-     * @param string|list<string> $subject
+     * @param list<string>|string $pattern
+     * @param list<string>|string $subject
      *
      * @dataProvider provideCommonCases
      */
@@ -201,8 +201,8 @@ final class PregTest extends TestCase
     }
 
     /**
-     * @param string|list<string> $pattern
-     * @param string|list<string> $subject
+     * @param list<string>|string $pattern
+     * @param list<string>|string $subject
      *
      * @dataProvider provideCommonCases
      */
