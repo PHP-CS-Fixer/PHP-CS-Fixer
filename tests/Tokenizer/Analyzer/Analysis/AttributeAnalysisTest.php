5e35d265eb0380aae5def8b792479c097ae1e0b4
HypeMC <hypemc@gmail.com>
2024-03-25 12:40:44 +0100

    feat: Introduce `AttributeAnalysis`

---
+++ tests/Tokenizer/Analyzer/Analysis/AttributeAnalysisTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Tests\Tokenizer\Analyzer\Analysis;
+
+use PhpCsFixer\Tests\TestCase;
+use PhpCsFixer\Tokenizer\Analyzer\Analysis\AttributeAnalysis;
+
+/**
+ * @covers \PhpCsFixer\Tokenizer\Analyzer\Analysis\AttributeAnalysis
+ *
+ * @internal
+ */
+final class AttributeAnalysisTest extends TestCase
+{
+    public function testAttribute(): void
+    {
+        $analysis = new AttributeAnalysis(2, 34, $attributes = [[
+            'start' => 3,
+            'end' => 12,
+            'name' => 'AB\\Baz',
+        ], [
+            'start' => 14,
+            'end' => 32,
+            'name' => '\\A\\B\\Qux',
+        ]]);
+
+        self::assertSame(2, $analysis->getStartIndex());
+        self::assertSame(34, $analysis->getEndIndex());
+        self::assertSame($attributes, $analysis->getAttributes());
+    }
+}
