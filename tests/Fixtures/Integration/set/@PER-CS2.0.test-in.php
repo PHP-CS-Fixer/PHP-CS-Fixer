272fd38f79fbc088681f8fd96cae44577f6a9be3
Michael Vickers <michaelvickers.uk@gmail.com>
2024-03-30 22:32:18 +0000

    Add match statement tests

--- tests/Fixtures/Integration/set/@PER-CS2.0.test-in.php
+++ tests/Fixtures/Integration/set/@PER-CS2.0.test-in.php
@@ -67,5 +67,9 @@ function parametersMultilineWithoutComma(
     $x,
     $y
 ) {}
+$matchMultilineWithoutComma = match ($a) {
+    1 => 0,
+    2 => 1
+};
 
 ?>
--- tests/Fixtures/Integration/set/@PER-CS2.0.test-out.php
+++ tests/Fixtures/Integration/set/@PER-CS2.0.test-out.php
@@ -75,3 +75,7 @@ function parametersMultilineWithoutComma(
     $x,
     $y,
 ) {}
+$matchMultilineWithoutComma = match ($a) {
+    1 => 0,
+    2 => 1,
+};
