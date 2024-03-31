44fe0056ad3b31335cc418ef0a6f5fbc4ce690b3
Michael Vickers <michaelvickers.uk@gmail.com>
2024-03-31 22:53:59 +0100

    Expand tests

--- tests/Fixtures/Integration/set/@PER-CS2.0_php74.test-out.php
+++ tests/Fixtures/Integration/set/@PER-CS2.0_php74.test-out.php
@@ -1,6 +1,20 @@
 <?php
 
+$arrayMultilineWithoutComma = [
+    'foo' => 'bar',
+    'foo2' => 'bar',
+];
+$heredocMultilineWithoutComma = [
+    'foo',
+    <<<EOD
+        bar
+        EOD,
+];
 argumentsMultilineWithoutComma(
     1,
-    2
+    2,
 );
+function parametersMultilineWithoutComma(
+    $x,
+    $y
+) {}
