553d91fcbd533b7786572b9c2596c9a24a7cb199
Michael Vickers <michaelvickers.uk@gmail.com>
2024-04-02 20:55:40 +0100

    Revert "Expand tests"
    This reverts commit 44fe0056ad3b31335cc418ef0a6f5fbc4ce690b3.

--- tests/Fixtures/Integration/set/@PER-CS2.0_php74.test-in.php
+++ tests/Fixtures/Integration/set/@PER-CS2.0_php74.test-in.php
@@ -1,20 +1,6 @@
 <?php
 
-$arrayMultilineWithoutComma = [
-    'foo' => 'bar',
-    'foo2' => 'bar'
-];
-$heredocMultilineWithoutComma = [
-    'foo',
-    <<<EOD
-        bar
-        EOD
-];
 argumentsMultilineWithoutComma(
     1,
     2
 );
-function parametersMultilineWithoutComma(
-    $x,
-    $y
-) {}
