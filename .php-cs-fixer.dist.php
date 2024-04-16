66ed06dc47c827712c4feeb781e632a4edafe382
Greg Korba <greg@codito.dev>
2024-04-16 17:00:31 +0200

    Make dist config more readable

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -15,23 +15,6 @@ declare(strict_types=1);
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
-$header = <<<'EOF'
-    This file is part of PHP CS Fixer.
-
-    (c) Fabien Potencier <fabien@symfony.com>
-        Dariusz Rumiński <dariusz.ruminski@gmail.com>
-
-    This source file is subject to the MIT license that is bundled
-    with this source code in the file LICENSE.
-    EOF;
-
-$finder = (new Finder())
-    ->ignoreDotFiles(false)
-    ->ignoreVCSIgnored(true)
-    ->exclude(['dev-tools/phpstan', 'tests/Fixtures'])
-    ->in(__DIR__)
-;
-
 return (new Config())
     ->setRiskyAllowed(true)
     ->setRules([
@@ -41,10 +24,24 @@ return (new Config())
         '@PhpCsFixer' => true,
         '@PhpCsFixer:risky' => true,
         'general_phpdoc_annotation_remove' => ['annotations' => ['expectedDeprecation']], // one should use PHPUnit built-in method instead
-        'header_comment' => ['header' => $header],
+        'header_comment' => ['header' => <<<'EOF'
+            This file is part of PHP CS Fixer.
+
+            (c) Fabien Potencier <fabien@symfony.com>
+                Dariusz Rumiński <dariusz.ruminski@gmail.com>
+
+            This source file is subject to the MIT license that is bundled
+            with this source code in the file LICENSE.
+            EOF],
         'modernize_strpos' => true, // needs PHP 8+ or polyfill
         'no_useless_concat_operator' => false, // TODO switch back on when the `src/Console/Application.php` no longer needs the concat
         'numeric_literal_separator' => true,
     ])
-    ->setFinder($finder)
+    ->setFinder(
+        (new Finder())
+            ->ignoreDotFiles(false)
+            ->ignoreVCSIgnored(true)
+            ->exclude(['dev-tools/phpstan', 'tests/Fixtures'])
+            ->in(__DIR__)
+    )
 ;
