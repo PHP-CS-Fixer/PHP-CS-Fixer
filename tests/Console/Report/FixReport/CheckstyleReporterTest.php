e3ca390aeb2fbd64d484912dd2c76bd1e6d00d85
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2024-04-25 18:13:17 +0200

    solve '$xsd (string|null) does not accept string|false.'

--- tests/Console/Report/FixReport/CheckstyleReporterTest.php
+++ tests/Console/Report/FixReport/CheckstyleReporterTest.php
@@ -38,7 +38,12 @@ final class CheckstyleReporterTest extends AbstractReporterTestCase
 
     public static function setUpBeforeClass(): void
     {
-        self::$xsd = file_get_contents(__DIR__.'/../../../../doc/schemas/fix/checkstyle.xsd');
+        $content = file_get_contents(__DIR__.'/../../../../doc/schemas/fix/checkstyle.xsd');
+        if (false === $content) {
+            throw new \RuntimeException('Cannot read file.');
+        }
+
+        self::$xsd = $content;
     }
 
     public static function tearDownAfterClass(): void
