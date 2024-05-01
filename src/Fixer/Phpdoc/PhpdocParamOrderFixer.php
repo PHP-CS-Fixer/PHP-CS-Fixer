f01f38b653206ebf0e7c33e5a405ff242597fee7
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2024-05-01 21:34:37 +0200

    more

--- src/Fixer/Phpdoc/PhpdocParamOrderFixer.php
+++ src/Fixer/Phpdoc/PhpdocParamOrderFixer.php
@@ -103,7 +103,7 @@ function m($a, array $b, Foo $c) {}
     }
 
     /**
-     * @return Token[]
+     * @return list<Token>
      */
     private function getFunctionParamNames(Tokens $tokens, int $paramBlockStart): array
     {
@@ -124,8 +124,8 @@ function m($a, array $b, Foo $c) {}
     /**
      * Overwrite the param annotations in order.
      *
-     * @param Token[]      $paramNames
-     * @param Annotation[] $paramAnnotations
+     * @param list<Token>      $paramNames
+     * @param list<Annotation> $paramAnnotations
      */
     private function rewriteDocBlock(DocBlock $doc, array $paramNames, array $paramAnnotations): DocBlock
     {
@@ -161,8 +161,8 @@ function m($a, array $b, Foo $c) {}
     /**
      * Sort the param annotations according to the function parameters.
      *
-     * @param Token[]      $funcParamNames
-     * @param Annotation[] $paramAnnotations
+     * @param list<Token>      $funcParamNames
+     * @param list<Annotation> $paramAnnotations
      *
      * @return list<string>
      */
@@ -181,9 +181,10 @@ function m($a, array $b, Foo $c) {}
         }
 
         // Detect superfluous annotations
-        /** @var Annotation[] $invalidParams */
-        $invalidParams = array_diff_key($paramAnnotations, $validParams);
-        $invalidParams = array_values($invalidParams);
+        /** @var list<Annotation> $invalidParams */
+        $invalidParams = array_values(
+            array_diff_key($paramAnnotations, $validParams)
+        );
 
         // Append invalid parameters to the (ordered) valid ones
         $orderedParams = array_values($validParams);
@@ -197,7 +198,7 @@ function m($a, array $b, Foo $c) {}
     /**
      * Fetch all annotations except the param ones.
      *
-     * @param Annotation[] $paramAnnotations
+     * @param list<Annotation> $paramAnnotations
      *
      * @return list<string>
      */
@@ -227,7 +228,7 @@ function m($a, array $b, Foo $c) {}
     /**
      * Return the indices of the lines of a specific parameter annotation.
      *
-     * @param Annotation[] $paramAnnotations
+     * @param list<Annotation> $paramAnnotations
      *
      * @return ?list<int>
      */
