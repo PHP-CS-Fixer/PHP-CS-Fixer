4f8b88cb6afc0c8cbea1024cc02ae0c4874cc168
Kuba Werłos <werlos@gmail.com>
2024-05-09 12:37:21 +0200

    `bool` is not a const, but a type

--- tests/Fixer/Casing/ConstantCaseFixerTest.php
+++ tests/Fixer/Casing/ConstantCaseFixerTest.php
@@ -333,7 +333,7 @@ final class ConstantCaseFixerTest extends AbstractFixerTestCase
                 class Foo5  { const Foo|NULL       NULL  = NULL;  }
                 class Foo6  { const NULL|Foo       NULL  = NULL;  }
                 class Foo7  { const NULL|(Foo&Bar) NULL  = NULL;  }
-                class Foo8  { const BOOL           TRUE  = TRUE;  }
+                class Foo8  { const bool           TRUE  = TRUE;  }
                 class Foo9  { const FALSE          FALSE = FALSE; }
                 class Foo10 { const TRUE           TRUE  = TRUE;  }
                 PHP,
