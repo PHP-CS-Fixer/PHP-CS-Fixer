025ce394a2e65048e4ec0d96cb69bf2fa57ffc60
Greg Korba <greg@codito.dev>
2024-05-14 03:30:01 +0200

    DX: Fix Mess Detector violations (#8007)

--- src/Tokenizer/Analyzer/AttributeAnalyzer.php
+++ src/Tokenizer/Analyzer/AttributeAnalyzer.php
@@ -110,7 +110,9 @@ final class AttributeAnalyzer
         }
 
         $startIndex = $index;
-        if ($tokens[$prevIndex = $tokens->getPrevMeaningfulToken($index)]->isGivenKind(CT::T_ATTRIBUTE_CLOSE)) {
+        $prevIndex = $tokens->getPrevMeaningfulToken($index);
+
+        if ($tokens[$tokens->getPrevMeaningfulToken($index)]->isGivenKind(CT::T_ATTRIBUTE_CLOSE)) {
             // Include comments/PHPDoc if they are present
             $startIndex = $tokens->getNextNonWhitespace($prevIndex);
         }
