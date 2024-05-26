d1be00d97fdfaa91e43c93bf6c6934c72b6458d4
Kuba Werłos <werlos@gmail.com>
2024-05-26 23:06:11 +0200

    Fix namespace

--- tests/Tokenizer/Analyzer/FullyQualifiedNameAnalyzerTest.php
+++ tests/Tokenizer/Analyzer/FullyQualifiedNameAnalyzerTest.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
  * with this source code in the file LICENSE.
  */
 
-namespace Tokenizer\Analyzer;
+namespace PhpCsFixer\Tests\Tokenizer\Analyzer;
 
 use PhpCsFixer\Tests\TestCase;
 use PhpCsFixer\Tokenizer\Analyzer\FullyQualifiedNameAnalyzer;
