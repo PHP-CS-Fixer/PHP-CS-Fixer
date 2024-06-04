28972ce4349e18af7e7e93fdb352ed6c1a35dec5
Jannik <hi+github@jannik.io>
2024-06-04 17:17:21 +0200

    docs: Make wording in `final_class` docs less dismissive (#8065)

--- src/Fixer/ClassNotation/FinalClassFixer.php
+++ src/Fixer/ClassNotation/FinalClassFixer.php
@@ -39,7 +39,7 @@ class MyApp {}
             .'If you want to subclass a class, mark the parent class as abstract and create two child classes, one empty if necessary: you\'ll gain much more fine grained type-hinting. '
             .'If you need to mock a standalone class, create an interface, or maybe it\'s a value-object that shouldn\'t be mocked at all. '
             .'If you need to extend a standalone class, create an interface and use the Composite pattern. '
-            .'If you aren\'t ready yet for serious OOP, go with FinalInternalClassFixer, it\'s fine.',
+            .'If these rules are too strict for you, you can use `FinalInternalClassFixer` instead.',
             'Risky when subclassing non-abstract classes.'
         );
     }
