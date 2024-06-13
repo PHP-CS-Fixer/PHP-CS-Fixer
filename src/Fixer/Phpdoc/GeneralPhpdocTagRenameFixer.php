4c56541586435738efa2907a9ed7db2739db9029
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2024-06-13 19:11:54 +0200

    fix types

--- src/Fixer/Phpdoc/GeneralPhpdocTagRenameFixer.php
+++ src/Fixer/Phpdoc/GeneralPhpdocTagRenameFixer.php
@@ -91,7 +91,7 @@ final class GeneralPhpdocTagRenameFixer extends AbstractFixer implements Configu
                 ->setDefault(true)
                 ->getOption(),
             (new FixerOptionBuilder('replacements', 'A map of tags to replace.'))
-                ->setAllowedTypes(['string[]'])
+                ->setAllowedTypes(['array']) // array<string, string>
                 ->setNormalizer(static function (Options $options, array $value): array {
                     $normalizedValue = [];
 
