34aba91eb49c130c9d3ece4cec4a2c4669803a39
Kuba Werłos <werlos@gmail.com>
2024-07-24 21:35:35 +0200

    Fix SCA issues

--- tests/AutoReview/ProjectCodeTest.php
+++ tests/AutoReview/ProjectCodeTest.php
@@ -718,6 +718,8 @@ final class ProjectCodeTest extends TestCase
 
     /**
      * @dataProvider provideTestClassCases
+     *
+     * @param class-string $className
      */
     public function testNoDuplicatedMethods(string $className): void
     {
@@ -730,14 +732,16 @@ final class ProjectCodeTest extends TestCase
                 continue;
             }
 
-            $startLine = $method->getStartLine();
-            $length = $method->getEndLine() - $startLine;
+            $startLine = (int) $method->getStartLine();
+            $length = (int) $method->getEndLine() - $startLine;
             if (3 === $length) { // open and closing brace are included - this checks for single line methods
                 continue;
             }
 
-            $source = file($method->getFileName());
-            $candidateContent = implode('', array_slice($source, $startLine, $length));
+            /** @var list<string> $source */
+            $source = file((string) $method->getFileName());
+
+            $candidateContent = implode('', \array_slice($source, $startLine, $length));
             if (str_contains($candidateContent, '$this->doTest(')) {
                 continue;
             }
@@ -745,7 +749,7 @@ final class ProjectCodeTest extends TestCase
             $foundInDuplicates = false;
             foreach ($alreadyFoundMethods as $methodKey => $methodContent) {
                 if ($candidateContent === $methodContent) {
-                    $duplicates[] = [$methodKey,$method->getName(), ];
+                    $duplicates[] = \sprintf('%s is duplicate of %s', $methodKey, $method->getName());
                     $foundInDuplicates = true;
                 }
             }
@@ -757,16 +761,10 @@ final class ProjectCodeTest extends TestCase
         self::assertSame(
             [],
             $duplicates,
-            sprintf(
+            \sprintf(
                 "Duplicated methods found in %s:\n - %s",
                 $className,
-                implode(
-                    "\n - ",
-                    array_map(
-                        fn (array $duplicates):string => sprintf('%s is duplicate of %s', ...$duplicates),
-                        $duplicates
-                    )
-                )
+                implode("\n - ", $duplicates)
             )
         );
     }
