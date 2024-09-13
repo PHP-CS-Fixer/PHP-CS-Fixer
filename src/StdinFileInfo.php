21c3509b76ef94607965734d5d7a390114360ff4
Kuba Werłos <werlos@gmail.com>
2024-09-13 22:40:35 +0200

    Update PHPStan to 1.12.3

--- src/StdinFileInfo.php
+++ src/StdinFileInfo.php
@@ -58,6 +58,9 @@ final class StdinFileInfo extends \SplFileInfo
         return '.php';
     }
 
+    /**
+     * @param null|class-string<\SplFileInfo> $class
+     */
     public function getFileInfo($class = null): \SplFileInfo
     {
         throw new \BadMethodCallException(\sprintf('Method "%s" is not implemented.', __METHOD__));
@@ -105,6 +108,9 @@ final class StdinFileInfo extends \SplFileInfo
         return '';
     }
 
+    /**
+     * @param null|class-string<\SplFileInfo> $class
+     */
     public function getPathInfo($class = null): \SplFileInfo
     {
         throw new \BadMethodCallException(\sprintf('Method "%s" is not implemented.', __METHOD__));
