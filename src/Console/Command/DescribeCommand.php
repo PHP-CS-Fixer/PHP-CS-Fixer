9fe4aede7993b497ee66129d55c0c3185158aeca
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2024-11-18 02:10:03 +0100

    drop problematic types

--- src/Console/Command/DescribeCommand.php
+++ src/Console/Command/DescribeCommand.php
@@ -56,7 +56,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[AsCommand(name: 'describe')]
 final class DescribeCommand extends Command
 {
-    /** @var string */
     protected static $defaultName = 'describe';
 
     /**
--- src/Console/Command/FixCommand.php
+++ src/Console/Command/FixCommand.php
@@ -52,10 +52,8 @@ use Symfony\Component\Stopwatch\Stopwatch;
 #[AsCommand(name: 'fix', description: 'Fixes a directory or a file.')]
 /* final */ class FixCommand extends Command
 {
-    /** @var string */
     protected static $defaultName = 'fix';
 
-    /** @var string */
     protected static $defaultDescription = 'Fixes a directory or a file.';
 
     private EventDispatcherInterface $eventDispatcher;
