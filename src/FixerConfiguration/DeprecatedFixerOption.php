08089bdaeea45610643d58000b367898c08956a8
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2024-12-17 00:30:57 +0100

    batch: FixerConfiguration

--- src/FixerConfiguration/DeprecatedFixerOption.php
+++ src/FixerConfiguration/DeprecatedFixerOption.php
@@ -14,6 +14,9 @@ declare(strict_types=1);
 
 namespace PhpCsFixer\FixerConfiguration;
 
+/**
+ * @readonly
+ */
 final class DeprecatedFixerOption implements DeprecatedFixerOptionInterface
 {
     private FixerOptionInterface $option;
