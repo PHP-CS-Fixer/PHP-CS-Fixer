59eb572b2de19b690914a8d37092cd3931cdbb1a
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2024-12-17 00:31:54 +0100

    Linter

--- src/Linter/CachingLinter.php
+++ src/Linter/CachingLinter.php
@@ -17,6 +17,8 @@ namespace PhpCsFixer\Linter;
 /**
  * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
  *
+ * @readonly
+ *
  * @internal
  */
 final class CachingLinter implements LinterInterface
