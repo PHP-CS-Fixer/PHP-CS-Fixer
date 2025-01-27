8266f90de727a4080e5712f0b26fb17519512916
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-01-27 17:37:56 +0100

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -44,7 +44,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends BaseApplication
 {
     public const NAME = 'PHP CS Fixer';
-    public const VERSION = '3.68.3';
+    public const VERSION = '3.68.4-DEV';
     public const VERSION_CODENAME = 'Persian Successor';
 
     /**
