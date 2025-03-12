3fd0aaccdd730666491b2256966eb29288af5991
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-03-12 21:44:45 +0100

    namespace fix

--- src/Hasher.php
+++ src/Hasher.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
  * with this source code in the file LICENSE.
  */
 
-namespace PhpCsFixer\Tokenizer;
+namespace PhpCsFixer;
 
 /**
  * @author Dariusz Rumiński <dariusz.ruminski@gmail.com>
