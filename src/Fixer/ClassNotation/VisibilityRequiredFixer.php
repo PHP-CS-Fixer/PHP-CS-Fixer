1ec2ab4759016a7c5b806e962d4aa4fbd54541c4
Kuba Werłos <werlos@gmail.com>
2025-03-24 22:22:32 +0100

    chore: make options that have default and allowed sets the same size the same array (#8529)

--- src/Fixer/ClassNotation/VisibilityRequiredFixer.php
+++ src/Fixer/ClassNotation/VisibilityRequiredFixer.php
@@ -96,11 +96,13 @@ class Sample
 
     protected function createConfigurationDefinition(): FixerConfigurationResolverInterface
     {
+        $elements = ['const', 'method', 'property'];
+
         return new FixerConfigurationResolver([
             (new FixerOptionBuilder('elements', 'The structural elements to fix (PHP >= 7.1 required for `const`).'))
                 ->setAllowedTypes(['string[]'])
-                ->setAllowedValues([new AllowedValueSubset(['property', 'method', 'const'])])
-                ->setDefault(['property', 'method', 'const'])
+                ->setAllowedValues([new AllowedValueSubset($elements)])
+                ->setDefault($elements)
                 ->getOption(),
         ]);
     }
