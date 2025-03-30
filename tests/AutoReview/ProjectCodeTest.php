e279b83853004ecf321901f86ebbaf52e2d4045c
Kuba Werłos <werlos@gmail.com>
2025-03-30 15:45:40 +0200

    Test that data providers has `int` or `string` key type if all the keys are of the same type

--- tests/AutoReview/ProjectCodeTest.php
+++ tests/AutoReview/ProjectCodeTest.php
@@ -86,7 +86,7 @@ final class ProjectCodeTest extends TestCase
     }
 
     /**
-     * @return iterable<array{string}>
+     * @return iterable<int, array{string}>
      */
     public static function provideThatSrcClassHaveTestClassCases(): iterable
     {
@@ -237,7 +237,7 @@ final class ProjectCodeTest extends TestCase
     }
 
     /**
-     * @return iterable<array{string}>
+     * @return iterable<int, array{string}>
      */
     public static function provideThatSrcClassesNotAbuseInterfacesCases(): iterable
     {
@@ -514,7 +514,7 @@ final class ProjectCodeTest extends TestCase
     }
 
     /**
-     * @return iterable<array{string}>
+     * @return iterable<int, array{string}>
      */
     public static function provideThereIsNoPregFunctionUsedDirectlyCases(): iterable
     {
@@ -835,9 +835,9 @@ final class ProjectCodeTest extends TestCase
      *
      * @param class-string<TestCase> $testClassName
      */
-    public function testThatDataFromDataProvidersIsNotDuplicated(string $testClassName, string $dataProviderName): void
+    public function testDataFromDataProviders(string $testClassName, string $dataProviderName): void
     {
-        $exceptions = [ // should only shrink
+        $exceptionsForDuplicatesCheck = [ // should only shrink
             'PhpCsFixer\Tests\AutoReview\CommandTest::provideCommandHasNameConstCases',
             'PhpCsFixer\Tests\AutoReview\DocumentationTest::provideFixerDocumentationFileIsUpToDateCases',
             'PhpCsFixer\Tests\AutoReview\FixerFactoryTest::providePriorityIntegrationTestFilesAreListedInPriorityGraphCases',
@@ -847,21 +847,27 @@ final class ProjectCodeTest extends TestCase
             'PhpCsFixer\Tests\Fixer\Basic\EncodingFixerTest::provideFixCases',
             'PhpCsFixer\Tests\UtilsTest::provideStableSortCases',
         ];
-        if (\in_array($testClassName.'::'.$dataProviderName, $exceptions, true)) {
-            $this->addToAssertionCount(1);
-
-            return;
-        }
 
         $dataProvider = new \ReflectionMethod($testClassName, $dataProviderName);
 
+        $docComment = $dataProvider->getDocComment();
+        self::assertNotFalse($docComment, \sprintf('Data provider %s::%s is missing PHPDoc.', $testClassName, $dataProviderName));
+        self::assertSame(1, substr_count($docComment, '@return'));
+
         $duplicates = [];
+        $keyTypes = [];
         $alreadyFoundCases = [];
         foreach ($dataProvider->invoke($dataProvider->getDeclaringClass()->newInstanceWithoutConstructor()) as $candidateKey => $candidateData) {
-            $candidateData = serialize($candidateData);
+            $keyTypes[get_debug_type($candidateKey)] = true;
+
+            if (\in_array($testClassName.'::'.$dataProviderName, $exceptionsForDuplicatesCheck, true)) {
+                continue;
+            }
+
+            $serializedCandidateData = serialize($candidateData);
             $foundInDuplicates = false;
             foreach ($alreadyFoundCases as $caseKey => $caseData) {
-                if ($candidateData === $caseData) {
+                if ($serializedCandidateData === $caseData) {
                     $duplicates[] = \sprintf(
                         'Duplicate in %s::%s: %s and %s.'.PHP_EOL,
                         $testClassName,
@@ -875,6 +881,31 @@ final class ProjectCodeTest extends TestCase
             if (!$foundInDuplicates) {
                 $alreadyFoundCases[$candidateKey] = $candidateData;
             }
+        }
+
+        self::assertTrue(1 === \count($keyTypes) || 2 === \count($keyTypes));
+
+        if (1 === \count($keyTypes)) {
+            // all data provider's keys are of the same type - type must be present
+            $type = array_keys($keyTypes)[0];
+            self::assertTrue('int' === $type || 'string' === $type);
+            self::assertStringContainsString(
+                \sprintf('@return iterable<%s, array{', $type),
+                $docComment,
+                \sprintf('Data provider %s::%s iterable key type is not.', $testClassName, $dataProviderName)
+            );
+        }
+
+        if (2 === \count($keyTypes)) {
+            // data provider's keys are of both types - int and string - type must be omitted
+            $types = array_keys($keyTypes);
+            sort($types);
+            self::assertSame(['int', 'string'], $types);
+            self::assertStringContainsString(
+                '@return iterable<array{',
+                $docComment,
+                \sprintf('Data provider %s::%s iterable must not have key type.', $testClassName, $dataProviderName)
+            );
         }
 
         self::assertSame([], $duplicates);
@@ -918,7 +949,7 @@ final class ProjectCodeTest extends TestCase
     }
 
     /**
-     * @return iterable<array{string}>
+     * @return iterable<int, array{string}>
      */
     public static function providePhpUnitFixerExtendsAbstractPhpUnitFixerCases(): iterable
     {
