e73cb0c1e404d3f80babac337566da28c649b2c7
Kuba Werłos <werlos@gmail.com>
2025-05-13 22:07:57 +0200

    chore: cleanup `PhpdocSeparationFixerTest`

--- tests/Test/AbstractFixerTestCase.php
+++ tests/Test/AbstractFixerTestCase.php
@@ -56,7 +56,6 @@ use PhpCsFixer\Tests\Fixer\Phpdoc\PhpdocOrderByValueFixerTest;
 use PhpCsFixer\Tests\Fixer\Phpdoc\PhpdocOrderFixerTest;
 use PhpCsFixer\Tests\Fixer\Phpdoc\PhpdocParamOrderFixerTest;
 use PhpCsFixer\Tests\Fixer\Phpdoc\PhpdocReturnSelfReferenceFixerTest;
-use PhpCsFixer\Tests\Fixer\Phpdoc\PhpdocSeparationFixerTest;
 use PhpCsFixer\Tests\Fixer\PhpTag\EchoTagSyntaxFixerTest;
 use PhpCsFixer\Tests\Fixer\PhpTag\NoClosingTagFixerTest;
 use PhpCsFixer\Tests\Fixer\PhpUnit\PhpUnitTestCaseStaticMethodCallsFixerTest;
@@ -454,7 +453,6 @@ abstract class AbstractFixerTestCase extends TestCase
             PhpdocOrderFixerTest::class,
             PhpdocParamOrderFixerTest::class,
             PhpdocReturnSelfReferenceFixerTest::class,
-            PhpdocSeparationFixerTest::class,
         ];
 
         $names = ['Fix', 'FixDeprecated', 'FixPre80', 'Fix80', 'FixPre81', 'Fix81', 'Fix82', 'Fix83', 'FixPre84', 'Fix84', 'WithWhitespacesConfig', 'InvalidConfiguration'];
