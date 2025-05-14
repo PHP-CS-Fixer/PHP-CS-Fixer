ebd97923f4ee02be1f8114e2200198502962fad4
Kuba Werłos <werlos@gmail.com>
2025-05-14 07:00:22 +0200

    chore: cleanup `PhpdocParamOrderFixerTest` (#8659)

--- tests/Test/AbstractFixerTestCase.php
+++ tests/Test/AbstractFixerTestCase.php
@@ -54,7 +54,6 @@ use PhpCsFixer\Tests\Fixer\Phpdoc\PhpdocAddMissingParamAnnotationFixerTest;
 use PhpCsFixer\Tests\Fixer\Phpdoc\PhpdocNoEmptyReturnFixerTest;
 use PhpCsFixer\Tests\Fixer\Phpdoc\PhpdocOrderByValueFixerTest;
 use PhpCsFixer\Tests\Fixer\Phpdoc\PhpdocOrderFixerTest;
-use PhpCsFixer\Tests\Fixer\Phpdoc\PhpdocParamOrderFixerTest;
 use PhpCsFixer\Tests\Fixer\PhpTag\EchoTagSyntaxFixerTest;
 use PhpCsFixer\Tests\Fixer\PhpTag\NoClosingTagFixerTest;
 use PhpCsFixer\Tests\Fixer\PhpUnit\PhpUnitTestCaseStaticMethodCallsFixerTest;
@@ -450,7 +449,6 @@ abstract class AbstractFixerTestCase extends TestCase
             PhpdocNoEmptyReturnFixerTest::class,
             PhpdocOrderByValueFixerTest::class,
             PhpdocOrderFixerTest::class,
-            PhpdocParamOrderFixerTest::class,
         ];
 
         $names = ['Fix', 'FixDeprecated', 'FixPre80', 'Fix80', 'FixPre81', 'Fix81', 'Fix82', 'Fix83', 'FixPre84', 'Fix84', 'WithWhitespacesConfig', 'InvalidConfiguration'];
