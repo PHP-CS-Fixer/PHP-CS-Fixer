7b6851ebd4b20f14c4b6044caacfa100be9ae43e
Kuba Werłos <werlos@gmail.com>
2025-05-14 23:02:36 +0200

    chore: cleanup `MethodArgumentSpaceFixerTest`

--- tests/Test/AbstractFixerTestCase.php
+++ tests/Test/AbstractFixerTestCase.php
@@ -39,7 +39,6 @@ use PhpCsFixer\Tests\Fixer\Comment\HeaderCommentFixerTest;
 use PhpCsFixer\Tests\Fixer\Comment\NoEmptyCommentFixerTest;
 use PhpCsFixer\Tests\Fixer\ControlStructure\NoUselessElseFixerTest;
 use PhpCsFixer\Tests\Fixer\FunctionNotation\FunctionDeclarationFixerTest;
-use PhpCsFixer\Tests\Fixer\FunctionNotation\MethodArgumentSpaceFixerTest;
 use PhpCsFixer\Tests\Fixer\Import\GlobalNamespaceImportFixerTest;
 use PhpCsFixer\Tests\Fixer\ListNotation\ListSyntaxFixerTest;
 use PhpCsFixer\Tests\Fixer\Operator\BinaryOperatorSpacesFixerTest;
@@ -426,7 +425,6 @@ abstract class AbstractFixerTestCase extends TestCase
             IncrementStyleFixerTest::class,
             IndentationTypeFixerTest::class,
             ListSyntaxFixerTest::class,
-            MethodArgumentSpaceFixerTest::class,
         ];
 
         $names = ['Fix', 'FixDeprecated', 'FixPre80', 'Fix80', 'FixPre81', 'Fix81', 'Fix82', 'Fix83', 'FixPre84', 'Fix84', 'WithShortOpenTag', 'WithWhitespacesConfig', 'InvalidConfiguration'];
