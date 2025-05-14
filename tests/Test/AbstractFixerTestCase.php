b3519de4ad2cbd217424fa29fa50cf4f0d053c29
Kuba Werłos <werlos@gmail.com>
2025-05-14 22:42:02 +0200

    chore: cleanup `NativeConstantInvocationFixerTest`

--- tests/Test/AbstractFixerTestCase.php
+++ tests/Test/AbstractFixerTestCase.php
@@ -37,7 +37,6 @@ use PhpCsFixer\Tests\Fixer\ClassNotation\ClassAttributesSeparationFixerTest;
 use PhpCsFixer\Tests\Fixer\ClassNotation\ClassDefinitionFixerTest;
 use PhpCsFixer\Tests\Fixer\Comment\HeaderCommentFixerTest;
 use PhpCsFixer\Tests\Fixer\Comment\NoEmptyCommentFixerTest;
-use PhpCsFixer\Tests\Fixer\ConstantNotation\NativeConstantInvocationFixerTest;
 use PhpCsFixer\Tests\Fixer\ControlStructure\NoUselessElseFixerTest;
 use PhpCsFixer\Tests\Fixer\FunctionNotation\FunctionDeclarationFixerTest;
 use PhpCsFixer\Tests\Fixer\FunctionNotation\MethodArgumentSpaceFixerTest;
@@ -428,7 +427,6 @@ abstract class AbstractFixerTestCase extends TestCase
             IndentationTypeFixerTest::class,
             ListSyntaxFixerTest::class,
             MethodArgumentSpaceFixerTest::class,
-            NativeConstantInvocationFixerTest::class,
         ];
 
         $names = ['Fix', 'FixDeprecated', 'FixPre80', 'Fix80', 'FixPre81', 'Fix81', 'Fix82', 'Fix83', 'FixPre84', 'Fix84', 'WithShortOpenTag', 'WithWhitespacesConfig', 'InvalidConfiguration'];
