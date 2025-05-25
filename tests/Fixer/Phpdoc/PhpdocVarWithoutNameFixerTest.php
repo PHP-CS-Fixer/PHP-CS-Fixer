9b6a4b1934020cf3ddc91b30589126d8eed25c5d
Kuba Werłos <werlos@gmail.com>
2025-05-25 11:10:23 +0200

    Add test showing current behaviour

--- tests/Fixer/Phpdoc/PhpdocVarWithoutNameFixerTest.php
+++ tests/Fixer/Phpdoc/PhpdocVarWithoutNameFixerTest.php
@@ -653,4 +653,36 @@ class A
 ',
         ];
     }
+
+    /**
+     * @dataProvider provideFix84Cases
+     *
+     * @requires PHP 8.4
+     */
+    public function testFix84(string $expected, ?string $input = null): void
+    {
+        $this->doTest($expected, $input);
+    }
+
+    /**
+     * @return iterable<string, array{0: string, 1?: string}>
+     */
+    public static function provideFix84Cases(): iterable
+    {
+        yield 'asymmetric visibility' => [
+            <<<'PHP'
+                <?php class Foo
+                {
+                    /** @var bool $a */
+                    public(set) bool $a;
+
+                    /** @var bool $b */
+                    protected(set) bool $b;
+
+                    /** @var bool $c */
+                    private(set) bool $c;
+                }
+                PHP,
+        ];
+    }
 }
