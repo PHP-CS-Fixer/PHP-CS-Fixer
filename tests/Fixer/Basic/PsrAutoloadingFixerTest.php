01770eb90158e314276e4ff9bb3130bf6dd0aeb0
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-06-12 11:07:31 +0200

    unify test cases

--- tests/Fixer/Basic/PsrAutoloadingFixerTest.php
+++ tests/Fixer/Basic/PsrAutoloadingFixerTest.php
@@ -351,6 +351,7 @@ class PsrAutoloadingFixer {}
 
         foreach (['__halt_compiler', 'abstract', 'and', 'array', 'as', 'break', 'case', 'catch', 'class', 'clone', 'const', 'continue', 'declare', 'default', 'die', 'do', 'echo', 'else', 'elseif', 'empty', 'enddeclare', 'endfor', 'endforeach', 'endif', 'endswitch', 'endwhile', 'eval', 'exit', 'extends', 'final', 'for', 'foreach', 'function', 'global', 'goto', 'if', 'implements', 'include', 'include_once', 'instanceof', 'interface', 'isset', 'list', 'namespace', 'new', 'or', 'print', 'private', 'protected', 'public', 'require', 'require_once', 'return', 'static', 'switch', 'throw', 'try', 'unset', 'use', 'var', 'while', 'xor'] as $keyword) {
             $cases[] = $keyword.'.php';
+            $cases[] = strtoupper($keyword).'.php';
         }
 
         foreach (['__CLASS__', '__DIR__', '__FILE__', '__FUNCTION__', '__LINE__', '__METHOD__', '__NAMESPACE__', '__TRAIT__'] as $magicConstant) {
