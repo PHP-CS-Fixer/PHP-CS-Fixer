36a15a9d240805ccd6be2ffd47c7610ca01b8726
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2025-06-13 10:56:52 +0200

    tests: reflect the new max PHP version

--- tests/AutoReview/BinEntryFileTest.php
+++ tests/AutoReview/BinEntryFileTest.php
@@ -44,7 +44,7 @@ final class BinEntryFileTest extends TestCase
 
         self::assertEqualsCanonicalizing([
             '    if (\PHP_VERSION_ID === (int) \'80000\') { // TODO use 8_00_00 once only PHP 7.4+ is supported by this entry file'."\n",
-            '    if (\PHP_VERSION_ID < (int) \'70400\' || \PHP_VERSION_ID >= (int) \'80400\') {'."\n",
+            '    if (\PHP_VERSION_ID < (int) \'70400\') {'."\n",
         ], $phpVersionIdLines, 'Seems supported PHP versions changed in "./php-cs-fixer" - edit the README.md (and this test file) to match them!');
     }
 }
