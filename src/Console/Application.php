983f7f018fb0305618cf612b2d87289604e751ed
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-07-06 16:13:51 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -44,7 +44,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends BaseApplication
 {
     public const NAME = 'PHP CS Fixer';
-    public const VERSION = '3.78.1';
+    public const VERSION = '3.78.2-DEV';
     public const VERSION_CODENAME = 'Kids Playground';
 
     /**
