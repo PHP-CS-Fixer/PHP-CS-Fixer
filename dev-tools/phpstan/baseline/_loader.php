e2bc34b0d752fe21517e8f9bbc6ed60da8571d2c
Gregor Harlan <mail@gh01.de>
2025-07-20 09:22:00 +0200

    update baseline

--- dev-tools/phpstan/baseline/_loader.php
+++ dev-tools/phpstan/baseline/_loader.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-// total 485 errors
+// total 471 errors
 return ['includes' => [
     __DIR__ . '/argument.type.php',
     __DIR__ . '/argument.unpackNonIterable.php',
@@ -9,13 +9,11 @@ return ['includes' => [
     __DIR__ . '/generator.valueType.php',
     __DIR__ . '/method.nonObject.php',
     __DIR__ . '/minus.leftNonNumeric.php',
-    __DIR__ . '/missingType.property.php',
     __DIR__ . '/offsetAccess.nonOffsetAccessible.php',
     __DIR__ . '/offsetAccess.notFound.php',
     __DIR__ . '/plus.leftNonNumeric.php',
     __DIR__ . '/plus.rightNonNumeric.php',
     __DIR__ . '/preInc.nonNumeric.php',
-    __DIR__ . '/property.parentPropertyFinalByPhpDoc.php',
     __DIR__ . '/return.type.php',
     __DIR__ . '/smaller.alwaysFalse.php',
     __DIR__ . '/staticMethod.impossibleType.php',
