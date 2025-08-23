6aede2ff6b26a6337de8d5f0e7d969858721142c
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-08-23 21:17:36 +0200

    quasi-test

---
+++ tests/Runner/Parallel/ProcessUtilsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Tests\Runner\Parallel;
+
+use PhpCsFixer\Tests\TestCase;
+
+/**
+ * @internal
+ *
+ * @covers \PhpCsFixer\Runner\Parallel\ProcessUtils
+ */
+final class ProcessUtilsTest extends TestCase
+{
+    public function testEscapeArgument(): void
+    {
+        self::markTestSkipped('The `escapeArgument` method is copy-pasted from Symfony codebase. Is tested indirectly on testing Parallel flow under Windows.');
+    }
+}
