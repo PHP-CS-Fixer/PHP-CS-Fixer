e049ae96802f176c138c508f54e5431eb79b4bc2
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-08-26 00:36:30 +0200

    rename tests for rulesets

---
+++ tests/RuleSet/Sets/PHP7x0MigrationSetTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Tests\RuleSet\Sets;
+
+/**
+ * @internal
+ *
+ * @covers \PhpCsFixer\RuleSet\Sets\PHP7x0MigrationSet
+ */
+final class PHP7x0MigrationSetTest extends AbstractSetTestCase {}
