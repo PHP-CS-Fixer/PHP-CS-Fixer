b0807a9ffe5f330bc804f6c9ef750e94a977dc17
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2025-08-28 00:19:10 +0200

    CS: re-apply rules (#8988)

--- tests/Runner/Parallel/ProcessUtilsTest.php
+++ tests/Runner/Parallel/ProcessUtilsTest.php
@@ -20,6 +20,8 @@ use PhpCsFixer\Tests\TestCase;
  * @internal
  *
  * @covers \PhpCsFixer\Runner\Parallel\ProcessUtils
+ *
+ * @no-named-arguments Parameter names are not covered by the backward compatibility promise.
  */
 final class ProcessUtilsTest extends TestCase
 {
