ef9b3e7c388387f617861656b13d9d7a914d495d
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2025-08-28 12:35:02 +0200

    chore: Add TODO for ENUM conversion in ProgressOutputType (#8991)

--- src/Console/Output/Progress/ProgressOutputType.php
+++ src/Console/Output/Progress/ProgressOutputType.php
@@ -15,6 +15,8 @@ declare(strict_types=1);
 namespace PhpCsFixer\Console\Output\Progress;
 
 /**
+ * @TODO PHP 8.1 switch this and similar classes to ENUM
+ *
  * @internal
  *
  * @no-named-arguments Parameter names are not covered by the backward compatibility promise.
