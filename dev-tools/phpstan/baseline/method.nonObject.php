f8e24696abbed002b9c99bbcb323cf273dd06aab
Gregor Harlan <mail@gh01.de>
2025-09-02 11:53:26 +0200

    chore: `PhpdocParamOrderFixer` - improve/simplify code (#9012)

--- dev-tools/phpstan/baseline/method.nonObject.php
+++ dev-tools/phpstan/baseline/method.nonObject.php
@@ -6,15 +6,5 @@ $ignoreErrors[] = [
     'count' => 1,
     'path' => __DIR__ . '/../../../src/AbstractProxyFixer.php',
 ];
-$ignoreErrors[] = [
-    'message' => '#^Cannot call method getEnd\\(\\) on PhpCsFixer\\\\DocBlock\\\\Annotation\\|false\\.$#',
-    'count' => 1,
-    'path' => __DIR__ . '/../../../src/Fixer/Phpdoc/PhpdocParamOrderFixer.php',
-];
-$ignoreErrors[] = [
-    'message' => '#^Cannot call method getStart\\(\\) on PhpCsFixer\\\\DocBlock\\\\Annotation\\|false\\.$#',
-    'count' => 1,
-    'path' => __DIR__ . '/../../../src/Fixer/Phpdoc/PhpdocParamOrderFixer.php',
-];
 
 return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
