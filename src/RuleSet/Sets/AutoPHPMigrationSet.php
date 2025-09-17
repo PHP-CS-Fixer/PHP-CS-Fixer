ec8d3df7d668f2d149ab0fdafbe96470244d0748
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-09-17 16:03:02 +0200

    feat: introduce sets: \@autoPHPMigration, \@autoPHPMigration:risky, \@autoPHPUnitMigration:risky

---
+++ src/RuleSet/Sets/AutoPHPMigrationSet.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\RuleSet\Sets;
+
+use PhpCsFixer\RuleSet\AbstractRuleSetDescription;
+use PhpCsFixer\RuleSet\AutomaticMigrationSetTrait;
+use PhpCsFixer\RuleSet\AutomaticRuleSetDescriptionInterface;
+
+/**
+ * @internal
+ *
+ * @no-named-arguments Parameter names are not covered by the backward compatibility promise.
+ */
+final class AutoPHPMigrationSet extends AbstractRuleSetDescription implements AutomaticRuleSetDescriptionInterface
+{
+    use AutomaticMigrationSetTrait;
+
+    public function getName(): string
+    {
+        return '@'.lcfirst(ltrim(parent::getName(), '@'));
+    }
+
+    public function getRules(): array
+    {
+        return [
+            $this->calculateTargetSet($this->getName(), 'PHP', $this->isRisky()) => true,
+        ];
+    }
+
+    public function getDescription(): string
+    {
+        return 'Rules to improve code for PHP. Target version is automatically detected from project\'s "composer.json" file.';
+    }
+}
