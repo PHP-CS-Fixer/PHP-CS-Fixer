82843895f7463e3442bfb389c60a16c390cac2db
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-09-17 16:01:46 +0200

    introduce concept of \@auto rule sets

--- src/RuleSet/AbstractMigrationSetDescription.php
+++ src/RuleSet/AbstractMigrationSetDescription.php
@@ -23,20 +23,76 @@ use PhpCsFixer\Preg;
  */
 abstract class AbstractMigrationSetDescription extends AbstractRuleSetDescription
 {
+    private string $entity;
+
+    /** @var array{'major': int, 'minor': int} */
+    private array $version;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->parseRuleSetName();
+    }
+
+    /**
+     * @internal
+     */
+    public function getEntity(): string
+    {
+        return $this->entity;
+    }
+
+    /**
+     * @return array{'major': int, 'minor': int}
+     *
+     * @internal
+     */
+    public function getVersion(): array
+    {
+        return $this->version;
+    }
+
+    public function getVersionMajorMinor(): string
+    {
+        return \sprintf('%s.%s', $this->version['major'], $this->version['minor']);
+    }
+
     public function getDescription(): string
+    {
+        $improvement = [
+            'PHPUnit' => 'tests code',
+        ][$this->getEntity()] ?? 'code';
+
+        return \sprintf('Rules to improve %s for %s %s compatibility.', $improvement, $this->getEntity(), $this->getVersionMajorMinor());
+    }
+
+    private function parseRuleSetName(): void
     {
         $name = $this->getName();
 
         // @TODO v4 - `x?` -> `x`
         if (Preg::match('#^@PHPUnit(\d+)x?(\d)Migration.*$#', $name, $matches)) {
-            return \sprintf('Rules to improve tests code for PHPUnit %d.%d compatibility.', $matches[1], $matches[2]);
+            $this->entity = 'PHPUnit';
+            $this->version = [
+                'major' => (int) $matches[1],
+                'minor' => (int) $matches[2],
+            ];
+
+            return;
         }
 
         // @TODO v4 - `x?` -> `x`
         if (Preg::match('#^@PHP(\d)x?(\d)Migration.*$#', $name, $matches)) {
-            return \sprintf('Rules to improve code for PHP %d.%d compatibility.', $matches[1], $matches[2]);
+            $this->entity = 'PHP';
+            // var_dump("FRS", $name, $matches);
+            $this->version = [
+                'major' => (int) $matches[1],
+                'minor' => (int) $matches[2],
+            ];
+
+            return;
         }
 
-        throw new \RuntimeException(\sprintf('Cannot generate description for "%s" "%s".', static::class, $name));
+        throw new \RuntimeException(\sprintf('Cannot parse name of "%s" / "%s".', static::class, $name));
     }
 }
