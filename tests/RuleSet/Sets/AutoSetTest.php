df98a5ded5d4d1fa53083f99971a4274f533b021
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-09-24 15:16:59 +0200

    fix typo

--- tests/RuleSet/Sets/AutoSetTest.php
+++ tests/RuleSet/Sets/AutoSetTest.php
@@ -17,7 +17,7 @@ namespace PhpCsFixer\Tests\RuleSet\Sets;
 /**
  * @internal
  *
- * @covers \PhpCsFixer\RuleSet\Sets\AutoSe
+ * @covers \PhpCsFixer\RuleSet\Sets\AutoSet
  *
  * @no-named-arguments Parameter names are not covered by the backward compatibility promise.
  */
