a90929981ee5d90d6c50a718e14578fcbf73e6ca
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-09-24 23:32:12 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -48,7 +48,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends BaseApplication
 {
     public const NAME = 'PHP CS Fixer';
-    public const VERSION = '3.88.0';
+    public const VERSION = '3.88.1-DEV';
     public const VERSION_CODENAME = 'Folding Bike';
 
     /**
