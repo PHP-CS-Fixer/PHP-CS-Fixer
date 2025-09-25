20d92a9512aed0dc6df4535a076c78d4ce1c7d3a
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-09-25 17:57:45 +0200

    docs: update Set descriptions

--- src/RuleSet/Sets/AutoPHPUnitMigrationRiskySet.php
+++ src/RuleSet/Sets/AutoPHPUnitMigrationRiskySet.php
@@ -41,7 +41,7 @@ final class AutoPHPUnitMigrationRiskySet extends AbstractRuleSetDefinition imple
 
     public function getDescription(): string
     {
-        return 'Rules to improve test code for PHPUnit. Target version is automatically detected from project\'s "composer.json" file.';
+        return 'Migration rules to improve test code towards the minimum PHPUnit supported by your project (taken from "composer.json" file).';
     }
 
     public function getRulesCandidates(): array
