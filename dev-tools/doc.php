49ef2bcb253f558f855fdc06e50006c7486767be
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-09-30 23:03:22 +0200

    unify entry-points templates

--- dev-tools/doc.php
+++ dev-tools/doc.php
@@ -13,13 +13,33 @@ declare(strict_types=1);
  * with this source code in the file LICENSE.
  */
 
-error_reporting(\E_ALL & ~\E_DEPRECATED & ~\E_USER_DEPRECATED);
 
-require __DIR__.'/../vendor/autoload.php';
+namespace PhpCsFixer;
 
+use Composer\XdebugHandler\XdebugHandler;
 use PhpCsFixer\Console\Command\DocumentationCommand;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Filesystem\Filesystem;
+
+error_reporting(\E_ALL & ~\E_DEPRECATED & ~\E_USER_DEPRECATED);
+
+set_error_handler(static function (int $severity, string $message, string $file, int $line): bool {
+    if (0 !== ($severity & error_reporting())) {
+        throw new \ErrorException($message, 0, $severity, $file, $line);
+    }
+
+    return true;
+});
+
+// load dependencies
+(static function (): void {
+    require __DIR__.'/../vendor/autoload.php';
+})();
+
+// Restart if xdebug is loaded, unless the environment variable PHP_CS_FIXER_ALLOW_XDEBUG is set.
+$xdebug = new XdebugHandler('PHP_CS_FIXER');
+$xdebug->check();
+unset($xdebug);
 
 $command = new DocumentationCommand(new Filesystem());
 
