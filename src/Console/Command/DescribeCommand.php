2be19de7067d1893527c375c47895c705faf0106
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-10-01 23:23:23 +0200

    Revert "UX: allow to describe custom rulesets"
    This reverts commit 6cc57dea55eb0e3a259b47176d85b74caefa35d9.

--- src/Console/Command/DescribeCommand.php
+++ src/Console/Command/DescribeCommand.php
@@ -15,7 +15,6 @@ declare(strict_types=1);
 namespace PhpCsFixer\Console\Command;
 
 use PhpCsFixer\Config;
-use PhpCsFixer\CustomRulesetsAwareConfigInterface;
 use PhpCsFixer\Console\Application;
 use PhpCsFixer\Console\ConfigurationResolver;
 use PhpCsFixer\Differ\DiffConsoleFormatter;
@@ -129,12 +128,6 @@ final class DescribeCommand extends Command
         );
 
         $this->fixerFactory->registerCustomFixers($resolver->getConfig()->getCustomFixers());
-
-        if ($resolver->getConfig() instanceof CustomRulesetsAwareConfigInterface) {
-            foreach ($resolver->getConfig()->getCustomRuleSets() as $ruleSet) {
-                RuleSets::registerCustomRuleSet($ruleSet);
-            }
-        }
 
         /** @var ?string $name */
         $name = $input->getArgument('name');
