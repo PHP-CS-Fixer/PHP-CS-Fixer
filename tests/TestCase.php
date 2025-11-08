68413ac251d78f3400389e6a14f736ec95ba3827
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2025-11-08 22:00:49 +0100

    refactor: move assertions from `tearDown` into `assertPostConditions` to fix `Test code or tested code did not remove its own error handlers` warning (#9182)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -34,15 +34,23 @@ abstract class TestCase extends BaseTestCase
     /** @var array<int, string> */
     private array $actualDeprecations = [];
 
-    protected function tearDown(): void
+    protected function assertPostConditions(): void
     {
         if (null !== $this->previouslyDefinedErrorHandler) {
             $this->actualDeprecations = array_unique($this->actualDeprecations);
             sort($this->actualDeprecations);
             $this->expectedDeprecations = array_unique($this->expectedDeprecations);
             sort($this->expectedDeprecations);
-            self::assertSame($this->expectedDeprecations, $this->actualDeprecations);
 
+            self::assertSame($this->expectedDeprecations, $this->actualDeprecations);
+        }
+
+        parent::assertPostConditions();
+    }
+
+    protected function tearDown(): void
+    {
+        if (null !== $this->previouslyDefinedErrorHandler) {
             restore_error_handler();
         }
 
