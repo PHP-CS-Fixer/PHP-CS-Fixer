4db613bc5cd2793a99dba2e33dd58e62dbc7b8a6
Florian Moser <git@famoser.ch>
2025-11-12 20:48:38 +0100

    maint: Remove unused import

--- src/Console/Command/FixCommand.php
+++ src/Console/Command/FixCommand.php
@@ -35,7 +35,6 @@ use PhpCsFixer\Runner\Runner;
 use PhpCsFixer\ToolInfoInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
