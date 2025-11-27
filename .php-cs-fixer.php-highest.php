830f0004a4cff66ac52970c881ebeb3ea517587d
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-11-28 00:58:40 +0100

    CI: upgrade .php-cs-fixer.php-highest.php to fully reflect PHP 8.4

--- .php-cs-fixer.php-highest.php
+++ .php-cs-fixer.php-highest.php
@@ -49,7 +49,7 @@ $typesMap = [
 
 $config->setRules(array_merge($config->getRules(), [
     '@PHP8x4Migration' => true,
-    '@PHP8x2Migration:risky' => true,
+    '@PHP8x4Migration:risky' => true,
     'phpdoc_to_param_type' => ['types_map' => $typesMap],
     'phpdoc_to_return_type' => ['types_map' => $typesMap],
     'phpdoc_to_property_type' => ['types_map' => $typesMap],
