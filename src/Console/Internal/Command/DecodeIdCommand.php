d1acb407caa6842cde147b4383dbe276247be48b
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2025-12-01 13:37:48 +0100

    CS

--- src/Console/Internal/Command/DecodeIdCommand.php
+++ src/Console/Internal/Command/DecodeIdCommand.php
@@ -19,6 +19,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
