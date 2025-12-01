2ca858b129b4555f2e946c6cfea5e33a6dc52826
Kuba Werłos <werlos@gmail.com>
2025-12-01 21:00:21 +0100

    Update

--- tests/Fixtures/.php-cs-fixer.vanilla.php
+++ tests/Fixtures/.php-cs-fixer.vanilla.php
@@ -1,14 +1,8 @@
 <?php
 
-$config = (new \PhpCsFixer\Config())
+return (new \PhpCsFixer\Config())
     ->setFinder(
         \PhpCsFixer\Finder::create()
             ->in(__DIR__ . '/../../')
     )
 ;
-
-if (PHP_VERSION_ID >= 7_06_00) {
-    $config->setUnsupportedPhpVersionAllowed(true);
-}
-
-return $config;
