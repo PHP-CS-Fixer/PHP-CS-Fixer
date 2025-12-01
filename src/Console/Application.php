f171dc216bc8a4192d5a0876a899cce6f9e379d6
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2025-12-01 21:56:13 +0100

    prepared the 3.91.2 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -50,7 +50,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends BaseApplication
 {
     public const NAME = 'PHP CS Fixer';
-    public const VERSION = '3.91.2-DEV';
+    public const VERSION = '3.91.2';
     public const VERSION_CODENAME = 'Folding Bike';
 
     /**
