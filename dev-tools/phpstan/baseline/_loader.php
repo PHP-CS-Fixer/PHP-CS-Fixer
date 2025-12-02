3336f5675d3e66d473a3cc8bc2af71ffe6d79f92
Kuba Werłos <werlos@gmail.com>
2025-12-02 18:25:34 +0100

    chore: fix `generator.valueType` error from PHPStan

--- dev-tools/phpstan/baseline/_loader.php
+++ dev-tools/phpstan/baseline/_loader.php
@@ -3,7 +3,6 @@
 return ['includes' => [
     __DIR__ . '/argument.type.php',
     __DIR__ . '/assign.propertyType.php',
-    __DIR__ . '/generator.valueType.php',
     __DIR__ . '/offsetAccess.notFound.php',
     __DIR__ . '/plus.leftNonNumeric.php',
     __DIR__ . '/plus.rightNonNumeric.php',
