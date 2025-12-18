c8e4f3513310ff04cd4d8eee8c15d8b5ed01df19
Felix Bernhard <mail@felix-bernhard.com>
2025-12-18 19:44:48 +0100

    adjust priority graph

--- tests/AutoReview/FixerFactoryTest.php
+++ tests/AutoReview/FixerFactoryTest.php
@@ -823,12 +823,14 @@ final class FixerFactoryTest extends TestCase
             ],
             'phpdoc_array_type' => [
                 'phpdoc_list_type',
+                'phpdoc_types_no_duplicates',
                 'phpdoc_types_order',
             ],
             'phpdoc_line_span' => [
                 'no_superfluous_phpdoc_tags',
             ],
             'phpdoc_list_type' => [
+                'phpdoc_types_no_duplicates',
                 'phpdoc_types_order',
             ],
             'phpdoc_no_access' => [
