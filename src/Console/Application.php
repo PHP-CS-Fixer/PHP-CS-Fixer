9e7488b19403423e02e8403cc1eb596baf4673b0
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2026-01-04 01:38:52 +0100

    prepared the 3.92.4 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -50,7 +50,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends BaseApplication
 {
     public const NAME = 'PHP CS Fixer';
-    public const VERSION = '3.92.4-DEV';
+    public const VERSION = '3.92.4';
     public const VERSION_CODENAME = 'Exceptional Exception';
 
     /**
