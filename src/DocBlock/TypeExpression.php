403ee211c66fab804dc4ccdfa61ebe33ef25ae0e
Gregor Harlan <mail@gh01.de>
2026-01-04 22:59:43 +0100

    Update src/DocBlock/TypeExpression.php
    Co-authored-by: Dariusz Rumiński <dariusz.ruminski@gmail.com>

--- src/DocBlock/TypeExpression.php
+++ src/DocBlock/TypeExpression.php
@@ -506,7 +506,7 @@ final class TypeExpression
                             $innerValue .= $innerValues[$i]['next_glue_raw'];
 
                             ++$i;
-                            \assert(isset($innerValues[$i]));
+                            \assert(isset($innerValues[$i])); // for PHPStan
                         }
 
                         $this->innerTypeExpressions[] = [
