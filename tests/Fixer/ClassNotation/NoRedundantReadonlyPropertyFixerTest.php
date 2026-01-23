7101f88c404abf3ef4f7a559bcad4e5319aaf589
Jack Worman <66142217+jack-worman@users.noreply.github.com>
2026-01-23 07:44:28 -0500

    feat: Add `NoRedundantReadonlyPropertyFixer` (#9339)

---
+++ tests/Fixer/ClassNotation/NoRedundantReadonlyPropertyFixerTest.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Tests\Fixer\ClassNotation;
+
+use PhpCsFixer\Tests\Test\AbstractFixerTestCase;
+
+/**
+ * @internal
+ *
+ * @covers \PhpCsFixer\Fixer\ClassNotation\NoRedundantReadonlyPropertyFixer
+ *
+ * @extends AbstractFixerTestCase<\PhpCsFixer\Fixer\ClassNotation\NoRedundantReadonlyPropertyFixer>
+ *
+ * @no-named-arguments Parameter names are not covered by the backward compatibility promise.
+ */
+final class NoRedundantReadonlyPropertyFixerTest extends AbstractFixerTestCase
+{
+    /**
+     * @dataProvider provideFixCases
+     *
+     * @requires PHP 8.2
+     */
+    public function testFix(string $expected, ?string $input): void
+    {
+        $this->doTest($expected, $input);
+    }
+
+    /**
+     * @return iterable<string, array{string, null|string}>
+     */
+    public static function provideFixCases(): iterable
+    {
+        yield 'normal properties' => [
+            <<<'PHP'
+                <?php
+                readonly class C1
+                {
+                    private int $bar;
+                    private int $baz;
+                }
+
+                class C2
+                {
+                    private readonly int $bar;
+                    private readonly int $baz;
+                }
+                PHP,
+            <<<'PHP'
+                <?php
+                readonly class C1
+                {
+                    private readonly int $bar;
+                    private readonly int $baz;
+                }
+
+                class C2
+                {
+                    private readonly int $bar;
+                    private readonly int $baz;
+                }
+                PHP,
+        ];
+
+        yield 'promoted properties' => [
+            <<<'PHP'
+                <?php
+                readonly class C1
+                {
+                    public function __construct(
+                        private int $bar,
+                        private int $baz,
+                    ) {
+                    }
+                }
+
+                class C2
+                {
+                    public function __construct(
+                        private readonly int $bar,
+                        private readonly int $baz,
+                    ) {
+                    }
+                }
+                PHP,
+            <<<'PHP'
+                <?php
+                readonly class C1
+                {
+                    public function __construct(
+                        private readonly int $bar,
+                        private readonly int $baz,
+                    ) {
+                    }
+                }
+
+                class C2
+                {
+                    public function __construct(
+                        private readonly int $bar,
+                        private readonly int $baz,
+                    ) {
+                    }
+                }
+                PHP,
+        ];
+    }
+}
