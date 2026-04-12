abae191993b81d844b14dfea15d40102b8ab6f43
Kuba Werłos <werlos@gmail.com>
2026-04-12 19:38:12 +0200

    self-approval

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -35,9 +35,6 @@ final class Cache implements CacheInterface
 
     public function __construct(SignatureInterface $signature)
     {
-        if (false) { // @phpstan-ignore if.alwaysFalse
-            throw new \Exception();
-        }
         $this->signature = $signature;
     }
 
