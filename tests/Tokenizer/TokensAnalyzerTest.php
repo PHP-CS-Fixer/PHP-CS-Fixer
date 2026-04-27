168a7173fe38940f2a594a576ab7975ea766355d
Kuba Werłos <werlos@gmail.com>
2026-04-27 21:14:47 +0200

    Add tests

--- tests/Tokenizer/TokensAnalyzerTest.php
+++ tests/Tokenizer/TokensAnalyzerTest.php
@@ -930,6 +930,30 @@ enum Foo: string
                 PHP,
         ];
 
+        yield 'closures' => [
+            [
+                11 => ['classIndex' => 3, 'type' => 'const'],
+                22 => ['classIndex' => 3, 'type' => 'method'],
+                33 => ['classIndex' => 3, 'type' => 'const'],
+                44 => ['classIndex' => 3, 'type' => 'method'],
+                67 => ['classIndex' => 3, 'type' => 'property'],
+                73 => ['classIndex' => 3, 'type' => 'method'],
+                91 => ['classIndex' => 3, 'type' => 'method'],
+                104 => ['classIndex' => 3, 'type' => 'method'],
+            ],
+            <<<'PHP'
+                <?php final class Foo
+                {
+                    private const \Closure I_AM_EMPTY = static function () {};
+                    private const \Closure I_AM_1 = static function (): int { return 1; };
+                    private \Closure $iAmTwo = static function (): int { return 2; };
+                    function my_array_filter(
+                        Closure $callback = static function ($item) { return !empty($item); },
+                    ) {}
+                }
+                PHP,
+        ];
+
         yield 'closure in attribute' => [
             [
                 46 => [
