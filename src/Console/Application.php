4140023f552ff02346df9b1329742532166f677f
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2026-06-16 11:52:26 +0200

    prepared the 3.95.8 release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -51,7 +51,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends BaseApplication
 {
     public const NAME = 'PHP CS Fixer';
-    public const VERSION = '3.95.8-DEV';
+    public const VERSION = '3.95.8';
     public const VERSION_CODENAME = 'Adalbertus';
 
     /**
