28828d9ce6d41e33ec4d705a39cc97918921481c
John Rayes <live627@gmail.com>
2026-07-24 06:21:14 -0700

    perf: `Tokens::findSequence()` - improve looping logic (#9737)

--- src/Tokenizer/Tokens.php
+++ src/Tokenizer/Tokens.php
@@ -935,17 +935,15 @@ class Tokens extends \SplFixedArray
         unset($sequence[$firstKey]);
 
         // begin searching for the first token in the sequence (start included)
-        $index = $start - 1;
-        while ($index <= $end) {
-            $index = $this->getNextTokenOfKind($index, [$firstToken], $firstCs);
+        for ($index = $start; $index <= $end; ++$index) {
+            $current = $this[$index];
 
-            // ensure we found a match and didn't get past the end index
-            if (null === $index || $index > $end) {
-                return null;
+            if (!$current->equals($firstToken, $firstCs)) {
+                continue;
             }
 
             // initialise the result array with the current index
-            $result = [$index => $this[$index]];
+            $result = [$index => $current];
 
             // advance cursor to the current position
             $currIdx = $index;
@@ -956,16 +954,18 @@ class Tokens extends \SplFixedArray
 
                 // ensure we didn't go too far
                 if (null === $currIdx || $currIdx > $end) {
-                    return null;
+                    continue 2;
                 }
 
-                if (!$this[$currIdx]->equals($token, self::isKeyCaseSensitive($caseSensitive, $key))) {
+                $current = $this[$currIdx];
+
+                if (!$current->equals($token, self::isKeyCaseSensitive($caseSensitive, $key))) {
                     // not a match, restart the outer loop
                     continue 2;
                 }
 
                 // append index to the result array
-                $result[$currIdx] = $this[$currIdx];
+                $result[$currIdx] = $current;
             }
 
             // do we have a complete match?
