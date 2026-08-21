373525063356fa64fc5f10bffc907a5f8f6ec02e
John Rayes <live627@gmail.com>
2026-08-21 06:06:30 -0700

    Update ArrayTypehintTransformer.php

--- src/Tokenizer/Transformer/ArrayTypehintTransformer.php
+++ src/Tokenizer/Transformer/ArrayTypehintTransformer.php
@@ -37,7 +37,7 @@ final class ArrayTypehintTransformer extends AbstractTransformer
 
     public function isCandidate(Tokens $tokens): bool
     {
-        return true;
+        return $tokens->isTokenKindFound(\T_ARRAY);
     }
 
     public function processToken(Tokens $tokens, Token $token, int $index): void
