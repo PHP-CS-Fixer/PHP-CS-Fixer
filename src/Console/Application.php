033b7c44bd79db75d30d1649aebd892941856bb9
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2026-08-21 19:04:14 +0200

    bumped version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -51,7 +51,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class Application extends BaseApplication
 {
     public const NAME = 'PHP CS Fixer';
-    public const VERSION = '3.95.21';
+    public const VERSION = '3.95.22-DEV';
     public const VERSION_CODENAME = 'Adalbertus';
 
     /**
